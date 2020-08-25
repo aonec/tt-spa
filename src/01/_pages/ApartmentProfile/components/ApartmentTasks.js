0d70b9194372653d0230065e40ecc0f00eeeefb6
Marat <maratismodest@gmail.com>
2020-08-25 14:04:09 +0300

    styled-compponents warnings

--- src/01/_pages/ApartmentProfile/components/ApartmentTasks.js
+++ src/01/_pages/ApartmentProfile/components/ApartmentTasks.js
@@ -1,60 +1,46 @@
-import styled, {
-  css
-} from 'styled-components'
+import styled from "styled-components";
 
-export const ApartmentTasks = styled.div `
-  padding:0 16px;
+export const ApartmentTasks = styled.div`
+  padding: 0 16px;
   display: flex;
-  flex-direction:column;
-    ${props =>
-      props.description &&
-      css`
-      font-size: 24px;
-      line-height: 48px;
-    `||
-      props.value &&
-      css`
-      font-size: 32px;
-      line-height: 48px;
-        `
-      };
-`
-export const ApartmentTasksTitle = styled.h2 `
-  padding-bottom:24px;
-  margin:0;
+  flex-direction: column;
+`;
+export const ApartmentTasksTitle = styled.h2`
+  padding-bottom: 24px;
+  margin: 0;
   font-weight: normal;
   font-size: 24px;
   line-height: 32px;
-`
-export const ApartmentTask = styled.a `
-  padding:0;
-  padding-top:32px;
-  margin:0;
-  &:first-of-type{
-    padding:0;
+`;
+export const ApartmentTask = styled.a`
+  padding: 0;
+  padding-top: 32px;
+  margin: 0;
+  &:first-of-type {
+    padding: 0;
   }
-`
-export const ApartmentTaskTitle = styled.h3 `
+`;
+export const ApartmentTaskTitle = styled.h3`
   font-size: 14px;
   line-height: 16px;
-`
+`;
 
-export const ApartmentTaskState = styled.p `
-  padding:0;
-  padding-top:8px;
-  margin:0;
-  display:flex;
+export const ApartmentTaskState = styled.p`
+  padding: 0;
+  padding-top: 8px;
+  margin: 0;
+  display: flex;
   font-size: 12px;
   line-height: 16px;
-  color: #17B45A;
-`
+  color: #17b45a;
+`;
 
-export const ApartmentTaskDate = styled.p `
-  padding:0;
-  padding-top:8px;
-  margin:0;
-  display:flex;
+export const ApartmentTaskDate = styled.p`
+  padding: 0;
+  padding-top: 8px;
+  margin: 0;
+  display: flex;
   font-size: 12px;
   line-height: 16px;
   color: rgba(39, 47, 90, 0.6);
-`
+`;
--- src/01/_pages/ApartmentProfile/components/Text.js
+++ src/01/_pages/ApartmentProfile/components/Text.js
@@ -7,17 +7,17 @@ export const Text = styled.p`
   font-size: 12px;
   line-height: 16px;
   ${(props) =>
-    (props.size == 12 &&
+    (props.size === 12 &&
       css`
         font-size: 12px;
         line-height: 16px;
       `) ||
-    (props.size == 24 &&
+    (props.size === 24 &&
       css`
         font-size: 24px;
         line-height: 48px;
       `) ||
-    (props.size == 32 &&
+    (props.size === 32 &&
       css`
         font-size: 32px;
         line-height: 48px;
