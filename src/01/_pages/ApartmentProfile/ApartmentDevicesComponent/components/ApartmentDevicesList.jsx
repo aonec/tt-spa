22f217a469a516eea8e697850a939288231a5b08
Marat <maratismodest@gmail.com>
2020-08-31 16:54:54 +0300

    передача свойств компонента через ApartmentDevicesList

--- src/01/_pages/ApartmentProfile/ApartmentDevicesComponent/components/ApartmentDevicesList.jsx
+++ src/01/_pages/ApartmentProfile/ApartmentDevicesComponent/components/ApartmentDevicesList.jsx
@@ -5,20 +5,12 @@ import { ApartmentDevicesContext } from '../ApartmentDevices';
 export function ApartmentDevicesList() {
   const ApartmentDevicesListContext = useContext(ApartmentDevicesContext);
 
-  const DevicesList = ApartmentDevicesListContext.map((value, index) => {
-    const {
-      id, model, serialNumber, resource, futureCheckingDate,
-    } = value;
-    return (
-      <ApartmentDeviceItem
-        id={id}
-        model={model}
-        serialNumber={serialNumber}
-        resource={resource}
-        futureCheckingDate={futureCheckingDate}
-      />
-    );
-  });
+  const DevicesList = ApartmentDevicesListContext.map((value) => (
+    // const {
+    //   id, model, serialNumber, resource, futureCheckingDate,
+    // } = value;
+    <ApartmentDeviceItem {...value} />
+  ));
 
   return <>{DevicesList}</>;
 }
