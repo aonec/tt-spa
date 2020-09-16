4872bccc4117d3e4d707016931c93b8cdf4003ae
Marat <maratismodest@gmail.com>
2020-09-16 18:13:44 +0300

    Update index.jsx

--- src/01/_pages/DeviceProfile/components/Modal/index.jsx
+++ src/01/_pages/DeviceProfile/components/Modal/index.jsx
@@ -6,7 +6,6 @@ import React, {
   Children,
 } from 'react';
 import './modal.scss';
-import { makeStyles } from '@material-ui/core/styles';
 import {
   Radio, ConfigProvider, DatePicker, Tabs, Select,
 } from 'antd';
@@ -17,11 +16,6 @@ import moment from 'moment';
 import $ from 'jquery';
 import DeviceIcons from '01/_components/DeviceIcons';
 import _ from 'lodash';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-// import Select from '@material-ui/core/Select';
 import { DEFAULT_ICON } from '../Templates';
 import { Icon } from '../../../../_components/Icon';
 import { DeviceContext } from '../../DeviceProfile';
@@ -31,311 +25,298 @@ const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 export const ModalODPU = () => {
-  const { device, building, hubs } = useContext(DeviceContext);
-  const { id, model } = { ...device };
-  const { number, street } = { ...building };
+    const { device, building, hubs } = useContext(DeviceContext);
+    const { id, model, serialNumber } = { ...device };
+    const serialNumberODPU = serialNumber;
+    const { number, street } = { ...building };
 
-  const period = useRef('month');
-  const detail = useRef('daily');
-  const type = useRef('coldwatersupply');
+    const period = useRef('month');
+    const detail = useRef('daily');
+    const type = useRef('coldwatersupply');
 
-  const [begin, setBegin] = useState(moment().subtract(1, 'month'));
-  const [end, setEnd] = useState(moment());
-  const [hubsarr, setHubsarr] = useState();
+    const [begin, setBegin] = useState(moment().subtract(1, 'month'));
+    const [end, setEnd] = useState(moment());
+    const [hubsarr, setHubsarr] = useState();
 
-  useEffect(() => {
-    setHubsarr(hubs);
-    console.log('hubs = ', hubs);
-  }, [hubs]);
+    useEffect(() => {
+      setHubsarr(hubs);
+    }, [hubs]);
 
-  const datePickerHandler = (event) => {
-    setBegin(event[0] || begin);
-    setEnd(event[1] || end);
-  };
+    const datePickerHandler = (event) => {
+      setBegin(event[0] || begin);
+      setEnd(event[1] || end);
+    };
 
-  const someFunc = () => {
-    console.log(hubsarr);
-    console.log(devicesList);
-    console.log(list);
-  };
+    const someFunc = () => {
+      console.log(hubs);
+      console.log(devicesList);
+    };
 
-  let list = [];
-  const devicesList = [];
+    const list = [];
+    const devicesList = [];
 
-  const Translate = {
-    Heat: 'Отопление',
-    ColdWaterSupply: 'Холодная вода',
-    HotWaterSupply: 'Горячая вода',
-  };
+    const Translate = {
+      "Heat": 'Отопление',
+      "ColdWaterSupply": 'Холодная вода',
+      "HotWaterSupply": 'Горячая вода',
+    };
 
-  const translate = (resource) => {
-    console.log('Translate[resource]', Translate[resource]);
-    return Translate[resource];
-  };
+    const translate = (resource) => Translate[resource];
 
-  function DevicesListDiv() {
-    const b = devicesList.map(
-      ({
-        resource, entryNumber, type, serialNumber,
-      }, index) => {
+    const onTabsChangeHandler = (key) => {
+      console.log('onTabsChangeHandler', key);
+    };
+
+    function DevicesListDiv(){
+      devicesList.map(({
+                         resource, entryNumber, type, serialNumber,
+                       }, index) => {
         if (!list.includes(resource)) {
           list.push(resource);
         }
-      },
-    );
-    const someList = list.map((value, index) => {
-      const a = translate(value);
+      });
+
+      const someList = list.map((value, index) => {
+        const res = translate(value);
+        return (
+          <TabPane tab={res} key={index + 1}>
+            {res}
+          </TabPane>
+        );
+      });
 
       return (
-        <TabPane tab={a} key={index + 1}>
-          {a}
-        </TabPane>
+        <Tabs defaultActiveKey="1" onChange={onTabsChangeHandler}>
+          {someList}
+          <TabPane tab='Все' key="4">
+            Все
+          </TabPane>
+        </Tabs>
       );
-    });
+    }
 
-    return <Tabs defaultActiveKey="1">{someList}</Tabs>;
-  }
-
-  // Получаем массив всех ПРЭМ, которые походят
-  if (hubsarr) {
-    // console.log('hubsarr', hubsarr);
-    hubsarr.map((value) => {
-      const { resource, entryNumber, pipes } = { ...value };
-      // console.log(resource);
-      const pipesList = pipes.map((values) => {
-        const { devices } = { ...values };
-        // console.log(devices);
-        const devicesRes = devices.map((value) => {
-          const { serialNumber, type } = { ...value };
-          if (type === 'FlowMeter') {
-            // console.log(serialNumber);
-            devicesList.push({
-              resource,
-              entryNumber,
-              type,
-              serialNumber,
-            });
-          }
+    // Получаем массив всех ПРЭМ, которые походят
+    if (hubsarr) {
+      hubsarr.map((value) => {
+        const { resource, entryNumber, pipes } = { ...value };
+        const pipesList = pipes.map((values) => {
+          const { devices } = { ...values };
+          const devicesRes = devices.map((value) => {
+            const { serialNumber, type } = { ...value };
+            if (type === 'FlowMeter') {
+              devicesList.push({
+                resource,
+                entryNumber,
+                type,
+                serialNumber,
+              });
+            }
+          });
         });
       });
-    });
-  }
+    }
 
-  const onPeriodChange = (e) => {
-    const res = e.target.value;
-    period.current = res;
-    setBegin(moment().subtract(1, res));
-    setEnd(moment());
-  };
+    const onPeriodChange = (e) => {
+      const res = e.target.value;
+      period.current = res;
+      setBegin(moment().subtract(1, res));
+      setEnd(moment());
+    };
 
-  const onDetailChange = (e) => {
-    const result = e.target.value;
-    detail.current = result;
-  };
+    const onDetailChange = (e) => {
+      const result = e.target.value;
+      detail.current = result;
+    };
 
-  const onTypeChange = (e) => {
-    const typeResult = e.target.value;
-    type.current = typeResult;
-  };
+    const onTypeChange = (e) => {
+      const typeResult = e.target.value;
+      type.current = typeResult;
+    };
 
-  function callback(key) {
-    console.log(key);
-    console.log('test');
-  }
+    function callback(key){
+      console.log(key);
+      console.log('test');
+    }
 
-  const onSelectChange = () => {
-    console.log('onSelectChange');
-  };
+    const onSelectChange = () => {
+      console.log('onSelectChange');
+    };
 
-  const onTabsChange = (event) => {
-    console.log('onTabsChange', event);
-  };
+    const onTabsChange = (event) => {
+      console.log('onTabsChange', event);
+    };
 
-  // const selectOptions = [
-  //   'Узел 1: ВКТ-7 (1234567890), ПРЭМ (1234567890), ПРЭМ (9876543210)',
-  //   'Узел 2: ВКТ-7 (9876543210), ПРЭМ (23549579374023), ПРЭМ(29387592701)',
-  // ];
+    // const selectOptions = [
+    //   'Узел 1: ВКТ-7 (1234567890), ПРЭМ (1234567890), ПРЭМ (9876543210)',
+    //   'Узел 2: ВКТ-7 (9876543210), ПРЭМ (23549579374023), ПРЭМ(29387592701)',
+    // ];
 
-  const selectOptions = [];
-  devicesList.map(({ resource }) => {
-    selectOptions.push(resource);
-  });
+    const selectOptions = [];
+    devicesList.map(({ resource, serialNumber, entryNumber }) => {
 
-  const downloadReport = () => {
-    const link = `http://84.201.132.164:8080/api/reports/xlsx?deviceId=${id}&ereporttype=${
-      detail.current
-    }&resourcetype=${type.current}&entrynumber=2&from=${convertDateOnly(
-      begin,
-    )}T00:00:00Z&to=${convertDateOnly(end)}T00:00:00Z`;
+        var users = [
+          { 'user': 'barney', 'age': 36, 'active': true },
+          { 'user': 'fred', 'age': 40, 'active': false },
+          { 'user': 'pebbles', 'age': 1, 'active': true }
+        ];
 
-    const template = 'http://84.201.132.164:8080/api/reports/xlsx?deviceId=1510&ereporttype=daily&resourcetype=heat&entrynumber=1&from=2020-08-15T00:00:00Z&to=2020-08-25T00:00:00Z';
-    window.location.assign(link);
-  };
+        if (_.find(selectOptions, function (o){
+          return o.value === resource
+        })) {
 
-  const hideMe = () => {
-    $('.overlay').css('display', 'none');
-  };
 
-  const handleChange = () => {
-  };
+          // selectOptions.push({
+          //   label: label + ` ПРЭМ ${serialNumber}) ${resource}`,
+          //   value: resource,
+          // });
+        }
+          // selectOptions.push({
+          //   label: label + ` ПРЭМ ${serialNumber}) ${resource}`,
+          //   value: resource,
+        // });
 
-  return (
-    <div className="overlay">
-      <div className="modal-odpu">
-        <Icon
-          className="modal__close"
-          icon="close"
-          color="#272F5A"
-          onClick={hideMe}
-        />
-        <div className="modal__top">
-          <h3 className="modal__title">
-            Выгрузка отчета о общедомовом потреблении
-          </h3>
-          {/* <Demo /> */}
-          <button onClick={someFunc}>someFunc</button>
-          <DevicesListDiv />
-          <div>
-            <Select
-              defaultValue="lucy"
-              style={{ width: 120 }}
-              onChange={handleChange}
-              className="inner"
-            >
-              <Option value="jacks" key="1">Jacks</Option>
-              <Option value="jacks" key="2">Jacks</Option>
-            </Select>
 
-            <label className="modal__label" htmlFor="#input">
-              Название отчета
-            </label>
-            <input
-              className="modal__input"
-              id="input"
-              value={`${model}_${street}_${number}.exls`}
-              disabled
-            />
-          </div>
-          <div className="period_and_type ">
-            <div className="period">
-              <label className="modal__label" htmlFor="#period">
-                Период выгрузки
-              </label>
-              <ConfigProvider locale={ruRu}>
-                <RangePicker
-                  allowClear={false}
-                  size="48px"
-                  value={[begin, end]}
-                  placeholder={['Дата Начала', 'Дата окончания']}
-                  onChange={(event) => {
-                    datePickerHandler(event);
-                  }}
-                />
-              </ConfigProvider>
-              <label className="modal__label" htmlFor="#resource">
-                Тип ресурса
-              </label>
+        else {
+          selectOptions.push({
+            "label": `Узел ${entryNumber} ${model} ${serialNumberODPU} ПРЭМ (${serialNumber}) ${resource}`,
+            "value": resource,
+          });
+        }
 
-              <Radio.Group
-                id="resource"
-                defaultValue="coldwatersupply"
-                size="large"
-                onChange={(event) => onTypeChange(event)}
-              >
-                <Radio.Button value="coldwatersupply" checked>
-                  Холодная вода
-                </Radio.Button>
-                <Radio.Button value="heat">Горячая вода</Radio.Button>
-              </Radio.Group>
-            </div>
-            <div className="type">
-              <label className="modal__label" htmlFor="#type">
-                Тип архива
-              </label>
 
-              <Radio.Group
-                defaultValue="month"
-                size="large"
-                onChange={(event) => onPeriodChange(event)}
-              >
-                <Radio.Button value="month" checked>
-                  Месячный
-                </Radio.Button>
-                <Radio.Button value="day">Суточный</Radio.Button>
-                <Radio.Button value="year">Годовой</Radio.Button>
-              </Radio.Group>
-              <label className="modal__label" htmlFor="#type">
-                Детализация
-              </label>
-            </div>
-          </div>
-        </div>
+      }
+    )
+    ;
 
-        <div className="modal__bottom">
-          <button
-            className="modal__button modal__button_cancel"
+    const showExactOptions = () => {
+    };
+
+    const downloadReport = () => {
+      const link = `http://84.201.132.164:8080/api/reports/xlsx?deviceId=${id}&ereporttype=${
+        detail.current
+      }&resourcetype=${type.current}&entrynumber=2&from=${convertDateOnly(
+        begin,
+      )}T00:00:00Z&to=${convertDateOnly(end)}T00:00:00Z`;
+
+      const template = 'http://84.201.132.164:8080/api/reports/xlsx?deviceId=1510&ereporttype=daily&resourcetype=heat&entrynumber=1&from=2020-08-15T00:00:00Z&to=2020-08-25T00:00:00Z';
+      window.location.assign(link);
+    };
+
+    const hideMe = () => {
+      $('.overlay').css('display', 'none');
+    };
+
+    const handleChange = () => {
+    };
+
+    return (
+      <div className="overlay">
+        <div className="modal-odpu">
+          <Icon
+            className="modal__close"
+            icon="close"
+            color="#272F5A"
             onClick={hideMe}
-          >
-            Отмена
-          </button>
-          <button
-            className="modal__button modal__button_ok"
-            onClick={downloadReport}
-          >
-            Выгрузить
-          </button>
+          />
+          <div className="modal__top">
+            <h3 className="modal__title">
+              Выгрузка отчета о общедомовом потреблении
+            </h3>
+            {/* <Demo /> */}
+            <button onClick={someFunc}>someFunc</button>
+            <DevicesListDiv/>
+            <div>
+              <Select
+                defaultValue="Выберите узел"
+                style={{ "width": '100%' }}
+                onChange={handleChange}
+                className="inner"
+                options={selectOptions}
+              />
+
+              <label className="modal__label" htmlFor="#input">
+                Название отчета
+              </label>
+              <input
+                className="modal__input"
+                id="input"
+                value={`${model}_${street}_${number}.exls`}
+                disabled
+              />
+            </div>
+            <div className="period_and_type ">
+              <div className="period">
+                <label className="modal__label" htmlFor="#period">
+                  Период выгрузки
+                </label>
+                <ConfigProvider locale={ruRu}>
+                  <RangePicker
+                    allowClear={false}
+                    size="48px"
+                    value={[begin, end]}
+                    placeholder={['Дата Начала', 'Дата окончания']}
+                    onChange={(event) => {
+                      datePickerHandler(event);
+                    }}
+                  />
+                </ConfigProvider>
+                <label className="modal__label" htmlFor="#resource">
+                  Тип ресурса
+                </label>
+
+                <Radio.Group
+                  id="resource"
+                  defaultValue="coldwatersupply"
+                  size="large"
+                  onChange={(event) => onTypeChange(event)}
+                >
+                  <Radio.Button value="coldwatersupply" checked>
+                    Холодная вода
+                  </Radio.Button>
+                  <Radio.Button value="heat">Горячая вода</Radio.Button>
+                </Radio.Group>
+              </div>
+              <div className="type">
+                <label className="modal__label" htmlFor="#type">
+                  Тип архива
+                </label>
+
+                <Radio.Group
+                  defaultValue="month"
+                  size="large"
+                  onChange={(event) => onPeriodChange(event)}
+                >
+                  <Radio.Button value="month" checked>
+                    Месячный
+                  </Radio.Button>
+                  <Radio.Button value="day">Суточный</Radio.Button>
+                  <Radio.Button value="year">Годовой</Radio.Button>
+                </Radio.Group>
+                <label className="modal__label" htmlFor="#type">
+                  Детализация
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="modal__bottom">
+            <button
+              className="modal__button modal__button_cancel"
+              onClick={hideMe}
+            >
+              Отмена
+            </button>
+            <button
+              className="modal__button modal__button_ok"
+              onClick={downloadReport}
+            >
+              Выгрузить
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+;
 
 export default ModalODPU;
-
-/* <Option value="jack">{selectOptions[0]}</Option>
-              <Option value="lucy">{selectOptions[1]}</Option> */
-
-// Создаем Табы
-// const tabsArr = () => hubsarr.map((value, id) => {
-//   const { resource, entryNumber } = { ...value };
-//   switch (resource) {
-//     case 'Heat':
-//       Arr.Heat.push(value);
-//       break;
-//     case 'ColdWaterSupply':
-//       Arr.Cold.push(value);
-//       break;
-//     case 'HotWaterSupply':
-//       Arr.Hot.push(value);
-//       break;
-//     default:
-//   }
-// });
-
-// let a;
-// if (hubsarr) {
-//   a = hubsarr.map((value, id) => {
-//     const { resource, entryNumber } = { ...value };
-//     switch (resource) {
-//       case 'Heat':
-//         Arr.Heat.push(value);
-//         break;
-//       case 'ColdWaterSupply':
-//         Arr.Cold.push(value);
-//         break;
-//       case 'HotWaterSupply':
-//         Arr.Hot.push(value);
-//         break;
-//       default:
-//     }
-
-//     const { icon, color, translate } = DeviceIcons[resource] || DEFAULT_ICON;
-
-//     return (
-//       <TabPane tab={translate} key={id + 1}>
-//         <div>{`${translate} ${entryNumber}`}</div>
-//         <Icon icon={icon} color={color} />
-//       </TabPane>
-//     );
-//   });
-// }
