c49ecaee05ff5875c92b47607d587aaa0377e2d3
Marat <maratismodest@gmail.com>
2020-09-17 15:52:39 +0300

    commit

--- src/01/_pages/ObjectProfile/components/Modal/index.js
+++ src/01/_pages/ObjectProfile/components/Modal/index.js
@@ -1,52 +1,58 @@
-import React from "react";
-import './modal.scss'
-import { Icon } from "../../../../_components/Icon";
-import $ from "jquery";
-import Page1 from "./components/Page1";
-import Page2 from "./components/Page2";
-import TabsComponent from "./components/Tabs";
+import React, { useState } from 'react';
+import './modal.scss';
+import $ from 'jquery';
+import { Icon } from '../../../../_components/Icon';
+import TabsComponent from './components/Tabs';
+
+export const AddDeviceContext = React.createContext();
 
 export const ModalODPU = () => {
+  const nextOrDone = () => {
+    console.log('nextOrDone');
 
-  const nextOrDone  = () => {
-    console.log("nextOrDone")
-  }
+  };
+
+  const [tab, setTab] = useState(1);
+
   const hideMe = () => {
     $('.overlay').css('display', 'none');
   };
-
-  console.log("ModalODPU'")
+  const test = 'test';
+  console.log("ModalODPU'");
   return (
-    <div className="overlay">
-      <div className="modal-odpu-add">
-        <Icon
-          className="modal__close"
-          icon="close"
-          color="#272F5A"
-          onClick={hideMe}
-        />
-        <h2 className={'title-32'}>Добавление нового ОДПУ</h2>
-        {/*<Page1 />*/}
-      <TabsComponent />
-
-        <div className="modal__bottom">
-          <button
-            className="modal__button modal__button_cancel"
+    <AddDeviceContext.Provider
+      value={{ tab, setTab }}
+    >
+      <div className="overlay">
+        <div className="modal-odpu-add">
+          <Icon
+            className="modal__close"
+            icon="close"
+            color="#272F5A"
             onClick={hideMe}
-          >
-            Отмена
-          </button>
-          <button
-            className="modal__button modal__button_ok"
-            onClick={nextOrDone}
-          >
-            Далее
-          </button>
-        </div>
+          />
+          <h2 className="title-32">Добавление нового ОДПУ</h2>
+          <TabsComponent/>
 
+          <div className="modal__bottom">
+            <button
+              className="modal__button modal__button_cancel"
+              onClick={hideMe}
+            >
+              Отмена
+            </button>
+            <button
+              className="modal__button modal__button_ok"
+              onClick={nextOrDone}
+            >
+              Далее
+            </button>
+          </div>
+
+        </div>
       </div>
-    </div>
-  )
-}
+    </AddDeviceContext.Provider>
+  );
+};
 
 export default ModalODPU;
