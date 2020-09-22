eda9b64bf01907fcae66e9e15cff8d2691e0c3af
Marat <maratismodest@gmail.com>
2020-09-22 09:54:49 +0300

    Update index.jsx

--- src/01/_pages/Login/index.jsx
+++ src/01/_pages/Login/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Loader } from '01/components';
+import axios from '01/axios';
+import { useHistory } from 'react-router-dom';
 import login from '01/assets/svg/login.svg';
 import logo from '01/assets/svg/logo.svg';
 import {
   Title, Label, Wrap, Button, Input, Icon,
 } from '01/tt-components';
-import styledcomponent from 'styled-components';
-import { useLogin } from './useLogin';
+import styled from 'styled-components';
+import { some } from 'lodash';
+// import { useLogin } from './useLogin';
 
-export const Main = styledcomponent.div`
+export const Main = styled.div`
   height: 100vh;
   padding: 40px;
   place-content: center;
@@ -18,45 +21,61 @@ export const Main = styledcomponent.div`
   grid-template-columns: 1fr 1fr;
   align-items: center;
 `;
-export const Form = styledcomponent.form`
+export const Form = styled.form`
   display: grid;
   max-width: 400px;
   height: fit-content;
   grid-gap: 24px;
 `;
 
-export const Img = styledcomponent.img`
+export const Img = styled.img`
   margin-right: 16px;
 `;
 
-export const LoginLeft = styledcomponent.div`
+export const LoginLeft = styled.div`
   position: relative;
   display: flex;
   justify-self: end;
   flex-direction: column;
   align-items: center;
-  // margin-left: auto;
   padding-top: 48px;
 `;
 
-export const LoginRight = styledcomponent.div`
+export const LoginRight = styled.div`
   margin-left: 48px;
 `;
 
-export const LoginTop = styledcomponent.div`
+export const LoginTop = styled.div`
   position: absolute;
   display: flex;
 `;
 
 export const Login = () => {
-  const {
-    submit,
-    formProps,
-    emailProps,
-    passProps,
-    btnFormProps,
-    passBtnProps,
-  } = useLogin();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [submit, setSubmit] = useState(false);
+  const [showPass, setShowPass] = useState(false);
+  const { replace } = useHistory();
+
+  const someFunc = () => {
+    console.log('someFunc');
+  };
+
+  useEffect(() => {
+    if (submit) {
+      (async function () {
+        try {
+          await axios.post('auth/login', { email, password });
+          await axios.get('ManagingFirmUsers/current');
+          replace('/tasks/');
+        } catch (error) {
+          setSubmit(false);
+        } finally {
+          setSubmit(false);
+        }
+      }());
+    }
+  }, [submit, email, password, replace]);
 
   return (
     <Main>
@@ -69,23 +88,62 @@ export const Login = () => {
       </LoginLeft>
       <LoginRight>
         <Title size="40">Вход в систему</Title>
-        <Form {...formProps}>
+        <Form
+          // {...formProps}
+          onSubmit={(e) => {
+            e.preventDefault();
+            setSubmit(true);
+          }}
+        >
           <Wrap>
             <Label>Логин</Label>
             <Input data-big>
-              <input {...emailProps} />
+              <input
+                // {...emailProps}
+                name="email"
+                placeholder="Введите логин"
+                type="text"
+                readOnly={submit}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </Input>
           </Wrap>
 
           <Wrap>
             <Label>Пароль</Label>
             <Input data-big>
-              <input {...passProps} />
-              <Icon {...passBtnProps} />
+              <input
+                //  {...passProps}
+                name="password"
+                placeholder="Введите пароль"
+                readOnly={submit}
+                type={showPass ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <Icon
+                //  {...passBtnProps}
+                icon={showPass ? 'off' : 'on'}
+                // onClick={setShowPass(!showPass)}
+                onClick={() => {
+                  setShowPass(!showPass);
+                }}
+              />
             </Input>
           </Wrap>
           <Loader show={submit} size="48">
-            <Button data-big data-primary {...btnFormProps}>
+            <Button
+              data-big
+              data-primary
+              //  {...btnFormProps}
+              disabled={submit}
+              type="submit"
+            >
               <span>Вход в систему</span>
             </Button>
           </Loader>
@@ -96,89 +154,3 @@ export const Login = () => {
 };
 
 export default Login;
-// import React from 'react';
-
-// import { input, button } from '01/r_comp';
-// import { Icon, Loader } from '01/components';
-// import styled from 'styled-components';
-// import { useLogin } from './useLogin';
-
-// export const Template = styled.div``;
-
-// export const Form = styled.form`
-//   display: grid;
-//   min-width: 400px;
-//   grid-gap: 24px;
-// `;
-
-// export const Main = styled.form`
-//   display: grid;
-//   height: 100vh;
-//   place-content: center;
-//   background: var(--login-bg);
-//   color: #fff;
-// `;
-
-// export const Index = styled.form`
-//   display: grid;
-//   grid-gap: 8px;
-// `;
-
-// export const Input = styled.input`
-//   background: #ffffff;
-//   border: 1px solid #dcdee4;
-//   box-sizing: border-box;
-//   border-radius: 4px;
-//   height: 48px;
-//   color: var(--main-80);
-// `;
-
-// export const Index = styled.h2`
-//   padding: 0;
-//   margin: 0;
-//   font-weight: 300;
-//   font-size: 40px;
-//   line-height: 48px;
-//   color: #ffffff;
-// `;
-
-// export const Login = () => {
-//   const {
-//     submit,
-//     formProps,
-//     emailProps,
-//     passProps,
-//     btnFormProps,
-//     passBtnProps,
-//   } = useLogin();
-
-//   return (
-//     <Main>
-//       <Form {...formProps}>
-//         <Index>Вход в систему</Index>
-//         <Index>
-//           Логин
-//           <Input />
-//           {/* <input_frame data-big>
-//             <Input {...emailProps} />
-//           </input_frame> */}
-//         </Index>
-//         <Index>
-//           Пароль
-//           <Input />
-//           {/* <input_frame data-big>
-//             <Input {...passProps} />
-//             <pass_btn as="Icon" {...passBtnProps} />
-//           </input_frame> */}
-//         </Index>
-//         <Loader show={submit} size="48">
-//           <button data-big data-primary {...btnFormProps}>
-//             <span>Вход в систему</span>
-//           </button>
-//         </Loader>
-//       </Form>
-//     </Main>
-//   );
-// };
-
-// export default Login;
