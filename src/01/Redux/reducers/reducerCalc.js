7111268c6097234949379f2502efd9bbf2d3a82e
Marat <maratismodest@gmail.com>
2020-10-01 18:15:37 +0300

    Update reducerCalc.js

--- src/01/Redux/reducers/reducerCalc.js
+++ src/01/Redux/reducers/reducerCalc.js
@@ -1,5 +1,16 @@
 import moment from 'moment';
 
+const INFO_ID = 'InfoId';
+const SERIAL_NUMBER = 'serialNumber';
+const CHECKING_DATE = 'checkingDate';
+const FUTURE_CHECKING_DATE = 'futureCheckingDate';
+const LAST_COMMERCIAL_ACCOUNTING_DATE = 'lastCommercialAccountingDate';
+const IPV4 = 'ipV4';
+const DEVICE_ADDRESS = 'deviceAddress';
+const PORT = 'port';
+const FUTURE_COMMERCIAL_ACCOUNTING_DATE = 'futureCommercialAccountingDate';
+const HOUSE_STOCKING_ID = 'housingStockId';
+
 export const initialState = {
   checkingDate: moment().toISOString(),
   futureCheckingDate: moment().toISOString(),
@@ -10,7 +21,7 @@ export const initialState = {
     port: 1234,
   },
   futureCommercialAccountingDate: moment().toISOString(),
-  housingStockId: 485,
+  housingStockId: 0,
   infoId: 1,
   serialNumber: 'serialNumber',
 };
@@ -21,33 +32,33 @@ export default function reducerCalc(state = initialState, action) {
 
   // console.log(connection);
 
-  if (action.type === 'InfoId') {
+  if (action.type === INFO_ID) {
     return { ...state, infoId: +action.value };
   }
-  if (action.type === 'serialNumber') {
+  if (action.type === SERIAL_NUMBER) {
     // здесь это тоже передается строковым значением
     return { ...state, serialNumber: `${action.value}` };
   }
-  if (action.type === 'housingStockId') {
+  if (action.type === HOUSE_STOCKING_ID) {
     return { ...state, housingStockId: action.value };
   }
-  if (action.type === 'lastCommercialAccountingDate') {
+  if (action.type === LAST_COMMERCIAL_ACCOUNTING_DATE) {
     return { ...state, lastCommercialAccountingDate: action.value };
   }
-  if (action.type === 'checkingDate') {
+  if (action.type === CHECKING_DATE) {
     return { ...state, checkingDate: action.value };
   }
-  if (action.type === 'futureCheckingDate') {
+  if (action.type === FUTURE_CHECKING_DATE) {
     return { ...state, futureCheckingDate: action.value };
   }
-  if (action.type === 'futureCommercialAccountingDate') {
+  if (action.type === FUTURE_COMMERCIAL_ACCOUNTING_DATE) {
     return { ...state, futureCommercialAccountingDate: action.value };
   }
-  if (action.type === 'port') {
+  if (action.type === PORT) {
     port = Number(action.value);
     return { ...state, connection: { ipV4, deviceAddress, port } };
   }
-  if (action.type === 'ipV4') {
+  if (action.type === IPV4) {
     ipV4 = `${action.value}`;
     return { ...state, connection: { ipV4, deviceAddress, port } };
   }
