89a906d5322cf256e26654178822eb67a839414e
Marat <maratismodest@gmail.com>
2020-10-02 13:19:33 +0300

    Update index.jsx

--- src/01/_pages/Devices/index.jsx
+++ src/01/_pages/Devices/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Header } from '../../tt-components';
-import TabsDevices from "./components/TabsDevices";
-
+import TabsDevices from './components/TabsDevices';
 
 const Devices = () => (
-    <div>
-        <Header>Приборы</Header>
-        <TabsDevices />
-    </div>
+  <div>
+    <Header>Приборы</Header>
+    <TabsDevices />
+  </div>
 );
 
 export default Devices;
