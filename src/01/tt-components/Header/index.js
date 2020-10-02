a42025ff6c0d3b3f63c0f1c252813f6f15086e7a
makeevvd <makeevvd1@gmail.com>
2020-10-02 11:27:12 +0300

    addHeader

---
+++ src/01/tt-components/Header/index.js
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components';
+
+export const Header = styled.h1`
+  font-weight: 300;
+  color: #272F5A;
+  font-size: 32px;
+  line-height: 1.5;
+`;
+
+export default Header;
