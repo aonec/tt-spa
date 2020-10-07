6f863688f0ae5d56ef54640882b85d47bb154064
Vladislav Makeev <makeevvd1@gmail.com>
2020-10-07 16:31:35 +0300

    improve pagination for real data

--- src/01/Redux/reducers/reducerDevicesPage.js
+++ src/01/Redux/reducers/reducerDevicesPage.js
@@ -2,11 +2,12 @@ import { devicesAPI } from '../../_api/devices_page';
 
 const SET_DEVICES = 'SET_DEVICES';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const TOGGLE_IS_LOADING = 'TOGGLE_IS_LOADING';
 
 const initialState = {
   totalItems: 0,
   pageNumber: 1,
-  pageSize: 10,
+  pageSize: 5,
   totalPages: 48,
   hasPreviousPage: false,
   hasNextPage: true,
@@ -14,6 +15,7 @@ const initialState = {
   previousPageNumber: 1,
   devices: [],
   currentPage: 1,
+  isLoading: false,
 };
 
 export default function reducerDevicesPage(state = initialState, action) {
@@ -24,20 +26,28 @@ export default function reducerDevicesPage(state = initialState, action) {
     case SET_CURRENT_PAGE:
       return { ...state, currentPage: action.currentPage}
 
+    case TOGGLE_IS_LOADING:
+      return { ...state, isLoading: !state.isLoading}
+
     default: return state;
   }
 
 }
 
 export const setDevices = (devices) => ({ type: SET_DEVICES, devices });
-export const setCurrentPage = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage})
+export const setCurrentPage = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage});
+export const toggleIsLoading = () => ({ type: TOGGLE_IS_LOADING});
+
+
 
 export const getDevices = (pageNumber, pageSize) => async (dispatch) => {
+  dispatch(toggleIsLoading());
   const devices = await devicesAPI.getDevices(pageNumber, pageSize);
   const devicesWithRelated = await Promise.all(devices.map(async (d) => {
     const relatedDevices = await devicesAPI.getRelatedDevices(d.id);
     // if (!relatedDevices.length) return {...d, relatedDevices}
     return { ...d, relatedDevices: [...relatedDevices] };
   }));
+  dispatch(toggleIsLoading());
   dispatch(setDevices(devicesWithRelated));
 };
--- src/01/_pages/Devices/components/TabsDevices.jsx
+++ src/01/_pages/Devices/components/TabsDevices.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import { Tabs } from 'antd';
 import {devicesAPI} from "../../../_api/devices_page";
 import {Loader} from "../../../components/Loader";
-import {getDevices, setCurrentPage} from "../../../Redux/reducers/reducerDevicesPage";
+import {getDevices, setCurrentPage, toggleIsLoading} from "../../../Redux/reducers/reducerDevicesPage";
 import {compose} from "redux";
 import Device from "./Device";
 import {useDispatch, useSelector} from "react-redux";
@@ -20,49 +20,60 @@ function callback(key) {
 
 
 const TabsDevices = () => {
-
     const dispatch = useDispatch();
     const pageSize = useSelector((state) => state.devicePage.pageSize);
     const currentPage = useSelector((state) => state.devicePage.currentPage);
     const totalPages = useSelector((state) => state.devicePage.totalPages);
+    const isLoading = useSelector((state) => state.devicePage.isLoading);
+
 
 
     const pages = [];
     createPages(pages, totalPages, currentPage);
 
     useEffect( () => {
-        dispatch(getDevices(currentPage, pageSize))
+        dispatch(toggleIsLoading());
+        dispatch(getDevices(currentPage, pageSize));
+        dispatch(toggleIsLoading());
     }, [currentPage, pageSize]);
 
     const deviceItems = useSelector((state) => state.devicePage.devices);
 
 
-    if (!deviceItems) return <div>LOADING...</div>
-
-
-
-        const deviceElems = deviceItems.map((device) => {
-                return <div>Прибор - {device.id}</div>
+    const deviceElems = deviceItems.map((device) => {
+        return <div>
+            Прибор - {device.id}
+            <div>Подприборы: {device.relatedDevices.length ?
+                device.relatedDevices.map((device) => <div>Подприбор {device.id}</div>) :
+                'Подприборов нет'
             }
-        );
+            </div>
 
-        return <Tabs defaultActiveKey="1" onChange={callback}>
-            <TabPane tab="ОДПУ" key="1">
-                {deviceElems}
-                <div className={styles.pages}>
-                    {pages.map((page, index) => <span
-                        key={index}
-                        className={currentPage == page ? styles.currentPage : styles.page}
-                        onClick={() => dispatch(setCurrentPage(page))}
-                    >{page}</span> )}
-                </div>
-            </TabPane>
-            <TabPane tab="ИПУ" key="2">
-                Content of Tab Pane 2
-            </TabPane>
-        </Tabs>
-    }
+        </div>
+    })
+
+    // if (isLoading) return <div>LOADING...</div>
+
+    return <Tabs defaultActiveKey="1" onChange={callback}>
+        <TabPane className={styles.tab} tab="ОДПУ" key="1">
+            {isLoading ? <div>ЗАГРУЗКА... <Loader show={true}/></div> :
+                <div>
+                <div className={styles.devices}>{deviceElems}</div>
+            <div className={styles.pages}>
+                {pages.map((page, index) => <span
+                    key={index}
+                    className={currentPage == page ? styles.currentPage : styles.page}
+                    onClick={() => dispatch(setCurrentPage(page))}
+                >{page}</span> )}
+            </div>
+                </div>}
+        </TabPane>
+        <TabPane tab="ИПУ" key="2">
+            Content of Tab Pane 2
+        </TabPane>
+    </Tabs>
+}
 
 
 
-    export default TabsDevices
+export default TabsDevices
