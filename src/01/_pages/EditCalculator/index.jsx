fc8041f5164c71970ad91e08e99b924488295488
aonec <githib@aonec.ru>
2020-10-08 14:41:40 +0300

    commit

--- src/01/_pages/EditCalculator/index.jsx
+++ src/01/_pages/EditCalculator/index.jsx
@@ -15,42 +15,18 @@ import {
   ModalClose,
 } from '01/tt-components/Modal';
 
-import { Title, ButtonTT, Header } from '01/tt-components';
+import { ButtonTT, Header } from '01/tt-components';
 import TabsComponent from './components/Tabs/Main';
 import { setAddCalculatorForm } from './store/actions';
-
-import { items } from './components/CalculatorJSON';
 
 export const AddDeviceContext = React.createContext();
 
 const EditCalculator = () => {
   const { 0: objid, 1: deviceId } = useParams();
   const [currentTabKey, setTab] = useState('1');
-  const modalRef = React.createRef();
   const dispatch = useDispatch();
   const calculatorPage = useSelector((state) => state.calculatorPage);
-  const [currentCalc, setCurrentCalc] = useState({});
-
-  const {
-    calculator,
-    canBeEdited,
-    closingDate,
-    deviceAddress,
-    diameter,
-    futureCheckingDate,
-    futureCommercialAccountingDate,
-    housingStockId,
-    id,
-    ipV4,
-    lastCheckingDate,
-    lastCommercialAccountingDate,
-    model,
-    port,
-    resource,
-    serialNumber,
-    type,
-    underTransaction,
-  } = currentCalc;
+  const [currentCalc, setCurrentCalc] = useState();
 
   const items = [
     {
@@ -77,17 +53,10 @@ const EditCalculator = () => {
 
   async function getCalculator(url = '') {
     try {
-      // const res = await axios.get(replaceURL(url));
       const res = await axios.get(`MeteringDevices/${url}`);
-      console.log('getCalculator', res);
-      //  return { ...res, info: true, header: createTitleObject(res) };
       return res;
     } catch (error) {
       console.log(error);
-      throw {
-        resource: 'device',
-        message: 'Произошла ошибка запроса устройства',
-      };
     }
   }
 
@@ -99,26 +68,34 @@ const EditCalculator = () => {
   }, []);
 
   useEffect(() => {
-    // console.log('some', currentCalc);
-    const currentInfoId = _.find(items, { label: model });
-    console.log(currentInfoId)
-    const { value } = {...currentInfoId} ;
-    console.log(value)
+    if (currentCalc) {
+        const {
+        calculator,
+        canBeEdited,
+        closingDate,
+        deviceAddress,
+        diameter,
+        futureCheckingDate,
+        futureCommercialAccountingDate,
+        housingStockId,
+        id,
+        ipV4,
+        lastCheckingDate,
+        lastCommercialAccountingDate,
+        model,
+        port,
+        resource,
+        serialNumber,
+        type,
+        underTransaction,
+      } = currentCalc;
+
+   const currentInfoId = _.find(items, { label: model || 'ТЭМ-106' });
+
+    const { value } = currentInfoId;
 
 
-    async function getDevice(url = '') {
-      try {
-        const res = await axios.get(`MeteringDevices/${url}`);
-        return res;
-      } catch (error) {
-        console.log(error);
-        throw {
-          resource: 'device',
-          message: 'Произошла ошибка запроса устройства',
-        };
-      }
-    }
-      const initialStateDefaultValues = {
+          const initialStateDefaultValues = {
           serialNumber,
           checkingDate: lastCommercialAccountingDate,
           futureCheckingDate,
@@ -130,19 +107,13 @@ const EditCalculator = () => {
           },
           futureCommercialAccountingDate,
           housingStockId,
-          infoId: Number(value),
-          // infoId: Number((_.find(items, {'label': 'ВКТ-7'})).value)
-      };
-
-
-
-    getDevice(deviceId).then(() => {
+          infoId: Number(value)
+      }
       dispatch(
         setAddCalculatorForm(calculatorPage, initialStateDefaultValues),
-      );
-    });
-
-
+      )
+  }
+  
   }, [currentCalc]);
 
   function handleChangeTab(value) {
@@ -189,14 +160,14 @@ const EditCalculator = () => {
 
   const buttonHandler = () => {
     console.log('test');
-    console.log(serialNumber);
+   
   };
 
   const handleSubmit = async () => {
     alert('Cейчас будем отправлять данные!');
     try {
-      // const res = await axios.post('Calculators', calculatorPage);
-      // alert('Вычислитель успешно создан !');
+      const res = await axios.put(`Calculators/${deviceId}`, calculatorPage);
+      alert('Вычислитель успешно создан !');
       // console.log(res);
       // return res;
         console.log(calculatorPage,"calculatorPage")
@@ -212,9 +183,7 @@ const EditCalculator = () => {
   return (
     <AddDeviceContext.Provider value={{}}>
 
-      {/* <Modal id="add-calculator" ref={modalRef}>
-        <ModalWrap>
-          <ModalClose getModal={modalRef} /> */}
+
       <ModalTop>
 
         <Header>ВКТ-7 (123456789). Редактирование</Header>
@@ -234,9 +203,7 @@ const EditCalculator = () => {
         </ButtonTT>
         {renderNextButton()}
         {renderSubmitButton()}
-        {/* </ModalBottom>
-        </ModalWrap> */}
-        {/* </Modal> */}
+
       </ModalBottom>
 
     </AddDeviceContext.Provider>
@@ -245,35 +212,3 @@ const EditCalculator = () => {
 
 export default connect()(EditCalculator);
 
-// const initialState = {
-//   serialNumber: '',
-//   checkingDate: moment().toISOString(),
-//   futureCheckingDate: moment().toISOString(),
-//   lastCommercialAccountingDate: moment().toISOString(),
-//   connection: {
-//     ipV4: '192.168.0.1',
-//     deviceAddress: 0,
-//     port: 1234,
-//   },
-//   futureCommercialAccountingDate: moment().toISOString(),
-//   housingStockId: Number(objid),
-//   infoId: 1,
-// };
-
-// const initialStateFullfill = [
-//   { id: ['serialNumber'], value: 'serialNumber' },
-//   { id: ['housingStockId'], value: Number(objid) },
-//   { id: ['infoId'], value: 1 },
-//   { id: ['lastCommercialAccountingDate'], value: moment().toISOString() },
-//   { id: ['checkingDate'], value: moment().toISOString() },
-//   { id: ['futureCheckingDate'], value: moment().toISOString() },
-//   { id: ['connection', 'ipV4'], value: '192.168.1.1' },
-//   { id: ['connection', 'deviceAddress'], value: 0 },
-//   { id: ['connection', 'port'], value: 1 },
-//   {
-//     id: ['futureCommercialAccountingDate'],
-//     value: moment()
-//       .add(4, 'year')
-//       .toISOString(),
-//   },
-// ];
