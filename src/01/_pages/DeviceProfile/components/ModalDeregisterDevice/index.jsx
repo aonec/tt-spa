39840fd0d2ad27efb45b568c396af58f6171950e
Marat <maratismodest@gmail.com>
2020-10-12 16:08:34 +0300

    formik

--- src/01/_pages/DeviceProfile/components/ModalDeregisterDevice/index.jsx
+++ src/01/_pages/DeviceProfile/components/ModalDeregisterDevice/index.jsx
@@ -1,11 +1,69 @@
 import React, { useEffect } from 'react';
 import 'antd/dist/antd.css';
 import { Modal } from 'antd';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import { setModalDeregisterVisible } from '../../../../Redux/actions/actions';
+import { Formik } from 'formik';
 
 const ModalDeregisterDevice = () => {
-
+  const Basic = () => (
+    <div>
+      <h1>Anywhere in your app!</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
   const visible = useSelector((state) => state.deviceDeregisterReducer.visible);
   const dispatch = useDispatch();
 
@@ -17,9 +75,9 @@ const ModalDeregisterDevice = () => {
     dispatch(setModalDeregisterVisible(['visible'], false));
   };
 
-  useEffect(()=>{
+  useEffect(() => {
 
-  },[])
+  }, []);
 
   return (
     <Modal
@@ -30,9 +88,7 @@ const ModalDeregisterDevice = () => {
       cancelText="Отмена"
       okText="Сохранить"
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Basic/>
     </Modal>
   );
 };
