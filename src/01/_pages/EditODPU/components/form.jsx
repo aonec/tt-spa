ed8b327950137ec2da298b9bb6bc6141c506af22
Marat <maratismodest@gmail.com>
2020-10-19 11:15:40 +0300

    Update form.jsx

--- src/01/_pages/EditODPU/components/form.jsx
+++ src/01/_pages/EditODPU/components/form.jsx
@@ -84,19 +84,7 @@ const FormEditODPU = (props) => {
     ['housingMeteringDeviceType', 'resource', 'lastCommercialAccountingDate', 'futureCommercialAccountingDate', 'model', 'serialNumber'], ['connection', 'entryNumber', 'hubNumber', 'pipeNumber', 'magistral', 'calculatorId'], [],
   ];
 
-  useEffect(() => {
-    visibleInputs.map((item) => {
-      console.log(item);
-      item.map((someItem) => {
-        console.log(someItem);
-        $(`.${someItem}`).addClass('hide');
-      });
-    });
-
-    visibleInputs[currentTabKey - 1].map((item) => {
-      $(`.${item}`).removeClass('hide');
-    });
-  }, [props]);
+  const VisibleItems = currentTabKey
 
   const Alert = ({ name }) => {
     const touch = _.get(touched, `${name}`);
@@ -113,192 +101,186 @@ const FormEditODPU = (props) => {
     <>
       <form id="formikForm" onSubmit={handleSubmit}>
 
-        <Form.Item label="Выберите тип прибора" className="housingMeteringDeviceType">
-          <Select
-            height="60px"
-            name="housingMeteringDeviceType"
-            onChange={(event) => {
-              values.housingMeteringDeviceType = event;
-              // const value = event;
-              // const path = ['housingMeteringDeviceType'];
-              // dispatch(onChangeDeviceFormValueByPath(path, value));
-            }}
-            options={types}
-            value={values.housingMeteringDeviceType}
-          />
-          <Alert name="closingDateTime" />
-        </Form.Item>
+        <div hidden={!(currentTabKey == 1)}>
+          <Form.Item label="Выберите тип прибора" >
+            <Select
+              name="housingMeteringDeviceType"
+              onChange={(event) => {
+                values.housingMeteringDeviceType = event;
+              }}
+              options={types}
+              value={values.housingMeteringDeviceType}
+            />
+            <Alert name="closingDateTime"/>
+          </Form.Item>
 
-        <Form.Item label="Выберите тип ресурса" className="resource">
-          <Select
-            name="resource"
-            onChange={(event) => {
-              values.resource = event;
-              const value = event;
-              const path = ['resource'];
-              // dispatch(onChangeDeviceFormValueByPath(path, value));
-            }}
-            options={resources}
-            value={values.resource}
+          <Form.Item label="Выберите тип ресурса" >
+            <Select
+              name="resource"
+              onChange={(event) => {
+                values.resource = event;
+                const value = event;
+                const path = ['resource'];
+                // dispatch(onChangeDeviceFormValueByPath(path, value));
+              }}
+              options={resources}
+              value={values.resource}
 
-          />
-        </Form.Item>
+            />
+          </Form.Item>
 
-        <Form.Item label="Выберите модель прибора" className="model">
-          <Input
-            name="model"
-            type="text"
-            onChange={handleChange}
-            value={values.model || model}
-            onBlur={handleBlur}
-          />
-          <Alert name="model" />
-        </Form.Item>
+          <Form.Item label="Выберите модель прибора">
+            <Input
+              name="model"
+              type="text"
+              onChange={handleChange}
+              value={values.model || model}
+              onBlur={handleBlur}
+            />
+            <Alert name="model"/>
+          </Form.Item>
 
-        <Form.Item label="Серийный номер" className="serialNumber">
-          <Input
-            name="serialNumber"
-            type="text"
-            onChange={handleChange}
-            value={values.serialNumber || serialNumber}
-            onBlur={handleBlur}
-          />
-          <Alert name="serialNumber" />
-        </Form.Item>
+          <Form.Item label="Серийный номер" >
+            <Input
+              name="serialNumber"
+              type="text"
+              onChange={handleChange}
+              value={values.serialNumber}
+              onBlur={handleBlur}
+            />
+            <Alert name="serialNumber"/>
+          </Form.Item>
 
 
-
-        <Form.Item label="Дата выпуска прибора" className="lastCommercialAccountingDate">
-          <DatePicker
-            name="lastCommercialAccountingDate"
-            placeholder="Укажите дату..."
-            format="DD.MM.YYYY"
-            value = {moment(values.lastCommercialAccountingDate)}
-            onChange = {(date, dateString) => {values.lastCommercialAccountingDate = dateString}}
-
-            // onChange={(date) => { values.lastCommercialAccountingDate = 55 }}
-            // value={moment(values.lastCommercialAccountingDate)}
-            // value={moment(values.lastCommercialAccountingDate)}
-          />
-          <Alert name="lastCommercialAccountingDate" />
-        </Form.Item>
+          <Form.Item label="Дата выпуска прибора" >
+            <DatePicker
+              name="lastCommercialAccountingDate"
+              placeholder="Укажите дату..."
+              format="DD.MM.YYYY"
+              value={moment(values.lastCommercialAccountingDate)}
+              onChange={(date, dateString) => {
+                values.lastCommercialAccountingDate = dateString
+              }}
+            />
+            <Alert name="lastCommercialAccountingDate"/>
+          </Form.Item>
 
 
+          <Form.Item label="Дата ввода в эксплуатацию">
+            <DatePicker
+              value={moment(futureCheckingDate)}
+              placeholder="Укажите дату..."
+              format="DD.MM.YYYY"
+              onChange={(date) => {
+                const path = ['lastCommercialAccountingDate'];
+                const value = date.toISOString();
 
-        <Form.Item label="Дата ввода в эксплуатацию" className="lastCommercialAccountingDate">
-          <DatePicker
-            value={moment(futureCheckingDate)}
-            placeholder="Укажите дату..."
-            format="DD.MM.YYYY"
-            onChange={(date) => {
-              const path = ['lastCommercialAccountingDate'];
-              const value = date.toISOString();
+                // dispatch(onChangeDeviceFormValueByPath(path, value));
+              }}
+              name="futureCheckingDate"
+            />
+            <Alert name="futureCheckingDate"/>
+          </Form.Item>
 
-              // dispatch(onChangeDeviceFormValueByPath(path, value));
-            }}
-            name="futureCheckingDate"
-          />
-          <Alert name="futureCheckingDate" />
-        </Form.Item>
+          <Form.Item label="Срок эксплуатации по нормативу">
+            <Select
 
-        <Form.Item label="Срок эксплуатации по нормативу" className="futureCommercialAccountingDate">
-          <Select
+              name="futureCommercialAccountingDate"
+              onChange={(event) => {
+                const value = moment()
+                  .add(event, 'year')
+                  .toISOString();
+                const path = ['futureCommercialAccountingDate'];
+                // dispatch(onChangeDeviceFormValueByPath(path, value));
+              }}
+              name="futureCommercialAccountingDate"
+              placeholder="Укажите оперид эксплуатации"
+              options={serviceLife}
+              defaultValue={serviceLife[0].value}
+            />
+            <Alert name="futureCommercialAccountingDate"/>
+          </Form.Item>
+        </div>
 
-            name="futureCommercialAccountingDate"
-            onChange={(event) => {
-              const value = moment()
-                .add(event, 'year')
-                .toISOString();
-              const path = ['futureCommercialAccountingDate'];
-              // dispatch(onChangeDeviceFormValueByPath(path, value));
-            }}
-            name="futureCommercialAccountingDate"
-            placeholder="Укажите оперид эксплуатации"
-            options={serviceLife}
-            defaultValue={serviceLife[0].value}
-          />
-          <Alert name="futureCommercialAccountingDate" />
-        </Form.Item>
 
-        <Form.Item label="Подключение к вычислителю" className="connection">
-          <Select
-            id="connection"
-            name="connection"
-            onChange={(event) => {
-              values.connection = event;
-              const value = event;
-              const path = ['resource'];
-              // dispatch(onChangeDeviceFormValueByPath(path, value));
-            }}
-            options={connection}
-            value={values.connection}
+        <div hidden={!(currentTabKey == 2)}>
+          <Form.Item label="Подключение к вычислителю">
+            <Select
+              id="connection"
+              name="connection"
+              onChange={(event) => {
+                values.connection = event;
+                const value = event;
+                const path = ['resource'];
+                // dispatch(onChangeDeviceFormValueByPath(path, value));
+              }}
+              options={connection}
+              value={values.connection}
 
-          />
-        </Form.Item>
+            />
+          </Form.Item>
 
-        <Form.Item
-          label="Выберите вычислитель, к которому подключен прибор"
-          className="calculatorId"
-        >
-          <Input
-            id="calculatorId"
-            name="calculatorId"
-            type="number"
-            placeholder="Начните вводить ID прибора"
-            // value={calculatorId}
-            onChange={(event) => {
-              const { value } = event.target;
-              const path = ['calculatorId'];
-              // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
-            }}
-          />
-          <Alert name="calculatorId" />
-        </Form.Item>
+          <Form.Item
+            label="Выберите вычислитель, к которому подключен прибор"
+            className="calculatorId"
+          >
+            <Input
+              id="calculatorId"
+              name="calculatorId"
+              type="number"
+              placeholder="Начните вводить ID прибора"
+              // value={calculatorId}
+              onChange={(event) => {
+                const { value } = event.target;
+                const path = ['calculatorId'];
+                // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
+              }}
+            />
+            <Alert name="calculatorId"/>
+          </Form.Item>
 
-        <Form.Item label="Номер ввода" className="entryNumber">
-          <Input
-            id="entryNumber"
-            type="number"
-            placeholder="1"
-            value={entryNumber}
-            onChange={(event) => {
-              const { value } = event.target;
-              const path = ['pipe', 'entryNumber'];
-              // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
-            }}
-          />
-        </Form.Item>
+          <Form.Item label="Номер ввода">
+            <Input
+              id="entryNumber"
+              type="number"
+              placeholder="1"
+              value={entryNumber}
+              onChange={(event) => {
+                const { value } = event.target;
+                const path = ['pipe', 'entryNumber'];
+                // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
+              }}
+            />
+          </Form.Item>
 
-        <Form.Item label="Номер узла" className="hubNumber">
-          <Input
-            id="hubNumber"
-            type="number"
-            placeholder="1"
-            value={hubNumber}
-            onChange={(event) => {
-              const { value } = event.target;
-              const path = ['pipe', 'hubNumber'];
-              // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
-            }}
-          />
-        </Form.Item>
+          <Form.Item label="Номер узла" className="hubNumber">
+            <Input
+              id="hubNumber"
+              type="number"
+              placeholder="1"
+              value={hubNumber}
+              onChange={(event) => {
+                const { value } = event.target;
+                const path = ['pipe', 'hubNumber'];
+                // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
+              }}
+            />
+          </Form.Item>
 
-        <Form.Item label="Номер трубы" className="pipeNumber">
-          <Input
-            id="pipeNumber"
-            type="number"
-            placeholder="1"
-            value={pipeNumber}
-            onChange={(event) => {
-              const { value } = event.target;
-              const path = ['pipe', 'pipeNumber'];
-              // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
-            }}
-          />
-        </Form.Item>
-
-        {/* <Tab1 /> */}
-        {/* <Tab2 /> */}
+          <Form.Item label="Номер трубы">
+            <Input
+              id="pipeNumber"
+              type="number"
+              placeholder="1"
+              value={pipeNumber}
+              onChange={(event) => {
+                const { value } = event.target;
+                const path = ['pipe', 'pipeNumber'];
+                // dispatch(onChangeDeviceFormValueByPath(path, Number(value)));
+              }}
+            />
+          </Form.Item>
+        </div>
 
         <div>
           <ButtonTT
