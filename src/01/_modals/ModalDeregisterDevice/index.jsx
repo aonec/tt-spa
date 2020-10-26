8a0fbc344bcc0ff077c5a03a03315adecbaa4975
Marat <maratismodest@gmail.com>
2020-10-26 12:19:08 +0300

    commit

--- src/01/_modals/ModalDeregisterDevice/index.jsx
+++ src/01/_modals/ModalDeregisterDevice/index.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import { Modal } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import _ from 'lodash';
 import {
   setModalDeregisterVisible,
-} from '../../../../../Redux/actions/actions';
-import { ButtonTT } from '../../../../../tt-components';
+} from '../../Redux/actions/actions';
+import { ButtonTT } from '../../tt-components';
 import DeregisterForm from './DeregisterForm';
+import axios from '../../axios';
 
-const ModalDeregisterDevice = () => {
+async function getHousingMeteringDevices(id = '') {
+  try {
+    // const res = await axios.get(replaceURL(url));
+    const res = await axios.get(`HousingMeteringDevices/${id}`);
+    console.log('res', res);
+    return res;
+  } catch (error) {
+    console.log(error);
+    throw {
+      resource: 'device',
+      message: 'Произошла ошибка запроса устройства',
+    };
+  }
+}
+
+const ModalDeregisterDevice = ({ deviceId }) => {
   const dispatch = useDispatch();
   const visible = useSelector(
     (state) => _.get(state, ['deviceDeregisterReducer', 'visible'], false),
   );
+  const [device, setDevice] = useState();
+
+  useEffect(() => {
+    getHousingMeteringDevices(deviceId).then((res) => {
+      setDevice(res);
+    });
+  }, []);
 
   const handleCancel = () => {
     dispatch(setModalDeregisterVisible(false));
@@ -25,7 +48,7 @@ const ModalDeregisterDevice = () => {
       onCancel={handleCancel}
       footer={null}
     >
-      <DeregisterForm />
+      <DeregisterForm device={device} />
       <ButtonTT
         type="submit"
         color="red"
