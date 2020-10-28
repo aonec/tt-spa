a55b5cadb80801a738aef11bf174077696118506
makeevvd <70840382+makeevvd@users.noreply.github.com>
2020-10-28 12:02:33 +0300

    commit

--- src/01/_pages/DeviceProfile/DeviceProfile.jsx
+++ src/01/_pages/DeviceProfile/DeviceProfile.jsx
@@ -148,7 +148,7 @@ export const DeviceProfile = () => {
     getPagination();
   };
 
-  if (isLoading || typeODPU == undefined) return 'ЗАГРУЗКА...';
+  if (isLoading || typeODPU === undefined) return 'ЗАГРУЗКА...';
 
   if (typeODPU === 'Calculator') {
     return (
