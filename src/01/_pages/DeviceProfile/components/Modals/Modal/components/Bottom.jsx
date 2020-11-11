f9c87e34daeacc24d279a5f3eace565548ef2605
Marat <maratismodest@gmail.com>
2020-11-11 18:54:53 +0300

    commit

--- src/01/_pages/DeviceProfile/components/Modals/Modal/components/Bottom.jsx
+++ src/01/_pages/DeviceProfile/components/Modals/Modal/components/Bottom.jsx
@@ -1,76 +1,56 @@
 import React, { useContext } from 'react';
 import { Radio, DatePicker, Form } from 'antd';
 import { ReportContext } from '../index';
-
-const { RangePicker } = DatePicker;
-
-export const PeriodType = () => {
-  console.log('PeriodType');
-
-  const { onPeriodChange, onDetailChange } = useContext(ReportContext);
-
-  return (
-    <div className="period_and_type ">
-      <div className="type">
-        <Form.Item label="Тип архива">
-          <Radio.Group
-            defaultValue="month"
-            size="large"
-            onChange={(event) => onPeriodChange(event)}
-          >
-            <Radio.Button value="month" checked>
-              Месячный
-            </Radio.Button>
-            <Radio.Button value="day">Суточный</Radio.Button>
-            <Radio.Button value="year">Годовой</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-
-      </div>
-
-      <div className="detail">
-        <Form.Item label="Детализация отчета">
-
-          <Radio.Group
-            defaultValue="hourly"
-            size="large"
-            onChange={(event) => onDetailChange(event)}
-          >
-            <Radio.Button value="daily" checked>
-              Суточная
-            </Radio.Button>
-            <Radio.Button value="hourly">Часовая</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-
-      </div>
-    </div>
-  );
-};
+import { RangePickerTT } from '../../../../../../tt-components/RangePicker';
 
 export const Bottom = () => {
+  console.log('Bottom');
   const {
-    values,
+    onPeriodChange, onDetailChange, values,
     datePickerHandler,
   } = useContext(ReportContext);
 
   return (
     <>
-      <PeriodType />
-      <div className="period">
-        <Form.Item label="Период выгрузки">
-          <RangePicker
-            format="DD.MM.YYYY"
-            allowClear={false}
-            size="48px"
-            value={[values.begin, values.end]}
-            placeholder={['Дата Начала', 'Дата окончания']}
-            onChange={(event) => {
-              datePickerHandler(event);
-            }}
-          />
-        </Form.Item>
-      </div>
+      <Form.Item label="Тип архива">
+        <Radio.Group
+          defaultValue="month"
+          size="large"
+          onChange={(event) => onPeriodChange(event)}
+        >
+          <Radio.Button value="month" checked>
+            Месячный
+          </Radio.Button>
+          <Radio.Button value="day">Суточный</Radio.Button>
+          <Radio.Button value="year">Годовой</Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+
+      <Form.Item label="Детализация отчета">
+        <Radio.Group
+          defaultValue="hourly"
+          size="large"
+          onChange={(event) => onDetailChange(event)}
+        >
+          <Radio.Button value="daily" checked>
+            Суточная
+          </Radio.Button>
+          <Radio.Button value="hourly">Часовая</Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+
+      <Form.Item label="Период выгрузки">
+        <RangePickerTT
+          format="DD.MM.YYYY"
+          allowClear={false}
+          value={[values.begin, values.end]}
+          placeholder={['Дата Начала', 'Дата окончания']}
+          onChange={(event) => {
+            datePickerHandler(event);
+          }}
+        />
+      </Form.Item>
+
     </>
   );
 };
--- src/01/_pages/DeviceProfile/components/Modals/Modal/index.jsx
+++ src/01/_pages/DeviceProfile/components/Modals/Modal/index.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useMemo } from 'react';
 import _ from 'lodash';
 import moment from 'moment';
-import { Modal } from 'antd';
+import {
+  Form, Modal, Radio, Tabs,
+} from 'antd';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { Title, ButtonTT } from '../../../../../tt-components';
+import {
+  Title, ButtonTT, RangePickerTT, InputTT, SelectTT,
+} from '../../../../../tt-components';
 import { convertDateOnly } from '../../../../../_api/utils/convertDate';
-import { Bottom } from './components/Bottom';
-import { Top } from './components/Top';
 import { CalculatorTemplate, Translate } from './components/CalculatorTemplate';
 
-export const ReportContext = React.createContext();
+const { TabPane } = Tabs;
 
 // export const ModalODPU = ({ device }) => {
 export const ModalODPU = () => {
@@ -21,15 +23,72 @@ export const ModalODPU = () => {
   } = device;
   const { housingStockNumber, street, corpus } = address;
 
-  const [type, setType] = useState('');
-  const resources = []; // Список из ресурсов: ХВС, ГВС, Отопление
-  const devicesList = []; // Список всех устройств
-  const selectOptions = [];
+  // Список всех устройств, которые являются расходомерами
+  // const memoizedValue = useMemo(() => computeExpensiveValue(a, b), [a, b]);
+  const devicesList = useMemo((device) => hubs.reduce((result, item) => {
+    const {
+      resource, housingMeteringDeviceType, hub, serialNumber,
+    } = item;
+    const { entryNumber, pipeNumber } = hub;
+    if (housingMeteringDeviceType === 'FlowMeter') {
+      result.push({
+        resource,
+        entryNumber,
+        pipeNumber,
+        housingMeteringDeviceType,
+        serialNumber,
+      });
+    }
+    return result;
+  }, []));
 
-  useEffect(() => {
-    setType(resources[0]);
-    console.log('type', type);
+  console.log('devicesList', devicesList);
+
+  // Список всех типов Расходомеров: ХВС, ГВС, Отопление
+  const resources = useMemo((device) => devicesList.reduce((result, item) => {
+    const { resource } = item;
+    if (!(result.includes(resource))) {
+      result.push(resource);
+    }
+    return result;
+  }, []));
+
+  console.log('resources', resources);
+
+  // Список всех доступных строк для выбора Узла
+  const selectOptions = devicesList.reduce((result, item) => {
+    const {
+      resource, entryNumber, pipeNumber, serialNumber,
+    } = item;
+    const currentSelection = _.find(result, { resource });
+    console.log('resource = ', resource);
+    if (!currentSelection || (resource === 'ColdWaterSupply')) {
+      result.push({
+        key: result.length,
+        label: `Узел ${entryNumber} ${model}: ПРЭМ (${serialNumber})`,
+        resource,
+        entryNumber,
+        pipeNumber,
+        value: result.length,
+      });
+    } else {
+      const ind = result.indexOf(currentSelection);
+      result.splice(ind, 1, {
+        label: `${_.get(
+          result[ind],
+          'label',
+          'default',
+        )} ПРЭМ(${serialNumber})`,
+        resource,
+        value: currentSelection.value,
+        entryNumber,
+        pipeNumber,
+        key: currentSelection.key,
+      });
+    }
+    return result;
   }, []);
+  console.log('selectOptions', selectOptions);
 
   const {
     handleSubmit, handleChange, values, touched, errors,
@@ -37,11 +96,13 @@ export const ModalODPU = () => {
   } = useFormik({
     initialValues: {
       period: 'month',
-      detail: 'hourly',
-      entryNumberRes: null,
-      pipeNumberRes: null,
-      begin: moment().subtract(1, 'month'),
-      end: moment(),
+      detail: 'daily',
+      entryNumberRes: undefined,
+      pipeNumberRes: undefined,
+      begin: moment().subtract(1, 'month').toISOString(),
+      end: moment().toISOString(),
+      currentTab: resources[0],
+      currentSelect: null,
     },
     validationSchema: Yup.object({
       // test: Yup.string().required('Строка не должна быть пустой'),
@@ -55,85 +116,117 @@ export const ModalODPU = () => {
     },
   });
 
-  const datePickerHandler = (event) => {
-    setFieldValue('begin', event[0]);
-    setFieldValue('end', event[1]);
+  const Top = () => {
+    console.log('Top');
+    return (
+      <>
+        <Form.Item label=" Название отчета">
+          <InputTT
+            value={`${street}_${housingStockNumber}.exls`}
+            disabled
+          />
+        </Form.Item>
+
+        <Form.Item label="Выбор узла">
+          <SelectTT
+            options={modifiedSelectOptions}
+            placeholder="Выберите узел"
+            onChange={handleSomeChange}
+          />
+        </Form.Item>
+      </>
+    );
   };
+
+  const ReportTabs = () => {
+    const resList = resources.map((value) => {
+      const res = translate(value);
+      return <TabPane tab={res} key={value} />;
+    });
+
+    return (
+      <Tabs activeKey={values.currentTab} onChange={onTabsChangeHandler}>
+        {resList}
+      </Tabs>
+    );
+  };
+
+  const Bottom = () => {
+    console.log('Bottom');
+
+    const datePickerHandler = (event) => {
+      setFieldValue('begin', event[0].toISOString());
+      setFieldValue('end', event[1].toISOString());
+    };
+
+    const onPeriodChange = (event) => {
+      const res = event.target.value;
+      setFieldValue('period', res);
+      setFieldValue('begin', (moment().subtract(1, res)).toISOString());
+      setFieldValue('end', moment().toISOString());
+    };
+
+    return (
+      <>
+        <Form.Item label="Тип архива">
+          <Radio.Group
+            defaultValue={values.period}
+            size="large"
+            onChange={(event) => onPeriodChange(event)}
+          >
+            <Radio.Button value="month" checked>
+              Месячный
+            </Radio.Button>
+            <Radio.Button value="day">Суточный</Radio.Button>
+            <Radio.Button value="year">Годовой</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item label="Детализация отчета">
+          <Radio.Group
+            defaultValue={values.detail}
+            size="large"
+            onChange={(event) => onDetailChange(event)}
+          >
+            <Radio.Button value="daily" checked>
+              Суточная
+            </Radio.Button>
+            <Radio.Button value="hourly">Часовая</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item label="Период выгрузки">
+          <RangePickerTT
+            format="DD.MM.YYYY"
+            allowClear={false}
+            value={[moment(values.begin), moment(values.end)]}
+            placeholder={['Дата Начала', 'Дата окончания']}
+            onChange={(event) => {
+              datePickerHandler(event);
+            }}
+          />
+        </Form.Item>
+
+      </>
+    );
+  };
+
+  const modifiedSelectOptions = selectOptions.filter((option) => option.resource == values.currentTab);
 
   const onTabsChangeHandler = (resource) => {
-    setType(resource);
+    setFieldValue('currentTab', resource);
+    // setType(resource);
     setFieldValue('entryNumberRes', undefined);
-    // setFieldValue('pipeNumberRes', undefined);
+    setFieldValue('pipeNumberRes', undefined);
   };
 
-  if (hubs) {
-    hubs.map((item) => {
-      const {
-        resource, housingMeteringDeviceType, hub, serialNumber,
-      } = item;
-      const { entryNumber, pipeNumber } = hub;
-      if (housingMeteringDeviceType === 'FlowMeter') {
-        devicesList.push({
-          resource,
-          entryNumber,
-          pipeNumber,
-          housingMeteringDeviceType,
-          serialNumber,
-        });
-      }
-    });
-    console.log('devicesList', devicesList);
-  }
-
-  const onPeriodChange = (event) => {
-    const res = event.target.value;
-    setFieldValue('period', res);
-    setFieldValue('begin', moment().subtract(1, res));
-    setFieldValue('end', moment());
-  };
-
-  devicesList.map(({
-    resource, entryNumber, pipeNumber, serialNumber
-  }) => {
-    const currentSelection = _.find(selectOptions, { resource });
-    if (currentSelection && (resource !== 'ColdWaterSupply')) {
-      const index = selectOptions.indexOf(currentSelection);
-      selectOptions.splice(index, 1, {
-        label: `${_.get(
-          selectOptions[index],
-          'label',
-          'default',
-        )} ПРЭМ(${serialNumber})`,
-        value: selectOptions.length,
-        resource,
-        entryNumber,
-        pipeNumber,
-      });
-    } else {
-      selectOptions.push({
-        label: `Узел ${entryNumber} ${model}: ПРЭМ (${serialNumber})`,
-        value: selectOptions.length,
-        resource,
-        entryNumber,
-        pipeNumber,
-      });
-    }
-  });
-
-
-
-  devicesList.map((item) => {
-    const { resource } = item;
-    if (!(resources.includes(resource))) {
-      resources.push(resource);
-    }
-  });
+  console.log('sources', resources);
 
   const downloadReport = () => {
     if (values.entryNumberRes) {
       const link = `http://84.201.132.164:8080/api/reports/getByResource?deviceId=${id}&reporttype=${
         values.detail
-      }&resourcetype=${type}&entrynumber=${
+      }&resourcetype=${values.currentTab}&entrynumber=${
         values.entryNumberRes
       }&pipenumber=${values.pipeNumberRes}&from=${convertDateOnly(values.begin)}T00:00:00Z&to=${convertDateOnly(
         values.end,
@@ -147,16 +240,16 @@ export const ModalODPU = () => {
   };
 
   function handleSomeChange(value) {
-    console.log("value", value)
-    const b = _.filter(selectOptions, { value});
-    const { entryNumber, pipeNumber } = b[0];
-    setFieldValue('entryNumberRes', entryNumber);
-    setFieldValue('pipeNumberRes', pipeNumber);
-    console.log("NNN", b)
-    console.log("entryNumber", entryNumber)
-    console.log("pipeNumber", pipeNumber)
-    console.log("selectOptions", selectOptions)
-
+    console.log('value', value);
+    console.log(modifiedSelectOptions[value]);
+    const b = _.find(modifiedSelectOptions, { value });
+    console.log("b", b)
+    // // setFieldValue('currentSelect', value);
+    // // const b = _.filter(selectOptions, { value });
+    const { entryNumber, pipeNumber } = b;
+    console.log("entryNumber, pipeNumber", entryNumber, pipeNumber)
+    // setFieldValue('entryNumberRes', entryNumber);
+    // setFieldValue('pipeNumberRes', pipeNumber);
   }
 
   function onDetailChange(e) {
@@ -164,60 +257,43 @@ export const ModalODPU = () => {
     setFieldValue('detail', res);
   }
 
-  const context = {
-    values,
-    datePickerHandler,
-    devicesList,
-    translate,
-    onTabsChangeHandler,
-    model,
-    street,
-    housingStockNumber,
-    type,
-    selectOptions,
-    handleSomeChange,
-    onPeriodChange,
-    onDetailChange,
-    resources,
+  const buttonHandler = () => {
+    console.log(values);
   };
 
   return (
-    <ReportContext.Provider
-      value={context}
+    <Modal
+      visible
+      width="800px"
+      footer={null}
     >
-      <Modal
-        visible
-        width="800px"
-        footer={null}
-      >
-        <form onSubmit={handleSubmit}>
+      <ButtonTT onClick={buttonHandler}>ButtonTT</ButtonTT>
+      <form onSubmit={handleSubmit}>
 
-          {/* <div>{JSON.stringify(devicesList)}</div> */}
+        <div className="modal__top">
+          <Title color="black">
+            {`Выгрузка отчета о общедомовом потреблении ${model} (${serialNumber})`}
+          </Title>
+          <ReportTabs />
+          <Top />
+          <Bottom />
+        </div>
 
-          <div className="modal__top">
-            <Title color="black">
-              {`Выгрузка отчета о общедомовом потреблении ${model} (${serialNumber})`}
-            </Title>
-            <Top />
-            <Bottom />
-          </div>
-
-          <div className="modal__bottom">
-            <ButtonTT
-              color="white"
-            >
-              Отмена
-            </ButtonTT>
-            <ButtonTT
-              color="blue"
-              onClick={downloadReport}
-            >
-              Выгрузить
-            </ButtonTT>
-          </div>
-        </form>
-      </Modal>
-    </ReportContext.Provider>
+        <div className="modal__bottom">
+          <ButtonTT
+            color="white"
+          >
+            Отмена
+          </ButtonTT>
+          <ButtonTT
+            color="blue"
+            onClick={downloadReport}
+          >
+            Выгрузить
+          </ButtonTT>
+        </div>
+      </form>
+    </Modal>
   );
 };
 
