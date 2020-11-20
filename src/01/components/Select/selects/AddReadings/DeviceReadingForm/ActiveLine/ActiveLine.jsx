88d2473cc9c76c62673ed43aac7355106fd3bf03
makeevvd <70840382+makeevvd@users.noreply.github.com>
2020-11-20 11:50:05 +0300

    improve layout

--- src/01/components/Select/selects/AddReadings/DeviceReadingForm/ActiveLine/ActiveLine.jsx
+++ src/01/components/Select/selects/AddReadings/DeviceReadingForm/ActiveLine/ActiveLine.jsx
@@ -5,12 +5,12 @@ const ActiveLine = ({isActive}) => {
 
 
     return (
-        <>
+        <div style={{marginRight: 16, color: 'rgba(39, 47, 90, 0.8)'}}>
         {isActive
             ? <span> <Badge status="success" /> Активен </span>
             : <span> <Badge status="error" /> Не активен </span>
         }
-        </>
+        </div>
 )
 }
 
