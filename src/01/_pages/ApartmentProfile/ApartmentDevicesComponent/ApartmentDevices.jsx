5560da2da2f599af5ea4f77142cc74b3f6a3a006
Marat <maratismodest@gmail.com>
2020-11-24 11:53:49 +0300

    commit

--- src/01/_pages/ApartmentProfile/ApartmentDevicesComponent/ApartmentDevices.jsx
+++ src/01/_pages/ApartmentProfile/ApartmentDevicesComponent/ApartmentDevices.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { getApartmentDevices } from '01/_api/apartment_page';
 import { Header } from './components/Header';
 import { ApartmentDevicesList } from './components/ApartmentDevicesList';
 import { ShowHidden } from './components/ShowHidden';
@@ -9,23 +8,16 @@ export const ApartmentDevicesContext = React.createContext();
 
 export const ApartmentDevices = (props) => {
   const params = useParams();
-  const apartmentId = params[1];
-  const {devices} = props
-  // const [devices, setDevices] = useState();
+  const { devices } = props;
 
-  // useEffect(() => {
-  //   async function getDevicesWrap() {
-  //     await getApartmentDevices(apartmentId).then((response) => setDevices(response));
-  //   }
-  //   getDevicesWrap();
-  //   // console.log('useEffect');
-  // }, []);
-
-  // if (!devices) {
-  //   return <div>Загрузка</div>
-  // }
-  const {items} = devices;
-
+  const { items } = devices;
+  if (!items) {
+    return (
+      <div>
+        Что-то пошло не так. Попробуйте выйти из сессии и зайти заново.
+      </div>
+    );
+  }
   return (
     <>
       <ApartmentDevicesContext.Provider value={Object.values(items)}>
