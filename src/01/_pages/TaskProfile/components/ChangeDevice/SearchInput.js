5242e6959765bd0ee71e22380be137c69da54cc6
Marat <maratismodest@gmail.com>
2020-11-27 12:23:35 +0300

    commit

---
+++ src/01/_pages/TaskProfile/components/ChangeDevice/SearchInput.js
@@ -0,0 +1,67 @@
+import React, { useContext, useState } from 'react';
+import { AutoComplete } from 'antd';
+import { ChangeDeviceContext } from './index';
+
+const Complete = () => {
+  const { device, devices } = useContext(ChangeDeviceContext);
+  console.log(device, devices);
+
+  const availableDevices = devices.reduce((result, item) => {
+    const {
+      id, type, serialNumber, model,
+    } = item;
+    if (device.calculator === null && type === 'Calculator') {
+      result.push({
+        value: id,
+        label: `${model}: ${serialNumber}`,
+      });
+    }
+
+    if (device.calculator !== null && type === 'Housing') {
+      result.push({
+        value: id,
+        label: `${model}: ${serialNumber}`,
+      });
+    }
+
+    return result;
+  }, []);
+
+  const [value, setValue] = useState('');
+  const [id, setId] = useState();
+  const [options, setOptions] = useState([]);
+
+  const onSelect = (data, item) => {
+    console.log('onSelect', data, item);
+    setValue(item.label);
+    setId(item.value);
+  };
+  const onChange = (data) => {
+    setValue(data);
+    const devicesList = availableDevices.reduce((result, item) => {
+      const { value, label } = item;
+      if (label.includes(data)) {
+        result.push(item);
+      }
+      return result;
+    }, []);
+    setOptions(devicesList);
+  };
+
+  return (
+    <>
+      <AutoComplete
+        value={value}
+        options={options}
+        style={{
+          width: 200,
+        }}
+        onSelect={onSelect}
+        onChange={onChange}
+        placeholder="control mode"
+      />
+    </>
+  );
+};
+
+export default Complete;
--- src/01/_pages/TaskProfile/components/ChangeDevice/index.js
+++ src/01/_pages/TaskProfile/components/ChangeDevice/index.js
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import ChangeDeviceForm from './ChangeDeviceForm'
-import {getClosedDevices} from "./apiChangeDevice";
-import ButtonTT from "../../../../tt-components/ButtonTT";
+import React, { createContext, useEffect, useState } from 'react';
+import ChangeDeviceForm from './ChangeDeviceForm';
+import Complete from './SearchInput';
+import { getClosedDevices } from './apiChangeDevice';
+import { Header , ButtonTT} from '../../../../tt-components';
 
+export const ChangeDeviceContext = createContext();
 const ChangeDevice = (props) => {
-  const {device} = props;
+  const { device } = props;
   console.log('ChangeDevice');
 
-  const [devices, setDevices] = useState()
-  useEffect(()=>{
-    getClosedDevices().then((res)=>{
-      setDevices(res)
-    })
+  const [devices, setDevices] = useState();
+  useEffect(() => {
+    getClosedDevices().then((res) => {
+      setDevices(res);
+    });
+  }, []);
 
-  },[])
-
-  const handleButton = () =>{
-    console.log("handleButton")
-    console.log(devices)
-    console.log(device)
+  if (!devices) {
+    return <div>Загрузка</div>
   }
 
+  const handleButton = () => {
+    console.log('handleButton');
+    console.log(devices);
+    console.log(device);
+  };
+  const context = { device, devices };
+
   return (
-    <div>ChangeDevice
-      <ButtonTT onClick={handleButton}>handleButton</ButtonTT>
-    {/*<ChangeDeviceForm />*/}
-    </div>
+    <ChangeDeviceContext.Provider value={context}>
+      <div>
+        <Header>
+          ChangeDevice
+        </Header>
+        <Complete />
+        <ButtonTT onClick={handleButton}>handleButton</ButtonTT>
+
+        {/* <ChangeDeviceForm /> */}
+      </div>
+    </ChangeDeviceContext.Provider>
   );
 };
 
