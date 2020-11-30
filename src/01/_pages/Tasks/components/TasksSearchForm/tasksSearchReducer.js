b43155adf558a6757069ea7699340f7dc8e6330b
makeevvd <70840382+makeevvd@users.noreply.github.com>
2020-11-30 13:51:04 +0300

    addTaskIdSearch

--- src/01/_pages/Tasks/components/TasksSearchForm/tasksSearchReducer.js
+++ src/01/_pages/Tasks/components/TasksSearchForm/tasksSearchReducer.js
@@ -1,12 +1,16 @@
 const SET_SEARCHTERM = 'SET_SEARCHTERM';
 const SET_DEVICES_FILTER = 'SET_DEVICES_FILTER';
 const SET_TASKS_TYPE = 'SET_TASKS_TYPE';
+const SET_TASK_ID = 'SET_TASK_ID';
 
 const tasksSearchReducer = (state, action) => {
   console.log("action", action)
   switch (action.type) {
     case SET_SEARCHTERM:
       return { ...state, searchTerm: action.searchTerm };
+
+    case SET_TASK_ID:
+      return { ...state, taskId: action.taskId}
 
     case SET_TASKS_TYPE:
       return { ...state, taskTypeNumber: action.taskTypeNumber };
@@ -18,6 +22,7 @@ const tasksSearchReducer = (state, action) => {
 
 export const setSearchTerm = (searchTerm) => ({ type: SET_SEARCHTERM, searchTerm });
 export const setTaskType = (taskTypeNumber) => ({ type: SET_TASKS_TYPE, taskTypeNumber });
+export const setTaskId = (taskId) => ({ type: SET_TASK_ID, taskId });
 export const setDevicesFilter = (filterParameter) => ({ type: SET_DEVICES_FILTER, filterParameter });
 
 export default tasksSearchReducer;
