f580476e50f1d7b9e7d5d0d10acdef56a2955e7f
Marat <maratismodest@gmail.com>
2020-12-08 11:56:56 +0300

    commit

--- src/01/tt-components/localBases.js
+++ src/01/tt-components/localBases.js
@@ -137,4 +137,41 @@ export const UserRoles = [
   },
 ];
 
+export const DEFAULT_BUILDING = {
+  city: null,
+  street: null,
+  housingStockNumber: null,
+};
+
+export const DEFAULT_DEVICE = {
+  model: null,
+  diameter: null,
+  serialNumber: null,
+  resource: null,
+  commercialAccountingDate: null,
+  futureCheckingDate: null,
+  lastCheckingDate: null,
+};
+
+export const DEFAULT_ICON = {
+  icon: 'device',
+  color: 'initial',
+};
+
+export const periodList = [
+  { label: 'Месячный', value: 'month' },
+  { label: 'Суточный', value: 'day' },
+  { label: 'Годовой', value: 'year' },
+];
+
+export const detailList = [
+  { label: 'Суточный', value: 'daily' },
+  { label: 'Часовой', value: 'hourly' },
+];
+
+export const typelList = [
+  { label: 'Холодная вода', value: 'coldwatersupply' },
+  { label: 'Горячая вода', value: 'heat' },
+];
+
 export const phoneRegExp = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
