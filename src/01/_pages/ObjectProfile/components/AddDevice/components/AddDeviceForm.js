59c6e74f92a9f0ac97d11349fdb0b51f1fe191e1
Marat <maratismodest@gmail.com>
2020-12-26 12:11:59 +0300

    Update AddDeviceForm.js

--- src/01/_pages/ObjectProfile/components/AddDevice/components/AddDeviceForm.js
+++ src/01/_pages/ObjectProfile/components/AddDevice/components/AddDeviceForm.js
@@ -12,7 +12,19 @@ import {
   Title, SelectTT, InputTT, DatePickerTT, StyledModalBody, ButtonTT, StyledFooter,
 } from '../../../../../tt-components';
 import { addOdpu } from '../apiAddOdpu';
-import TabsComponent from "./Main";
+import TabsComponent from './Main';
+
+
+const Warning = (props) => {
+  console.log('warning');
+  console.log(props)
+
+  return (
+    <div>
+      Для данного узла не предусмотрено наличие термодатчика. Проверьте выбранный ресурс
+    </div>
+  );
+};
 
 const styles = {
   w49: {
@@ -30,7 +42,7 @@ const StyledFormPage = styled.div`
   `;
 
 const AddDeviceForm = (props) => {
-  const { calculators, setAddOdpu} = props;
+  const { calculators, setAddOdpu } = props;
   const [currentTabKey, setTab] = useState('1');
 
   function handleCancel() {
@@ -47,9 +59,7 @@ const AddDeviceForm = (props) => {
 
   const [disable, setDisable] = useState(false);
   const [state, setState] = useState('FlowMeter');
-  const [coldandthermo, setColdandthermo] = useState(false)
-
-
+  const [coldandthermo, setColdandthermo] = useState(false);
 
   const validationSchemaFlowMeter = Yup.object({
     model: Yup.string().min(3, 'Модель должна быть длиннее трех символов').required('Введите модель'),
@@ -157,12 +167,9 @@ const AddDeviceForm = (props) => {
     console.log(values);
     if (values.resource === 'ColdWaterSupply' && values.housingMeteringDeviceType === 'TemperatureSensor') {
       // alert('Для данного узла не предусмотрено наличие термодатчика. Проверьте выбранный ресурс');
-      setColdandthermo(true)
-
-    }
-    else setColdandthermo(false)
+      setColdandthermo(true);
+    } else setColdandthermo(false);
   }, [values.resource, values.housingMeteringDeviceType]);
-
 
   const Buttons = () => {
     const NextOkButton = () => {
@@ -179,22 +186,21 @@ const AddDeviceForm = (props) => {
           </ButtonTT>
         );
       }
-      else {
-        return (
-          <ButtonTT
-            color="blue"
-            onClick={handleNext}
-            big
-            disabled={coldandthermo}
-          >
-            Далее
-          </ButtonTT>
-        );
-      }
+
+      return (
+        <ButtonTT
+          color="blue"
+          onClick={handleNext}
+          big
+          disabled={coldandthermo}
+        >
+          Далее
+        </ButtonTT>
+      );
     };
 
     const CancelButton = () => (
-      <ButtonTT type='button' color="white" onClick={handleCancel} style={{ marginLeft: '16px' }}>
+      <ButtonTT type="button" color="white" onClick={handleCancel} style={{ marginLeft: '16px' }}>
         Отмена
       </ButtonTT>
     );
@@ -215,224 +221,224 @@ const AddDeviceForm = (props) => {
       style={{ display: 'flex', flexDirection: 'column' }}
     >
       <StyledModalBody>
-      <Title size="middle" color="black">
-        Добавление нового ОДПУ
-      </Title>
+        <Title size="middle" color="black">
+          Добавление нового ОДПУ
+        </Title>
         <div hidden={!coldandthermo}>Для данного узла не предусмотрено наличие термодатчика. Проверьте выбранный ресурс</div>
-      <TabsComponent
-        currentTabKey={currentTabKey}
-        handleChangeTab={handleChangeTab}
-      />
-      <StyledFormPage hidden={Number(currentTabKey) !== 1}>
-        <Form.Item label="Выберите тип прибора" style={styles.w100}>
-          <SelectTT
-            name="housingMeteringDeviceType"
-            onChange={(value) => {
-              setFieldValue('housingMeteringDeviceType', value);
-              setState(value);
-            }}
-            options={housingMeteringDeviceTypes}
-            value={values.housingMeteringDeviceType}
-          />
-          <Alert name="housingMeteringDeviceType" />
-        </Form.Item>
-
-        <Form.Item label="Выберите тип ресурса" style={styles.w100}>
-          <SelectTT
-            name="resource"
-            onChange={(value) => {
-              setFieldValue('resource', value);
-            }}
-            options={resources}
-            defaultValue={resources[0].value}
-            value={values.resource}
-          />
-          <Alert name="resource" />
-        </Form.Item>
-
-        <Form.Item label="Выберите модель прибора" style={styles.w49}>
-          <InputTT
-            name="model"
-            type="text"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.model}
-          />
-          <Alert name="model" />
-        </Form.Item>
-
-        <Form.Item label="Серийный номер" style={styles.w49}>
-          <InputTT
-            name="serialNumber"
-            type="text"
-            onBlur={handleBlur}
-            onChange={handleChange}
-            value={values.serialNumber}
-          />
-          <Alert name="serialNumber" />
-        </Form.Item>
-
-        {(state === 'FlowMeter') ? (
-          <Form.Item label="Диаметр трубы (мм)" style={styles.w100}>
-            <InputTT
-              name="diameter"
-              placeholder="Укажите диаметр трубы в мм"
-              type="number"
-              onChange={handleChange}
-              value={values.diameter}
-              onBlur={handleBlur}
+        <TabsComponent
+          currentTabKey={currentTabKey}
+          handleChangeTab={handleChangeTab}
+        />
+        <StyledFormPage hidden={Number(currentTabKey) !== 1}>
+          <Form.Item label="Выберите тип прибора" style={styles.w100}>
+            <SelectTT
+              name="housingMeteringDeviceType"
+              onChange={(value) => {
+                setFieldValue('housingMeteringDeviceType', value);
+                setState(value);
+              }}
+              options={housingMeteringDeviceTypes}
+              value={values.housingMeteringDeviceType}
             />
-            <Alert name="diameter" />
+            <Alert name="housingMeteringDeviceType" />
           </Form.Item>
-        ) : null}
 
-        <Form.Item label="Дата поверки" style={styles.w49}>
-          <DatePickerTT
-            format="DD.MM.YYYY"
-            name="lastCheckingDate"
-            placeholder="Укажите дату..."
-            allowClear={false}
-            onChange={(date) => {
-              setFieldValue('lastCheckingDate', date.toISOString());
-            }}
-            value={moment(values.lastCheckingDate)}
-          />
-        </Form.Item>
+          <Form.Item label="Выберите тип ресурса" style={styles.w100}>
+            <SelectTT
+              name="resource"
+              onChange={(value) => {
+                setFieldValue('resource', value);
+              }}
+              options={resources}
+              defaultValue={resources[0].value}
+              value={values.resource}
+            />
+            <Alert name="resource" />
+          </Form.Item>
 
-        <Form.Item label="Дата следующей поверки" style={styles.w49}>
-          <DatePickerTT
-            format="DD.MM.YYYY"
-            name="futureCheckingDate"
-            placeholder="Укажите дату..."
-            allowClear={false}
-            onChange={(date) => {
-              setFieldValue('futureCheckingDate', date.toISOString());
-            }}
-            value={moment(values.futureCheckingDate)}
-          />
-        </Form.Item>
+          <Form.Item label="Выберите модель прибора" style={styles.w49}>
+            <InputTT
+              name="model"
+              type="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.model}
+            />
+            <Alert name="model" />
+          </Form.Item>
 
-        <Form.Item label="Дата начала Акта действия допуска" style={styles.w49}>
-          <DatePickerTT
-            format="DD.MM.YYYY"
-            name="lastCommercialAccountingDate"
-            placeholder="Укажите дату..."
-            allowClear={false}
-            onChange={(date) => {
-              setFieldValue('lastCommercialAccountingDate', date.toISOString());
-            }}
-            value={moment(values.lastCommercialAccountingDate)}
-          />
-        </Form.Item>
+          <Form.Item label="Серийный номер" style={styles.w49}>
+            <InputTT
+              name="serialNumber"
+              type="text"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.serialNumber}
+            />
+            <Alert name="serialNumber" />
+          </Form.Item>
 
-        <Form.Item label="Дата окончания Акта действия допуска" style={styles.w49}>
-          <DatePickerTT
-            format="DD.MM.YYYY"
-            name="futureCommercialAccountingDate"
-            placeholder="Укажите дату..."
-            allowClear={false}
-            onChange={(date) => {
-              setFieldValue('futureCommercialAccountingDate', date.toISOString());
-            }}
-            value={moment(values.futureCommercialAccountingDate)}
-          />
-        </Form.Item>
-      </StyledFormPage>
+          {(state === 'FlowMeter') ? (
+            <Form.Item label="Диаметр трубы (мм)" style={styles.w100}>
+              <InputTT
+                name="diameter"
+                placeholder="Укажите диаметр трубы в мм"
+                type="number"
+                onChange={handleChange}
+                value={values.diameter}
+                onBlur={handleBlur}
+              />
+              <Alert name="diameter" />
+            </Form.Item>
+          ) : null}
 
-      {/* Second Tabs */}
-      <StyledFormPage hidden={Number(currentTabKey) !== 2}>
-        <Form.Item label="Подключение к вычислителю" style={styles.w100}>
-          <SelectTT
-            name="isConnected"
-            onChange={(item) => {
-              (item === false) ? setDisable(true) : setDisable(false);
-              setFieldValue('isConnected', item);
-            }}
-            placeholder="Подключение к вычислителю"
-            options={isConnected}
-            value={values.isConnected}
-            disabled
-          />
-        </Form.Item>
+          <Form.Item label="Дата поверки" style={styles.w49}>
+            <DatePickerTT
+              format="DD.MM.YYYY"
+              name="lastCheckingDate"
+              placeholder="Укажите дату..."
+              allowClear={false}
+              onChange={(date) => {
+                setFieldValue('lastCheckingDate', date.toISOString());
+              }}
+              value={moment(values.lastCheckingDate)}
+            />
+          </Form.Item>
 
-        <Form.Item
-          label="Выберите вычислитель, к которому подключен прибор"
-          style={styles.w100}
-        >
-          <SelectTT
-            name="calculatorId"
-            type="text"
-            onBlur={handleBlur}
-            placeholder="Начните вводить серийный номер или IP адрес прибора"
-            onChange={(value) => {
-              setFieldValue('calculatorId', value);
-            }}
-            options={calculators}
-            value={values.calculatorId}
-            disabled={disable}
-          />
-          <Alert name="calculatorId" />
-        </Form.Item>
+          <Form.Item label="Дата следующей поверки" style={styles.w49}>
+            <DatePickerTT
+              format="DD.MM.YYYY"
+              name="futureCheckingDate"
+              placeholder="Укажите дату..."
+              allowClear={false}
+              onChange={(date) => {
+                setFieldValue('futureCheckingDate', date.toISOString());
+              }}
+              value={moment(values.futureCheckingDate)}
+            />
+          </Form.Item>
 
-        <Form.Item label="Номер ввода" style={styles.w49}>
-          <InputTT
-            name="entryNumber"
-            type="number"
-            onBlur={handleBlur}
-            placeholder="Номер ввода"
-            value={values.entryNumber}
-            onChange={handleChange}
-            disabled={disable}
-          />
-          <Alert name="entryNumber" />
-        </Form.Item>
+          <Form.Item label="Дата начала Акта действия допуска" style={styles.w49}>
+            <DatePickerTT
+              format="DD.MM.YYYY"
+              name="lastCommercialAccountingDate"
+              placeholder="Укажите дату..."
+              allowClear={false}
+              onChange={(date) => {
+                setFieldValue('lastCommercialAccountingDate', date.toISOString());
+              }}
+              value={moment(values.lastCommercialAccountingDate)}
+            />
+          </Form.Item>
 
-        <Form.Item label="Номер узла" style={styles.w49}>
-          <InputTT
-            name="hubNumber"
-            type="number"
-            placeholder="Номер узла"
-            onBlur={handleBlur}
-            value={values.hubNumber}
-            onChange={handleChange}
-            disabled={disable}
-          />
-          <Alert name="hubNumber" />
-        </Form.Item>
+          <Form.Item label="Дата окончания Акта действия допуска" style={styles.w49}>
+            <DatePickerTT
+              format="DD.MM.YYYY"
+              name="futureCommercialAccountingDate"
+              placeholder="Укажите дату..."
+              allowClear={false}
+              onChange={(date) => {
+                setFieldValue('futureCommercialAccountingDate', date.toISOString());
+              }}
+              value={moment(values.futureCommercialAccountingDate)}
+            />
+          </Form.Item>
+        </StyledFormPage>
 
-        <Form.Item label="Номер трубы" style={styles.w49}>
-          <InputTT
-            name="pipeNumber"
-            type="number"
-            min="0"
-            step="1"
-            placeholder="Номер трубы"
-            value={values.pipeNumber}
-            onBlur={handleBlur}
-            onChange={handleChange}
-            disabled={disable}
-          />
-          <Alert name="pipeNumber" />
-        </Form.Item>
+        {/* Second Tabs */}
+        <StyledFormPage hidden={Number(currentTabKey) !== 2}>
+          <Form.Item label="Подключение к вычислителю" style={styles.w100}>
+            <SelectTT
+              name="isConnected"
+              onChange={(item) => {
+                (item === false) ? setDisable(true) : setDisable(false);
+                setFieldValue('isConnected', item);
+              }}
+              placeholder="Подключение к вычислителю"
+              options={isConnected}
+              value={values.isConnected}
+              disabled
+            />
+          </Form.Item>
 
-        <Form.Item name="text" label="Выберите направление магистрали" style={styles.w49}>
-          <SelectTT
-            placeholder="Выберите направление магистрали"
-            name="magistral"
-            options={magistrals}
-            onChange={(value) => {
-              setFieldValue('magistral', value);
-            }}
-            value={values.magistral}
-          />
-          <Alert name="magistral" />
-        </Form.Item>
+          <Form.Item
+            label="Выберите вычислитель, к которому подключен прибор"
+            style={styles.w100}
+          >
+            <SelectTT
+              name="calculatorId"
+              type="text"
+              onBlur={handleBlur}
+              placeholder="Начните вводить серийный номер или IP адрес прибора"
+              onChange={(value) => {
+                setFieldValue('calculatorId', value);
+              }}
+              options={calculators}
+              value={values.calculatorId}
+              disabled={disable}
+            />
+            <Alert name="calculatorId" />
+          </Form.Item>
 
-      </StyledFormPage>
+          <Form.Item label="Номер ввода" style={styles.w49}>
+            <InputTT
+              name="entryNumber"
+              type="number"
+              onBlur={handleBlur}
+              placeholder="Номер ввода"
+              value={values.entryNumber}
+              onChange={handleChange}
+              disabled={disable}
+            />
+            <Alert name="entryNumber" />
+          </Form.Item>
 
-      <StyledFormPage hidden={Number(currentTabKey) !== 3}>
-        <Title color="black">Компонент в разработке</Title>
-      </StyledFormPage>
+          <Form.Item label="Номер узла" style={styles.w49}>
+            <InputTT
+              name="hubNumber"
+              type="number"
+              placeholder="Номер узла"
+              onBlur={handleBlur}
+              value={values.hubNumber}
+              onChange={handleChange}
+              disabled={disable}
+            />
+            <Alert name="hubNumber" />
+          </Form.Item>
+
+          <Form.Item label="Номер трубы" style={styles.w49}>
+            <InputTT
+              name="pipeNumber"
+              type="number"
+              min="0"
+              step="1"
+              placeholder="Номер трубы"
+              value={values.pipeNumber}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              disabled={disable}
+            />
+            <Alert name="pipeNumber" />
+          </Form.Item>
+
+          <Form.Item name="text" label="Выберите направление магистрали" style={styles.w49}>
+            <SelectTT
+              placeholder="Выберите направление магистрали"
+              name="magistral"
+              options={magistrals}
+              onChange={(value) => {
+                setFieldValue('magistral', value);
+              }}
+              value={values.magistral}
+            />
+            <Alert name="magistral" />
+          </Form.Item>
+
+        </StyledFormPage>
+
+        <StyledFormPage hidden={Number(currentTabKey) !== 3}>
+          <Title color="black">Компонент в разработке</Title>
+        </StyledFormPage>
       </StyledModalBody>
       <Buttons />
     </form>
