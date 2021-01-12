ff8aaaba73fcfab55b70be53fbf225e5a6831988
Marat <maratismodest@gmail.com>
2021-01-12 17:37:29 +0300

    Update index.js

--- src/01/_pages/TaskProfile/components/Panel/index.js
+++ src/01/_pages/TaskProfile/components/Panel/index.js
@@ -6,12 +6,11 @@ import { UploadButton, useUpload, UploadList } from '01/components/Upload';
 import { Form, Upload } from 'antd';
 
 import * as s from '01/r_comp';
+import TextArea from 'antd/es/input/TextArea';
 import AddDate from '../../../../components/Select/selects/AddDate';
 import AddReadings from '../../../../components/Select/selects/AddReadings/AddReadings';
 import { addReadings } from '../../hooks/usePanel';
 import ChangeDevice from '../ChangeDevice';
-import { ButtonTT, InputTT } from '../../../../tt-components';
-import TextArea from "antd/es/input/TextArea";
 
 const styles = css`
   panel {
@@ -169,83 +168,81 @@ export const Panel = ({
   const addReadingsDone = stages.items[2]?.name === 'Ввод показаний' && Completion;
 
   const { emailNotify = {} } = state;
+  if (!isObserver) {
+    return styled(styles)(
+      <panel
+        {...use({
+          // one: AddPerpetrator && EmailNotify,
+          two: AddDocuments,
+          tree: (Switch && AddPerpetrator) || SetNextStageDeadline,
+          four: Completion,
+          five: Switch && PushButton,
+          six: UploadReadings || addReadingsDone,
+          seven: AddPerpetrator && EmailNotify,
+          // seven: SwitchDevices && ChangeDevice,
+        })}
+      >
+        {/* {(SwitchDevices && !isObserver) && <ChangeDevice device={device} state={state} />} */}
 
-  return styled(styles)(
-    // <Route path="/tasks/(\\d+)" exact>
-    <panel
-      {...use({
-        // one: AddPerpetrator && EmailNotify,
-        two: AddDocuments,
-        tree: (Switch && AddPerpetrator) || SetNextStageDeadline,
-        four: Completion,
-        five: Switch && PushButton,
-        six: UploadReadings || addReadingsDone,
-        seven: AddPerpetrator && EmailNotify,
-        // seven: SwitchDevices && ChangeDevice,
-      })}
-    >
-      {/* {(SwitchDevices && !isObserver) && <ChangeDevice device={device} state={state} />} */}
-      {AddPerpetrator && <Perpetrator getData={(data) => dispatch({ type: 'add_data', data })}
-      style={{width: EmailNotify ? '48%' : '100%'}}
-      />}
-      {SetNextStageDeadline && <AddDate getData={(data) => dispatch({ type: 'add_data', data })} />}
-      {/* Когда в actions приходит setNextStageDeadline (указание даты проверки), то показываем компонент добавления даты */}
 
-      {EmailNotify && (
-        <>
-        <Contractors style={{width: '48%' }}
-                     getData={(data) => dispatch({
-                       type: 'add_email_contractors',
-                       data,
-                     })}
-        />
-
-        <Form.Item label="Добавьте текст письма" style={{width: '100%' }}>
-            <TextArea rows={4}
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-                dispatch({
-                  type: 'add_email_message',
-                  data: { message: e.target.value },
-                });
-              }}
+        {(AddPerpetrator && EmailNotify) && (
+          <>
+            <Perpetrator
+              getData={(data) => dispatch({ type: 'add_data', data })}
+              style={{ width: '48%'}}
             />
-        </Form.Item>
-        </>
-      )}
 
-      {/* {EmailNotify && <TemplateButton />} */}
-      {(EmailNotify) && (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-end',
-        gap: '10px'
-      }}>
-        <UploadButton {...upload.button} text="Загрузить письмо из шаблона" />
-        <UploadList {...upload.list} />
-      </div>
-      )}
-      {/* {(!SwitchDevices && AddDocuments && !isObserver) && ( */}
-      {(AddDocuments && !isObserver) && (
-        <>
-          <UploadButton {...upload.button} />
-          <UploadList {...upload.list} />
-        </>
-      )}
+            <Contractors
+              style={{ width: '48%' }}
+              getData={(data) => dispatch({
+                type: 'add_email_contractors',
+                data,
+              })}
+            />
 
-      {Switch && (
-        <NextStage getData={(data) => dispatch({ type: 'add_data', data })} />
-      )}
-      {(UploadReadings || addReadingsDone) && (
-        <AddReadings apartmentId={apartment.id} addReadings={(readings) => dispatch(addReadings(readings))} readingsBlocked={addReadingsDone || isObserver} />
-      )}
-      {/* Скрываю кнопку "Завершить этап" только для задачи "Замена прибора" */}
-      {/* {!SwitchDevices && <PushButton {...pushProps} />} */}
-      <PushButton {...pushProps} />
+            <Form.Item label="Добавьте текст письма" style={{ width: '100%' }}>
+              <TextArea
+                rows={4}
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                  dispatch({
+                    type: 'add_email_message',
+                    data: { message: e.target.value },
+                  });
+                }}
+              />
+            </Form.Item>
+            <UploadButton {...upload.button} text="Загрузить письмо из шаблона" />
+            <UploadList {...upload.list} />
+          </>
+        )}
 
-    </panel>,
-    // </Route>
-  );
+        {SetNextStageDeadline && <AddDate getData={(data) => dispatch({ type: 'add_data', data })} />}
+
+        {(AddDocuments && !isObserver) && (
+          <>
+            <UploadButton {...upload.button} />
+            <UploadList {...upload.list} />
+          </>
+        )}
+
+        {Switch && (
+          <NextStage getData={(data) => dispatch({ type: 'add_data', data })} />
+        )}
+        {(UploadReadings || addReadingsDone) && (
+          <AddReadings apartmentId={apartment.id} addReadings={(readings) => dispatch(addReadings(readings))} readingsBlocked={addReadingsDone || isObserver} />
+        )}
+        {/* Скрываю кнопку "Завершить этап" только для задачи "Замена прибора" */}
+        {/* {!SwitchDevices && <PushButton {...pushProps} />} */}
+        {!isObserver && <PushButton {...pushProps} />}
+
+      </panel>,
+    );
+  }
+  return null
+
+
+
+
 };
