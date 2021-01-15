881d36a347ecf9f2bb2d1bd3406ec7b8b63b4ceb
Marat <maratismodest@gmail.com>
2021-01-15 11:21:00 +0300

    commit

---
+++ src/01/_pages/EditCalculator/components/validationSchemas.ts
@@ -0,0 +1,27 @@
+import {ipv4RegExp} from "../../../tt-components/localBases";
+import * as Yup from 'yup';
+
+const yupDate = Yup.date().typeError('Поле обязательное').required('Поле обязательное');
+const yupSerialNumber = Yup.string().required('Введите серийный номер');
+const yupInfoId = Yup.number().typeError('Выберите модель').required('Выберите модель');
+
+export const defaultValidationSchema = Yup.object({
+    lastCheckingDate: yupDate,
+    futureCheckingDate: yupDate,
+    lastCommercialAccountingDate: yupDate,
+    futureCommercialAccountingDate: yupDate,
+    serialNumber: yupSerialNumber,
+    ipV4: Yup.string().matches(ipv4RegExp, 'Укажите в формате X.X.X.X').required('Введите IP-адрес устройства'),
+    deviceAddress: Yup.number().nullable().required('Введите сетевой адрес устройства'),
+    port: Yup.number().nullable().required('Введите порт устройства'),
+    infoId: yupInfoId,
+});
+
+export const emptyConnectionValidationSchema = Yup.object({
+    lastCheckingDate: yupDate,
+    futureCheckingDate: yupDate,
+    lastCommercialAccountingDate: yupDate,
+    futureCommercialAccountingDate: yupDate,
+    serialNumber: yupSerialNumber,
+    infoId: yupInfoId,
+});
