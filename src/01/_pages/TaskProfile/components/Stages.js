e192a579a235672b913f0fa967ab5fdf813b1fd8
Marat <maratismodest@gmail.com>
2021-01-21 15:42:57 +0300

    Update Stages.js

--- src/01/_pages/TaskProfile/components/Stages.js
+++ src/01/_pages/TaskProfile/components/Stages.js
@@ -29,15 +29,6 @@ import { ButtonTT } from "../../../tt-components";
 //     }
 
 
-const Input = styled.input.attrs(props => ({
-  type: "text",
-  size: props.size || "1em",
-}))`
-  border: 2px solid palevioletred;
-  margin: ${props => props.size};
-  padding: ${props => props.size};
-`;
-
 const StageItem = styled.div.attrs(props => ({
   status: props.status,
 }))`
