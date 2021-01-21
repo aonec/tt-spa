8c91cb67a957f0720ed8fc3623aff2aa0d88d4f9
Marat <maratismodest@gmail.com>
2021-01-21 16:18:26 +0300

    Update Stages.js

--- src/01/_pages/TaskProfile/components/Stages.js
+++ src/01/_pages/TaskProfile/components/Stages.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { button } from '01/r_comp';
 import { Icon } from '01/components/Icon';
 import { Loader } from '01/components';
-import test from 'reshadow'
-import { ButtonTT } from "../../../tt-components";
-
+import test from 'reshadow';
+import { ButtonTT } from '../../../tt-components';
 
 //     &[|status] {
 //       --status-color: var(--main-80);
@@ -28,65 +27,69 @@ import { ButtonTT } from "../../../tt-components";
 //       --status-border: var(--primary-100);
 //     }
 
-
-const StageItem = styled.div.attrs(props => ({
+const StageItem = styled.div.attrs((props) => ({
   status: props.status,
 }))`
-    
-     display: grid;
-     grid-template-columns: auto auto 1fr;
-     grid-row-gap: 8px;
-     grid-column-gap: 16px;
-     padding-bottom: 20px;
-     position: relative;
-       &:not(:last-child)::before {
-         content: "";
-         position: absolute;
-         border: 1px solid;
-         border-color: var(--primary-100);
-         bottom: 2px;
-         top: 34px;
-         left: 16px;
-       }
-  `
+  display: grid;
+  //grid-template-columns: auto auto 1fr;
+  grid-template-columns: auto 1fr;
+  grid-row-gap: 8px;
+  grid-column-gap: 16px;
+  padding-bottom: 20px;
+  position: relative;
+
+  &:not(:last-child)::before {
+    content: "";
+    position: absolute;
+    border: 1px solid;
+    border-color: var(--primary-100);
+    bottom: 2px;
+    top: 34px;
+    left: 16px;
+  }
+`;
 const Circle = styled.span`
-       grid-row: 1 / var(--span);
-       width: 32px;
-       height: 32px;
-       border-radius: 50%;
-       border: 1px solid var(--status-border);
-       display: inline-grid;
-       place-items: center;
-       color: var(--status-color);
-       background: var(--status-bg);
-`
+  grid-row: 1 / var(--span);
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid;
+  border-color: var(--primary-100);
+  display: inline-grid;
+  place-items: center;
+  color: var(--primary-100);
+  background: var(--status-bg);
+
+  ${(props) => props.expired && css`
+    border-color: var(--error);
+    & svg {
+      color: var(--error)
+    }
+  `}
+`;
 
 const StageName = styled.div`
-       grid-column: 2 / -1;
-       opacity: var(--stage-name-opacity);
-       align-self: center;
-`
+  grid-column: 2 / -1;
+  opacity: var(--stage-name-opacity);
+  align-self: center;
+`;
 
 const Time = styled.div`
-       grid-column: 2;
-       grid-row: 2;
-       font-size: 12px;
-       opacity: 0.32;
-`
+  font-size: 12px;
+  opacity: 0.32;
+  grid-row-gap: 8px;
+`;
 
 const UserName = styled.div`
-       grid-column: 2;
-       grid-row: 2;
-       font-size: 12px;
-       opacity: 0.32;
-`
+  font-size: 12px;
+  opacity: 0.32;
+  grid-gap: 8px;
+`;
 
 const Button = styled(ButtonTT)`
-       grid-column: 2 / -1;
-       justify-self: start;
-`
-
-
+  grid-column: 2 / -1;
+  justify-self: start;
+`;
 
 export const Stages = ({
   items = [], revertProps = {}, state = {}, panelLoading,
@@ -97,20 +100,21 @@ export const Stages = ({
       <h2>Этапы задачи</h2>
       <div>
         {items.map(({
-          id, name, icon, number, info, status, canRevert,
+          id, name, icon, number, info, status, canRevert, expired,
         }) => (
           <StageItem key={id} status={status}>
-            <Circle>{icon ? <Icon icon={icon} /> : number}</Circle>
+            <Circle expired={expired}>{icon ? <Icon icon={icon} /> : number}</Circle>
             <StageName>{name}</StageName>
+            <div />
             {info && (
-            <>
+            <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr', gridColumnGap: '8px' }}>
               <UserName>{info.name}</UserName>
               <Time>{info.time}</Time>
-            </>
+            </div>
             )}
             {canRevert && (
-            <Button size="small" {...revertProps} disabled={panelLoading ?? false}>
-              <span>Вернуть этап</span>
+            <Button color="white" {...revertProps} disabled={panelLoading ?? false}>
+             Вернуть этап
               <Loader show={panelLoading ?? false} />
             </Button>
             )}
