57e2d762dafce431926ec865a03e05efa527f7c9
makeevvd <70840382+makeevvd@users.noreply.github.com>
2021-01-22 07:41:18 +0300

    fix

--- src/01/_pages/MetersPage/components/HousesReadings/HousesDevices/HousesDevices.tsx
+++ src/01/_pages/MetersPage/components/HousesReadings/HousesDevices/HousesDevices.tsx
@@ -8,6 +8,8 @@ import { HousesDeviceReadingLine } from "../DeviceReadingLine/HousesDeviceReadin
 import { HouseReadingsHeader } from "../HouseReadingsHeader/HouseReadingsHeader";
 import {selectDevices, selectDisabledState} from "../../../../../Redux/ducks/readings/selectors";
 import { setDevices } from "01/Redux/ducks/readings/actionCreators";
+import Arrow from "01/_components/Arrow/Arrow";
+import {Icon} from "../../../../../components/Icon";
 
 const HouseReadingsDevice = styled.div`
 display: grid;
@@ -65,7 +67,61 @@ const HousesDevices: React.FC = () => {
 
     return (
         <div>
-            {house.city}, {house.number}, {house.corpus}
+            <AddressHeader>
+                {house.city}, {house.street}, {house.number}{house.corpus ? `,${house.corpus}` : ''}
+            </AddressHeader>
+            <InfoSticker>
+                <InfoHeader>
+                    <div><Icon icon="down" /></div>
+                    Информация о доме
+                </InfoHeader>
+                <InfoContent>
+                    <InfoColumn>
+                        <InfoRow>
+                            <InfoItemHeader>
+                                Район
+                            </InfoItemHeader>
+                            <InfoItem>
+                                {house.district ?? 'Нет данных'}
+                            </InfoItem>
+                        </InfoRow>
+                        <InfoRow>
+                            <InfoItemHeader>
+                                Тип дома
+                            </InfoItemHeader>
+                            <InfoItem>
+                                {house.houseCategory ?? 'Нет данных'}
+                            </InfoItem>
+                        </InfoRow>
+                        <InfoRow>
+                            <InfoItemHeader>
+                                Год постройки
+                            </InfoItemHeader>
+                            <InfoItem>
+                                {house.constructionDate ?? 'Нет данных'}
+                            </InfoItem>
+                        </InfoRow>
+                    </InfoColumn>
+                    <InfoColumn>
+                        <InfoRow>
+                            <InfoItemHeader>
+                                Управляющая компания
+                            </InfoItemHeader>
+                            <InfoCompany>
+                                Нет данных
+                            </InfoCompany>
+                        </InfoRow>
+                        <InfoRow>
+                            <InfoItemHeader>
+                                Информация об УК
+                            </InfoItemHeader>
+                            <InfoItem>
+                                Нет данных
+                            </InfoItem>
+                        </InfoRow>
+                    </InfoColumn>
+                </InfoContent>
+            </InfoSticker>
             <HouseReadingsHeader/>
             {deviceElems}
         </div>
@@ -73,3 +129,58 @@ const HousesDevices: React.FC = () => {
 }
 
 export default HousesDevices
+
+const AddressHeader = styled.h2`
+margin-left: 8px;
+margin-bottom: 16px;
+font-weight: 400;
+font-size: 24px;
+line-height: 32px;
+`
+
+const InfoSticker = styled.div`
+  height: auto;
+  padding: 16px;
+  box-shadow: var(--shadow);
+`
+
+const InfoHeader = styled.div`
+  display: flex;
+  font-size: 16px;
+  line-height: 32px;
+`
+
+const InfoContent = styled.div`
+display: flex;
+gap: 16px;
+`
+
+const InfoColumn = styled.div`
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+  `
+
+const InfoRow = styled.div`
+display: flex;
+padding: 16px 8px 16px;
+border-bottom: 1px solid var(--frame);
+`
+
+const InfoItem = styled.div`
+width: 50%;
+`
+
+const InfoCompany = styled.div`
+  width: 50%;
+  font-weight: 500;
+`
+
+const InfoItemHeader = styled.div`
+width: 50%;
+font-size: 14px;
+line-height: 16px;
+color: var(--main-70);
+`
+
+
