f5441f229ebb1618ee8b22184274babbcfbb05f9
Marat <maratismodest@gmail.com>
2021-02-02 09:31:46 +0300

    commit

--- src/01/App/useApp.js
+++ src/01/App/useApp.js
@@ -5,7 +5,7 @@ import { AppContext } from '01/context';
 
 export function useApp(){
   const { replace } = useHistory();
-  const { pathname } = useLocation()
+  // const { pathname } = useLocation()
 
   const [state, dispatch] = React.useReducer(reducer, {}, (state) => {
     const user = JSON.parse(localStorage.getItem('user'));
@@ -13,10 +13,11 @@ export function useApp(){
   });
 
   React.useEffect(() => {
-    if (pathname.match(/registration/gi)) {
-      console.log(pathname.match(/registration/gi));
-      return
-    }
+
+    // if (pathname.match(/registration/gi)) {
+    //   console.log(pathname.match(/registration/gi));
+    //   return
+    // }
 
     const token = localStorage.getItem('token');
     if (!token) {
