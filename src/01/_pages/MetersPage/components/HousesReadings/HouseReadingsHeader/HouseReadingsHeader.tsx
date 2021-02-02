a9e5bd8a9048fb46c56021faa8265173436582cb
makeevvd <makeevvd1@gmail.com>
2021-02-02 12:28:22 +0300

    fix layout

--- src/01/_pages/MetersPage/components/HousesReadings/HouseReadingsHeader/HouseReadingsHeader.tsx
+++ src/01/_pages/MetersPage/components/HousesReadings/HouseReadingsHeader/HouseReadingsHeader.tsx
@@ -3,14 +3,19 @@ import styled from "styled-components";
 
 const Container = styled.div`
 display: grid;
-grid-template-columns: minmax(0, 1fr) minmax(0, 3fr) minmax(0, 0.3fr) minmax(0, 1.8fr) minmax(0, 1.7fr) minmax(0, 1.7fr) minmax(0, 1fr) minmax(0, 1.5fr);
+grid-template-columns: 32px 180px 16px 152px 120px 120px 75px minmax(0, 1.5fr);
 column-gap: 16px;
 color: var(--main-90);
 background-color: var(--main-4);
 border-bottom: 1px solid var(--frame);
 align-items: center;
 padding: 16px;
+
+
+font-size: 12px;
 `
+
+
 
 export const HouseReadingsHeader:React.FC = () => {
 
