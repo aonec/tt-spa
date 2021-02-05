982ab3ad3580e04e0a661635a96af757e5aba1e9
maratismodest <maratismodest@gmail.com>
2021-02-05 15:43:52 +0300

    commit

--- src/01/_pages/EditNode/components/EditNodeForm.js
+++ src/01/_pages/EditNode/components/EditNodeForm.js
@@ -7,8 +7,9 @@ import * as Yup from 'yup';
 import _ from 'lodash';
 import { NavLink } from 'react-router-dom';
 import moment from 'moment';
+import styled from 'styled-components';
 import {
-  InputTT, SelectTT, DatePickerTT, Wrap, ButtonTT, Title, IconTT,
+  InputTT, SelectTT, DatePickerTT, Wrap, ButtonTT, Title, IconTT, StyledFooter,
 } from '../../../tt-components';
 import {
   ipv4RegExp, items, nodeStatusList, resources, serviceZoneList,
@@ -20,11 +21,9 @@ import { returnNullIfEmptyString } from '../../../utils/returnNullIfEmptyString'
 import { handleTabsBeforeFormSubmit } from '../../../utils/handleTabsBeforeFormSubmit';
 
 import { defaultValidationSchema } from './validationSchemas';
-import { NodeContext } from "../../Node";
-import styled from "styled-components";
-import RelatedDevices from "./RelatedDevices";
-import Connection from "./Connection";
-
+import { NodeContext } from '../../Node';
+import RelatedDevices from './RelatedDevices';
+import Connection from './Connection';
 
 const EditNodeForm = () => {
   const {
@@ -43,7 +42,7 @@ const EditNodeForm = () => {
     id: nodeId,
     calculatorId,
   } = node;
-  console.log("resource", resource)
+  console.log('resource', resource);
 
   const {
     handleSubmit, handleChange, values, touched, errors,
@@ -70,7 +69,6 @@ const EditNodeForm = () => {
       };
 
       console.log('nodeForm', nodeForm);
-      console.log(JSON.stringify(nodeForm));
       putNode(nodeId, nodeForm).then((res) => {
         console.log('putNode', res);
       });
@@ -88,22 +86,6 @@ const EditNodeForm = () => {
     return null;
   };
 
-  const tabErrors = [
-    {
-      key: '1',
-      value: ['number',
-        'nodeStatus',
-        'nodeResourceType',
-        'serviceZone',
-        'lastCommercialAccountingDate',
-        'futureCommercialAccountingDate'],
-    },
-    // {
-    //   key: '2',
-    //   value: ['ipV4', 'port', 'deviceAddress'],
-    // },
-  ];
-
 
   return (
     <form onSubmit={handleSubmit}>
@@ -118,7 +100,7 @@ const EditNodeForm = () => {
             }}
             disabled
           />
-          <Alert name="resource"/>
+          <Alert name="resource" />
         </Form.Item>
 
         <Form.Item label="Номер узла">
@@ -129,7 +111,7 @@ const EditNodeForm = () => {
             placeholder="Номер узла"
             onChange={handleChange}
           />
-          <Alert name="number"/>
+          <Alert name="number" />
         </Form.Item>
 
         <Form.Item label="Зона">
@@ -141,7 +123,7 @@ const EditNodeForm = () => {
               setFieldValue('serviceZone', event);
             }}
           />
-          <Alert name="serviceZone"/>
+          <Alert name="serviceZone" />
         </Form.Item>
 
         <Form.Item label="Коммерческий учет показателей приборов">
@@ -153,7 +135,7 @@ const EditNodeForm = () => {
               setFieldValue('nodeStatus', event);
             }}
           />
-          <Alert name="nodeStatus"/>
+          <Alert name="nodeStatus" />
         </Form.Item>
 
         {values.nodeStatus === 'Registered'
@@ -166,8 +148,8 @@ const EditNodeForm = () => {
                   allowClear={false}
                   placeholder="Укажите дату..."
                   onChange={(date) => {
-                    setFieldValue('lastCommercialAccountingDate', date);
-                  }}
+                      setFieldValue('lastCommercialAccountingDate', date);
+                    }}
                   value={values.lastCommercialAccountingDate}
                 />
               </Form.Item>
@@ -178,8 +160,8 @@ const EditNodeForm = () => {
                   placeholder="Укажите дату..."
                   allowClear={false}
                   onChange={(date) => {
-                    setFieldValue('futureCommercialAccountingDate', date);
-                  }}
+                      setFieldValue('futureCommercialAccountingDate', date);
+                    }}
                   value={values.futureCommercialAccountingDate}
                   name="futureCommercialAccountingDate"
                 />
@@ -195,14 +177,14 @@ const EditNodeForm = () => {
       </div>
 
       <div hidden={Number(currentTabKey) !== 3} style={{ maxWidth: 620 }}>
-        <RelatedDevices/>
+        <RelatedDevices />
       </div>
 
       <div hidden={Number(currentTabKey) !== 4}>
         <Title size="16" color="black">Компонент в разработке</Title>
       </div>
 
-      <div style={{ padding: '32px 0' }}>
+      <StyledFooter form>
         <ButtonTT
           color="blue"
           style={{ marginRight: '16px' }}
@@ -216,7 +198,7 @@ const EditNodeForm = () => {
             Отмена
           </ButtonTT>
         </NavLink>
-      </div>
+      </StyledFooter>
 
     </form>
 
@@ -224,9 +206,6 @@ const EditNodeForm = () => {
 };
 
 export default EditNodeForm;
-
-
-
 
 export const Template = styled.div``;
 
