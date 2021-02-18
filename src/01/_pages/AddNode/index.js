021ed82a208b326d6ebe0c5924ffb924a2fc3bc5
maratismodest <maratismodest@gmail.com>
2021-02-18 17:31:48 +0300

    commit

--- src/01/_pages/AddNode/index.js
+++ src/01/_pages/AddNode/index.js
@@ -109,6 +109,7 @@ export const AddNode = () => {
     setAddCalculator,
     addOdpu,
     setAddOdpu,
+    calculatorsExtended,
     communicationPipes,
     setCommunicationPipes,
     housingStock,
--- src/01/_pages/AddNode/modals/ModalAddNode/components/AddNodeForm.js
+++ src/01/_pages/AddNode/modals/ModalAddNode/components/AddNodeForm.js
@@ -1,77 +1,274 @@
 import React, {
-    useContext, useEffect, useState,
+  useContext, useEffect, useState,
 } from 'react';
-import {Form} from 'antd';
+import { Form } from 'antd';
 import moment from 'moment';
-import {useFormik} from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import _ from 'lodash';
-import styled from "styled-components";
+import styled from 'styled-components';
 import {
-    magistrals, housingMeteringDeviceTypes, isConnected,
+  magistrals, housingMeteringDeviceTypes, isConnected, serviceZoneList, nodeStatusList, resources,
 } from '../../../../../tt-components/localBases';
 import {
-    Title, SelectTT, InputTT, DatePickerTT, StyledModalBody, ButtonTT, StyledFooter, Icon, Warning,
+  IconTT,
+  Title, SelectTT, InputTT, DatePickerTT, StyledModalBody, ButtonTT, StyledFooter, Icon, Warning,
 } from '../../../../../tt-components';
-import {styles, StyledFormPage} from './styledComponents';
+import { styles, StyledFormPage } from './styledComponents';
 
-import {AddNodeContext} from '../../../index';
-
+import { AddNodeContext } from '../../../index';
+import { ListItem, ListWrap } from '../../../../../tt-components/List';
+import {addNodeFinal} from "../../../apiAddNode";
 
 const AddNodeForm = (props) => {
-    const {handleCancel} = props;
+  const { handleCancel } = props;
 
-    const {
-        currentTabKey,
-        setTab,
-        handleChangeTab,
-        handleNext,
-        node,
-        setNode,
-        housingStockId,
-        calculators,
-        addCalculator,
-        setAddCalculator,
-        addOdpu,
-        setAddOdpu,
-        communicationPipes,
-        setCommunicationPipes,
-        housingStock,
-        stepsArr,
-        isEmpty,
-        addNode,
-        setAddNode,
-    } = useContext(AddNodeContext);
+  const {
+    currentTabKey,
+    setTab,
+    handleChangeTab,
+    handleNext,
+    node,
+    setNode,
+    housingStockId,
+    calculators,
+    calculatorsExtended,
+    addCalculator,
+    setAddCalculator,
+    addOdpu,
+    setAddOdpu,
+    communicationPipes,
+    setCommunicationPipes,
+    housingStock,
+    stepsArr,
+    isEmpty,
+    addNode,
+    setAddNode,
+  } = useContext(AddNodeContext);
 
-    console.log('node', node);
+  console.log('node', node);
 
-    const {resource, entryNumber, calculatorId} = node;
+  const {
+    calculatorId,
+    entryNumber,
+    futureCheckingDate,
+    isConnected,
+    lastCheckingDate,
+    nodeStatus,
+    number,
+    resource,
+    serviceZone,
+  } = node;
 
+  const calculator = _.find(calculatorsExtended, { id: calculatorId });
+  console.log('calculator', calculator);
 
-    const [validationSchema, setValidationSchema] = useState(Yup.object({}));
+  const { serialNumber, model, closingDate } = calculator;
 
-    const initialValues = {
+  const getServiceZone = _.find(serviceZoneList, { value: serviceZone })?.label ?? 'Зона не определена';
+  const getNodeStatus = _.find(nodeStatusList, { value: nodeStatus })?.label ?? 'Статус не определен';
+  const getNodeResource = _.find(resources, { value: resource })?.label ?? 'Ресурс не определен';
+  const devices = communicationPipes.map((communicationPipe) => {
+    const { devices } = communicationPipe;
+    return devices.map((device) => device);
+  });
+  const res = _.flatten(devices);
 
-    };
+  console.log('node', node);
 
-    const {
-        handleSubmit, handleChange, values, touched, errors,
-        handleBlur, setFieldValue, setValues,
-    } = useFormik({
-        initialValues,
-        validationSchema,
-        onSubmit: async () => {
-            console.log('Создаем Узел');
-        },
-    });
+  const [validationSchema, setValidationSchema] = useState(Yup.object({}));
 
-    const StyledList = styled.li`
+  const initialValues = {communicationPipes};
+
+  const {
+    handleSubmit, handleChange, values, touched, errors,
+    handleBlur, setFieldValue, setValues,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async () => {
+      console.log('Создаем Узел');
+        const form = {
+          communicationPipes: values.communicationPipes,
+        };
+        console.log(form);
+        const addNodeForm = { ...node, communicationPipes };
+        console.log('addNodeForm', addNodeForm);
+        addNodeFinal(addNodeForm).then((res)=>{
+            console.log("addNodeFormResponseFromServer", res)
+            setTimeout(handleCancel, 1000);
+        })
+
+    },
+  });
+
+  const FirstBlock = () => (
+    <Block>
+      <BlockTitle>1. Общие данные </BlockTitle>
+      <ul>
+        <List>
+          <Info>Адрес</Info>
+          <Description>Нижнекамск, ул. Мира, 36</Description>
+        </List>
+        <List>
+          <Info>Тип ресурса</Info>
+          <Description>{getNodeResource}</Description>
+        </List>
+        <List>
+          <Info>Номер узла</Info>
+          <Description>{number}</Description>
+        </List>
+        <List>
+          <Info>Зона</Info>
+          <Description>{getServiceZone}</Description>
+        </List>
+        <List>
+          <Info>Коммерческий учет показателей приборов</Info>
+          <Description>{getNodeStatus}</Description>
+        </List>
+        <List>
+          <Info>Даты действия акта-допуска</Info>
+          <Description>
+            {moment(lastCheckingDate).format('DD.MM.YYYY')}
+            {' '}
+            -
+            {' '}
+            {moment(futureCheckingDate).format('DD.MM.YYYY')}
+          </Description>
+        </List>
+      </ul>
+    </Block>
+  );
+
+  const SecondBlock = () => (
+    <Block>
+      <BlockTitle>2. Настройки соединения </BlockTitle>
+      <ul>
+        <List style={{ alignItems: 'center' }}>
+          <CalculatorInfo>
+            <IconTT icon="device" />
+            <DeviceModel>{model}</DeviceModel>
+            {' '}
+            <DeviceSerial>
+              (
+              {serialNumber}
+              )
+            </DeviceSerial>
+          </CalculatorInfo>
+          <DeviceDescription>
+            <Description>
+              {closingDate ? (
+                <Div>
+                  <IconTT icon="red" />
+                  <span>Не активен</span>
+                </Div>
+              ) : (
+                <Div>
+                  <IconTT icon="green" />
+                  <span>Активен</span>
+                </Div>
+              )}
+            </Description>
+            <Number>
+              Ввод:
+              {entryNumber}
+            </Number>
+          </DeviceDescription>
+        </List>
+      </ul>
+    </Block>
+  );
+
+  const ThirdBlock = () => (
+    <Block>
+      <BlockTitle>3. Приборы</BlockTitle>
+      <ul>
+        {res.map((device) => {
+          const {
+            closingDate, model, serialNumber, pipe,
+          } = device;
+          const { pipeNumber } = pipe;
+          return (
+            <List style={{ alignItems: 'center' }}>
+              <CalculatorInfo>
+                <IconTT icon={resource.toLowerCase()} />
+                <DeviceModel>{model}</DeviceModel>
+                {' '}
+                <DeviceSerial>
+                  (
+{serialNumber}
+                  )
+                </DeviceSerial>
+              </CalculatorInfo>
+              <DeviceDescription>
+                <Description>
+                  {closingDate ? (
+                    <Div>
+                              <IconTT icon="red" />
+                              <span>Не активен</span>
+                            </Div>
+                  ) : (
+                            <Div>
+                                <IconTT icon="green" />
+                                <span>Активен</span>
+                              </Div>
+                  )}
+                </Description>
+                <Number>
+                  Труба:
+{pipeNumber}
+                </Number>
+              </DeviceDescription>
+            </List>
+          );
+        })}
+
+      </ul>
+    </Block>
+  );
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      <StyledModalBody>
+
+        <Title size="middle" color="black">
+          Добавление нового узла
+        </Title>
+
+        <FirstBlock />
+        <SecondBlock />
+
+        <ThirdBlock />
+
+      </StyledModalBody>
+      <StyledFooter>
+        <ButtonTT
+          color="blue"
+          type="submit"
+          style={{ marginLeft: '16px' }}
+          big
+        >
+          Создать Узел
+        </ButtonTT>
+        <ButtonTT type="button" color="white" onClick={handleCancel} style={{ marginLeft: '16px' }}>
+          Отмена
+        </ButtonTT>
+      </StyledFooter>
+    </form>
+  );
+};
+
+export default AddNodeForm;
+
+const List = styled.li`
       padding: 16px 4px;
       border-bottom: 1px solid #DCDEE4;
       display: grid;
       grid-template-columns: 4fr 8fr;
-    `
-    const StyledInfo = styled.h4`
+    `;
+const Info = styled.div`
       padding: 0;
       margin: 0;
       font-style: normal;
@@ -79,8 +276,8 @@ const AddNodeForm = (props) => {
       font-size: 14px;
       line-height: 16px;
       color: rgba(39, 47, 90, 0.6);
-    `
-    const StyledDescription = styled.p`
+    `;
+const Description = styled.p`
       padding: 0;
       margin: 0;
       font-style: normal;
@@ -88,73 +285,57 @@ const AddNodeForm = (props) => {
       font-size: 14px;
       line-height: 16px;
       color: rgba(39, 47, 90, 0.8);
-    `
+    `;
+const Block = styled.div`
+    `;
 
-    return (
-        <form
-            onSubmit={handleSubmit}
-        >
-            <StyledModalBody>
+const BlockTitle = styled.h3`
+      padding: 0;
+      margin: 0;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 32px;
+    `;
 
-                <Title size="middle" color="black">
-                    Добавление нового узла
-                </Title>
+const CalculatorInfo = styled(Info)`
+      display: grid;
+      grid-template-columns: auto auto 1fr;
+      align-items: center;
+      grid-column-gap: 8px;
+    `;
+const DeviceModel = styled.h4`
+      padding: 0;
+      margin: 0;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 32px;
+    `;
 
+const DeviceSerial = styled.p`
+      padding: 0;
+      margin: 0;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 16px;
+    `;
 
+const Div = styled.div`
+        display: flex;
+      align-items: center;
+    `;
 
-
-                    <div >
-                        <h3>1. Общие данные </h3>
-                        <ul>
-                            <StyledList>
-                                <StyledInfo>Адрес </StyledInfo>
-                                <StyledDescription>Нижнекамск, ул. Мира, 36</StyledDescription>
-                            </StyledList>
-                            <StyledList>
-                                <StyledInfo>Тип ресурса </StyledInfo>
-                                <StyledDescription>Холодная вода</StyledDescription>
-                            </StyledList>
-                            <StyledList>
-                                <StyledInfo>Номер узла</StyledInfo>
-                                <StyledDescription>15</StyledDescription>
-                            </StyledList>
-                            <StyledList>
-                                <StyledInfo>Зона</StyledInfo>
-                                <StyledDescription>Апартаменты</StyledDescription>
-                            </StyledList>
-                            <StyledList>
-                                <StyledInfo>Коммерческий учет показателей приборов</StyledInfo>
-                                <StyledDescription>Приборы на коммерческом учете</StyledDescription>
-                            </StyledList>
-                            <StyledList>
-                                <StyledInfo>Даты действия акта-допуска</StyledInfo>
-                                <StyledDescription>16.01.2020 — 16.01.2024</StyledDescription>
-                            </StyledList>
-                        </ul>
-                    </div>
-
-
-
-
-            </StyledModalBody>
-            <StyledFooter>
-                <ButtonTT
-                    color="blue"
-                    type="submit"
-                    style={{marginLeft: '16px'}}
-                    big
-                >
-                    Создать Узел
-                </ButtonTT>
-                <ButtonTT type="button" color="white" onClick={handleCancel} style={{marginLeft: '16px'}}>
-                    Отмена
-                </ButtonTT>
-            </StyledFooter>
-        </form>
-    );
-};
-
-export default AddNodeForm;
+const Number = styled.div`
+        
+    `;
+const DeviceDescription = styled.div`
+      display: grid;
+      grid-template-columns: 4fr 8fr;
+      align-items: center;
+      grid-column-gap: 8px;
+    `;
 
 // const form = {
 //   serialNumber: values.serialNumber,
