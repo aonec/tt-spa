4c7500581d5ffe00e321bee1e6bc61e00eaee8c4
maratismodest <maratismodest@gmail.com>
2021-03-17 10:38:41 +0300

    commit

--- src/01/Redux/actions/actions.js
+++ src/01/Redux/actions/actions.js
@@ -3,20 +3,6 @@ export const onChangeFormValueByPath = (path, value) => ({
     payload: { path, value },
 })
 
-export const setAddCalculatorForm = (path, value) => ({
-    type: 'CALC_UPDATE_FORM',
-    payload: { path, value },
-})
-
-export const onChangeDeviceFormValueByPath = (path, value) => ({
-    type: 'DEV_UPDATE_FORM_VALUE_BY_PATH',
-    payload: { path, value },
-})
-
-export const setAddDeviceForm = (path, value) => ({
-    type: 'DEV_UPDATE_FORM',
-    payload: { path, value },
-})
 
 // Изменение отображния модального окна ModalChangeOdpu
 export const setModalDeregisterVisible = (value) => ({
