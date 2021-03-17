6c2f308b1df5bc767e3f1ea719d10ea116f35344
maratismodest <maratismodest@gmail.com>
2021-03-17 10:27:28 +0300

    commit

--- src/01/Redux/rootReducer.ts
+++ src/01/Redux/rootReducer.ts
@@ -3,9 +3,7 @@ import { combineReducers } from 'redux'
 import calculatorReducer from './reducers/reducerCalc'
 import deviceReducer from './reducers/reducerDev'
 import reducerDevicesPage from './reducers/reducerDevicesPage'
-import deviceDeregisterReducer from './reducers/reducerDeviceDeregister'
 import objectReducer from './reducers/reducerObject'
-import changeOdpuReducer from './reducers/reducerChangeOdpu'
 import calcReportReducer from './reducers/reducerCalcReport'
 import readingsReducer from './reducers/readingsReducer'
 
@@ -15,7 +13,5 @@ export default combineReducers({
     objectReducer,
     calculatorPage: calculatorReducer,
     devicePage: reducerDevicesPage,
-    deviceDeregisterReducer,
-    changeOdpuReducer,
     readings: readingsReducer,
 })
