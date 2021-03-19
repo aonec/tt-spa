c393f0360b56376898ad67821adafedb59bbbce7
maratismodest <maratismodest@gmail.com>
2021-03-19 11:57:20 +0300

    commit

--- src/01/_pages/ObjectProfile/components/Map/index.jsx
+++ src/01/_pages/ObjectProfile/components/Map/index.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useMemo, useState } from 'react';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import {YMaps, Map, Placemark, ZoomControl, FullscreenControl} from 'react-yandex-maps';
 import objectIcon from './object.svg';
-import { ButtonTT } from '../../../../tt-components';
+import styled from 'styled-components'
+import {IconTT, ButtonTT } from '../../../../tt-components';
 import { ObjectContext } from '../../index';
 // background-image: url(${img});
 
@@ -11,10 +12,10 @@ export const MapObject = () => {
   const { latitude, longitude } = coordinates;
   // coordinates: {latitude: 55.810405, longitude: 49.191192}
   const objectCoordinates = [latitude, longitude];
-  const [zoom, setZoom] = useState(15);
+  const [zoom, setZoom] = useState(10);
   const mapState = useMemo(() => ({
     center: objectCoordinates,
-    controls: ['zoomControl', 'fullscreenControl'],
+    // controls: ['zoomControl', 'fullscreenControl'],
     zoom,
   }),
   [
@@ -25,17 +26,18 @@ export const MapObject = () => {
     // [55.664758, 51.838521],
   ];
 
-  const [state, setState] = useState(false);
-  console.log('state', state);
+  const [show, setShow] = useState(false);
+  console.log('show', show);
   return (
     <div>
-      <button onClick={() => setState((state) => !state)}>Скрыть карту</button>
-      <div hidden={state}>
+      <ShowHide><IconTT icon={show ? 'off' : 'on'}/>&nbsp;<button onClick={() => setShow((show) => !show)}>{show ? 'Скрыть карту' : 'Показать карту'}</button></ShowHide>
+      <div hidden={!show}>
         <Map
           modules={['control.ZoomControl', 'control.FullscreenControl']}
           width="100%"
-          height={240}
+          height={360}
           state={mapState}
+
         >
           {coordinatesList.map((coordinate, index) => (
             <Placemark
@@ -45,8 +47,8 @@ export const MapObject = () => {
                 'geoObject.addon.hint',
               ]}
               properties={{
-                hintContent: 'Информация об объекте',
-                balloonContentBody: 'Это красивая метка',
+                // hintContent: 'Информация об объекте',
+                // balloonContentBody: 'Это красивая метка',
               }}
               options={{
                 iconLayout: 'default#image',
@@ -56,6 +58,8 @@ export const MapObject = () => {
               }}
             />
           ))}
+            <FullscreenControl options={{position : { right: 16, top: 16}}} style={{color: 'red'}}  />
+            <ZoomControl options={{ size: 'small',position : { right: 16, bottom: 32}}} />
         </Map>
 
       </div>
@@ -65,6 +69,15 @@ export const MapObject = () => {
 };
 
 export default MapObject;
+
+const ShowHide = styled.div`
+  padding: 16px 0;
+  font-size: 12px;
+  line-height: 16px;
+  color: #272F5A;
+  display: flex;
+  align-items: center;
+`
 
 { /* <ButtonTT */ }
 { /*  color="blue" */ }
