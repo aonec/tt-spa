2d2e14d3e46d976a3805086564f722f0f8626c27
maratismodest <maratismodest@gmail.com>
2021-03-19 14:31:54 +0300

    commit

--- src/01/_pages/CalculatorProfile/components/Modals/ModalCalculatorReport/index.jsx
+++ src/01/_pages/CalculatorProfile/components/Modals/ModalCalculatorReport/index.jsx
@@ -1,60 +1,47 @@
-import React, { useContext } from 'react'
-import ModalCalculatorReportForm from './ModalCalculatorReportForm'
-import { DeviceContext } from '../../../CalculatorProfile'
-import ModalSonoSafeReportForm, { App } from './ModalSonoSafeReportForm'
-import { StyledModal } from '../../../../../tt-components'
+import React, { useContext } from 'react';
+import ModalCalculatorReportForm from './ModalCalculatorReportForm';
+import { DeviceContext } from '../../../CalculatorProfile';
+import ModalSonoSafeReportForm from './ModalSonoSafeReportForm';
+import { StyledModal } from '../../../../../tt-components';
 
 export const ModalCalculatorReport = () => {
-    const { report, setReport, device } = useContext(DeviceContext)
+  const { report, setReport, device } = useContext(DeviceContext);
 
-    const handleCancel = () => {
-        setReport(false)
-    }
+  const handleCancel = () => {
+    setReport(false);
+  };
 
-    // return (
-    //     <StyledModal
-    //           visible={report}
-    //           // visible
-    //           width={800}
-    //           footer={null}
-    //           onCancel={handleCancel}
-    //         >
-    //           <ModalCalculatorReportForm device={device} handleCancel={handleCancel} />
-    //         </StyledModal>
-    //
-    // )
-
-    if (device.infoId !== 10) {
-        return (
-            <StyledModal
-                visible={report}
-                // visible
-                width={800}
-                footer={null}
-                onCancel={handleCancel}
-            >
-                <ModalCalculatorReportForm
-                    device={device}
-                    handleCancel={handleCancel}
-                />
-            </StyledModal>
-        )
-    }
-
+  if (device.infoId !== 10) {
     return (
-        <StyledModal
-            visible={report}
-            // visible
-            width={800}
-            footer={null}
-            onCancel={handleCancel}
-        >
-            <ModalSonoSafeReportForm
-                device={device}
-                handleCancel={handleCancel}
-            />
-        </StyledModal>
-    )
-}
+      <StyledModal
+        visible={report}
+                // visible
+        width={800}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        <ModalCalculatorReportForm
+          device={device}
+          handleCancel={handleCancel}
+        />
+      </StyledModal>
+    );
+  }
 
-export default ModalCalculatorReport
+  return (
+    <StyledModal
+      visible={report}
+            // visible
+      width={800}
+      footer={null}
+      onCancel={handleCancel}
+    >
+      <ModalSonoSafeReportForm
+        device={device}
+        handleCancel={handleCancel}
+      />
+    </StyledModal>
+  );
+};
+
+export default ModalCalculatorReport;
