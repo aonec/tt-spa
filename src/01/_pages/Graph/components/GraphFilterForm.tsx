0a8a9ae37029047281c55a28fe850a7b5035fa4c
Vlad <makeevvd1@gmail.com>
2021-03-21 19:37:37 +0300

    tabs

--- src/01/_pages/Graph/components/GraphFilterForm.tsx
+++ src/01/_pages/Graph/components/GraphFilterForm.tsx
@@ -20,16 +20,16 @@ import { Button, Tooltip } from 'antd'
 import { GraphParamsType } from '../Graph'
 import IconTT from '../../../tt-components/IconTT'
 import styled from 'styled-components'
-import {paramsTranslation, translateParam} from '../utils'
+import {paramsTranslation} from '../utils'
 import ButtonTT from '../../../tt-components/ButtonTT'
 import { ReportType } from './GraphView'
 import useOutsideClick from '../../../hooks/useOutsideClick'
 
 const GraphFilterForm: React.FC<GraphFilterFormProps> = ({
-    setGraphParam,
-    setSearchQuery,
-    paramsList,
-}) => {
+                                                             setGraphParam,
+                                                             setSearchQuery,
+                                                             paramsList,
+                                                         }) => {
     const [isActive, setIsActive] = useState(false)
 
     const formRef = useRef<any>()
@@ -49,8 +49,8 @@ const GraphFilterForm: React.FC<GraphFilterFormProps> = ({
     }
 
     const handleSubmit = (
-        values: FormValuesInterface,
-        actions: FormikHelpers<FormValuesInterface>
+      values: FormValuesInterface,
+      actions: FormikHelpers<FormValuesInterface>
     ) => {
         setSearchQuery((prevQuery) => {
             return {
@@ -80,167 +80,167 @@ const GraphFilterForm: React.FC<GraphFilterFormProps> = ({
     }))
 
     return (
-        <GraphFilter ref={formRef}>
-            <Formik
-                initialValues={{
-                    dateRange: [
-                        moment().subtract(6, 'day').set({
-                            hour: 0,
-                            minute: 0,
-                            second: 0,
-                            millisecond: 0,
-                        }),
-                        moment().set({
-                            hour: 23,
-                            minute: 0,
-                            second: 0,
-                            millisecond: 0,
-                        }),
-                    ],
-                    reportType: 'daily',
-                    autocomplete: paramsList[0],
-                }}
-                onSubmit={handleSubmit}
-            >
-                {!isActive ? (
-                    <div
-                        style={{
-                            display: 'flex',
-                            marginBottom: 10,
-                            paddingLeft: 8,
-                            paddingTop: 16,
-                        }}
-                    >
-                        <Tooltip title="Настройка параметров">
-                            <div style={{ marginRight: 16 }}>
+      <GraphFilter ref={formRef}>
+          <Formik
+            initialValues={{
+                dateRange: [
+                    moment().subtract(6, 'day').set({
+                        hour: 0,
+                        minute: 0,
+                        second: 0,
+                        millisecond: 0,
+                    }),
+                    moment().set({
+                        hour: 23,
+                        minute: 0,
+                        second: 0,
+                        millisecond: 0,
+                    }),
+                ],
+                reportType: 'daily',
+                autocomplete: paramsList[0],
+            }}
+            onSubmit={handleSubmit}
+          >
+              {!isActive ? (
+                <div
+                  style={{
+                      display: 'flex',
+                      marginBottom: 10,
+                      paddingLeft: 8,
+                      paddingTop: 16,
+                  }}
+                >
+                    <Tooltip title="Настройка параметров">
+                        <div style={{ marginRight: 16 }}>
+                            <Button
+                              onClick={() =>
+                                setIsActive((state) => !state)
+                              }
+                              icon={<IconTT icon="searchFilter" />}
+                            />
+                        </div>
+                    </Tooltip>
+                    <Select
+                      style={{ width: '100%' }}
+                      name="autocomplete"
+                      placeholder="Autocomplete"
+                      showArrow={true}
+                      options={options}
+                      onSelect={onSelectHandler}
+                    />
+                </div>
+              ) : (
+                <OpenedFilter>
+                    <Form>
+                        <FormBody>
+                            <Tooltip title="Настройка параметров">
                                 <Button
-                                    onClick={() =>
-                                        setIsActive((state) => !state)
-                                    }
-                                    icon={<IconTT icon="searchFilter" />}
+                                  onClick={() =>
+                                    setIsActive((state) => !state)
+                                  }
+                                  style={{ marginBottom: 8 }}
+                                  icon={<IconTT icon="searchFilter" />}
                                 />
-                            </div>
-                        </Tooltip>
-                        <Select
-                            style={{ width: '100%' }}
-                            name="autocomplete"
-                            placeholder="Autocomplete"
-                            showArrow={true}
-                            options={options}
-                            onSelect={onSelectHandler}
-                        />
-                    </div>
-                ) : (
-                    <OpenedFilter>
-                        <Form>
-                            <FormBody>
-                                <Tooltip title="Настройка параметров">
-                                    <Button
-                                        onClick={() =>
-                                            setIsActive((state) => !state)
-                                        }
-                                        style={{ marginBottom: 8 }}
-                                        icon={<IconTT icon="searchFilter" />}
+                            </Tooltip>
+                            <FormItem
+                              name="dateRange"
+                              label="Произвольный период"
+                            >
+                                <RangeWrapper id="div">
+                                    <DatePicker.RangePicker
+                                      getPopupContainer={() =>
+                                        document.getElementById('div')!
+                                      }
+                                      name="dateRange"
+                                      format="DD MMMM YYYY"
+                                      style={{ marginRight: 16 }}
+                                      ranges={{
+                                          'Последние сутки': [
+                                              moment(),
+                                              moment().set({
+                                                  hour: 23,
+                                                  minute: 0,
+                                                  second: 0,
+                                                  millisecond: 0,
+                                              }),
+                                          ],
+                                          'Последние 7 дней': [
+                                              moment()
+                                                .subtract(1, 'week')
+                                                .set({
+                                                    hour: 0,
+                                                    minute: 0,
+                                                    second: 0,
+                                                    millisecond: 0,
+                                                }),
+                                              moment().set({
+                                                  hour: 23,
+                                                  minute: 0,
+                                                  second: 0,
+                                                  millisecond: 0,
+                                              }),
+                                          ],
+                                          'С начала месяца': [
+                                              moment().startOf('month'),
+                                              moment().set({
+                                                  hour: 23,
+                                                  minute: 0,
+                                                  second: 0,
+                                                  millisecond: 0,
+                                              }),
+                                          ],
+                                          'За прошлый месяц': [
+                                              moment()
+                                                .startOf('month')
+                                                .subtract(1, 'months'),
+                                              moment()
+                                                .subtract(1, 'months')
+                                                .endOf('month'),
+                                          ],
+                                      }}
                                     />
-                                </Tooltip>
-                                <FormItem
-                                    name="dateRange"
-                                    label="Произвольный период"
-                                >
-                                    <RangeWrapper id="div">
-                                        <DatePicker.RangePicker
-                                            getPopupContainer={() =>
-                                                document.getElementById('div')!
-                                            }
-                                            name="dateRange"
-                                            format="DD MMMM YYYY"
-                                            style={{ marginRight: 16 }}
-                                            ranges={{
-                                                'Последние сутки': [
-                                                    moment(),
-                                                    moment().set({
-                                                        hour: 23,
-                                                        minute: 0,
-                                                        second: 0,
-                                                        millisecond: 0,
-                                                    }),
-                                                ],
-                                                'Последние 7 дней': [
-                                                    moment()
-                                                        .subtract(1, 'week')
-                                                        .set({
-                                                            hour: 0,
-                                                            minute: 0,
-                                                            second: 0,
-                                                            millisecond: 0,
-                                                        }),
-                                                    moment().set({
-                                                        hour: 23,
-                                                        minute: 0,
-                                                        second: 0,
-                                                        millisecond: 0,
-                                                    }),
-                                                ],
-                                                'С начала месяца': [
-                                                    moment().startOf('month'),
-                                                    moment().set({
-                                                        hour: 23,
-                                                        minute: 0,
-                                                        second: 0,
-                                                        millisecond: 0,
-                                                    }),
-                                                ],
-                                                'За прошлый месяц': [
-                                                    moment()
-                                                        .startOf('month')
-                                                        .subtract(1, 'months'),
-                                                    moment()
-                                                        .subtract(1, 'months')
-                                                        .endOf('month'),
-                                                ],
-                                            }}
-                                        />
-                                    </RangeWrapper>
-                                </FormItem>
+                                </RangeWrapper>
+                            </FormItem>
 
-                                <FormItem name="reportType" label="Тип отчета">
-                                    <div>
-                                        <Radio.Group
-                                            name="reportType"
-                                            options={[
-                                                {
-                                                    label: 'Часовой',
-                                                    value: 'hourly',
-                                                },
-                                                {
-                                                    label: 'Суточный',
-                                                    value: 'daily',
-                                                },
-                                            ]}
-                                        />
-                                    </div>
-                                </FormItem>
-                            </FormBody>
-                            <FormFooter>
-                                <ButtonTT
-                                    color="white"
-                                    small
-                                    onClick={() =>
-                                        setIsActive((state) => !state)
-                                    }
-                                    style={{ marginRight: 16 }}
-                                >
-                                    Отмена
-                                </ButtonTT>
-                                <StyledSubmit disabled={false}>
-                                    Применить настройки
-                                </StyledSubmit>
-                            </FormFooter>
-                        </Form>
-                    </OpenedFilter>
-                )}
-            </Formik>
-        </GraphFilter>
+                            <FormItem name="reportType" label="Тип отчета">
+                                <div>
+                                    <Radio.Group
+                                      name="reportType"
+                                      options={[
+                                          {
+                                              label: 'Часовой',
+                                              value: 'hourly',
+                                          },
+                                          {
+                                              label: 'Суточный',
+                                              value: 'daily',
+                                          },
+                                      ]}
+                                    />
+                                </div>
+                            </FormItem>
+                        </FormBody>
+                        <FormFooter>
+                            <ButtonTT
+                              color="white"
+                              small
+                              onClick={() =>
+                                setIsActive((state) => !state)
+                              }
+                              style={{ marginRight: 16 }}
+                            >
+                                Отмена
+                            </ButtonTT>
+                            <StyledSubmit disabled={false}>
+                                Применить настройки
+                            </StyledSubmit>
+                        </FormFooter>
+                    </Form>
+                </OpenedFilter>
+              )}
+          </Formik>
+      </GraphFilter>
     )
 }
 
