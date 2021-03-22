29ff7fb8f03deb9d1d393694b02b361a4a94ca8b
maratismodest <maratismodest@gmail.com>
2021-03-22 14:28:07 +0300

    commit

--- src/01/_api/utils/useAccessesList.ts
+++ src/01/_api/utils/useAccessesList.ts
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-const useAccessesList = (accessName: string) => {
+const useAccessesList = () => {
     const permissions = localStorage.getItem('permissions');
     const initialState = JSON.parse(permissions || '')
     const [state, setState] = useState(initialState);
