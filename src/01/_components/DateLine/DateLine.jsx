53110219def48243cdb88bfef5cea063bd014bc6
Vlad <makeevvd1@gmail.com>
2021-03-23 17:25:45 +0300

    fixes

--- src/01/_components/DateLine/DateLine.jsx
+++ src/01/_components/DateLine/DateLine.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import transformDate from '../../utils/transformDate'
-import styled from 'styled-components'
+import React from 'react';
+import transformDate from '../../utils/transformDate';
+import styled from 'styled-components';
 
 const Container = styled.div`
     display: flex;
@@ -8,6 +8,7 @@ const Container = styled.div`
     color: rgba(39, 47, 90, 0.6);
     line-height: 1.2;
     align-items: center;
+    white-space: nowrap;
 `
 
 export const DateLine = ({ lastCheckingDate, futureCheckingDate }) => {
