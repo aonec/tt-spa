16ce682acdaeff9108a88408b1ddb006923f6dbd
maratismodest <maratismodest@gmail.com>
2021-03-23 15:37:23 +0300

    Update useAccessesList.ts

--- src/01/_api/utils/useAccessesList.ts
+++ src/01/_api/utils/useAccessesList.ts
@@ -2,7 +2,7 @@ import {useState} from "react";
 
 const useAccessesList = () => {
     const permissions = localStorage.getItem('permissions');
-    const initialState = JSON.parse(permissions || '')
+    const initialState = JSON.parse(permissions || `['']`)
     const [state, setState] = useState(initialState);
     const show = (accessName: string) => state.includes(accessName);
     return {show, state}
