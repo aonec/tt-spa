b7c207f9e1d975e31c578d989bc1eb95913ea6f9
Vlad <makeevvd1@gmail.com>
2021-03-24 14:08:19 +0300

    fixes

--- src/01/Redux/rootReducer.ts
+++ src/01/Redux/rootReducer.ts
@@ -1,16 +1,18 @@
-import { combineReducers } from 'redux'
-
-import reducerDevicesPage from './reducers/reducerDevicesPage'
-import objectReducer from './reducers/reducerObject'
-import readingsReducer from './reducers/readingsReducer'
-import userReducer from './reducers/userReducer'
-import {CalculatorListResponsePagedList} from "../../myApi";
+import { combineReducers } from 'redux';
+import reducerDevicesPage from './reducers/reducerDevicesPage';
+import objectReducer from './reducers/reducerObject';
+import readingsReducer from './reducers/readingsReducer';
+import userReducer from './reducers/userReducer';
+import { CalculatorListResponsePagedList } from '../../myApi';
 
 export default combineReducers({
-    objectReducer,
-    devicePage: reducerDevicesPage,
-    readings: readingsReducer,
-    user: userReducer
-})
+  objectReducer,
+  devicePage: reducerDevicesPage,
+  readings: readingsReducer,
+  user: userReducer,
+});
 
-export type DevicePageType = CalculatorListResponsePagedList & { isLoading: boolean, currentPage: number }
+export type DevicePageType = CalculatorListResponsePagedList & {
+  isLoading: boolean;
+  currentPage: number;
+};
--- src/01/Redux/store.ts
+++ src/01/Redux/store.ts
@@ -1,39 +1,38 @@
-import createSagaMiddleware from 'redux-saga'
-import { applyMiddleware, compose, createStore } from 'redux'
-import rootReducer, {DevicePageType} from './rootReducer'
-import thunkMiddleWare from 'redux-thunk'
-import rootSaga from './saga'
-import { ReadingsStateType } from '../_api/houses_readings_page'
-import {CalculatorListResponsePagedList} from "../../myApi";
+import createSagaMiddleware from 'redux-saga';
+import { applyMiddleware, compose, createStore } from 'redux';
+import rootReducer, { DevicePageType } from './rootReducer';
+import thunkMiddleWare from 'redux-thunk';
+import rootSaga from './saga';
+import { ReadingsStateType } from '../_api/houses_readings_page';
 
 declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
-    }
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
 }
 
 const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
-    const result = next(action)
-    console.log('Middleware', store.getState())
-    return result
-}
+  const result = next(action);
+  console.log('Middleware', store.getState());
+  return result;
+};
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export interface RootState {
-    readings: ReadingsStateType
-    devicePage: DevicePageType
+  readings: ReadingsStateType;
+  devicePage: DevicePageType;
 }
 
 export const store = createStore(
-    rootReducer,
-    composeEnhancers(
-        applyMiddleware(loggerMiddleware, thunkMiddleWare, sagaMiddleware)
-    )
-)
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(loggerMiddleware, thunkMiddleWare, sagaMiddleware)
+  )
+);
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
