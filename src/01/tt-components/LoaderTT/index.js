4eb9c9f75dbb3a94218a877b4c21884c0b2d0516
maratismodest <maratismodest@gmail.com>
2021-03-30 09:49:09 +0300

    commit

--- src/01/tt-components/LoaderTT/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spin } from 'antd';
-import { SyncOutlined } from '@ant-design/icons';
-import styles from './loader.module.scss';
-
-export const LoaderTT = () => {
-  const antIcon = <SyncOutlined spin />;
-  return <Spin indicator={antIcon} className={styles.loader} />;
-};
