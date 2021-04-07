6779004624de3015061e33976e420cbab437cb3a
Vlad <makeevvd1@gmail.com>
2021-04-07 17:32:13 +0300

    improve layout

---
+++ src/01/_pages/Settings/components/CompanyInfo.tsx
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react';
+import { Subtitle } from '../../../_components/Headers';
+import { ListItem, Title } from '../../../tt-components/List';
+import moment from 'moment';
+import { SettingsContext } from '../index';
+import styled from 'styled-components';
+
+const CompanyInfo = () => {
+  const { firm } = useContext(SettingsContext);
+  const { id, name, phoneNumber, timeZoneOffset } = firm || {};
+
+  const timeOffset = Number(timeZoneOffset.split(':')[0]);
+  debugger;
+  return (
+    <div style={{ width: '66%' }}>
+      <Title>Информация</Title>
+      <CompanyItem>
+        <span>Название</span>
+        <div style={{ fontWeight: 500 }}>{name}</div>
+      </CompanyItem>
+      <CompanyItem>
+        <span>Адрес</span>
+        <div style={{ color: 'var(--main-80)' }}>Адрес компании</div>
+      </CompanyItem>
+      <CompanyItem>
+        <span>Электронная почта</span>
+        <div>qwerty@yandex.ru</div>
+      </CompanyItem>
+      <CompanyItem>
+        <span>Контактный телефон</span>
+        <div>{phoneNumber ? phoneNumber : '+7 888 888-88-88'}</div>
+      </CompanyItem>
+      <CompanyItem>
+        <span>Часовой пояс</span>
+        <div>UTC+{timeOffset}</div>
+      </CompanyItem>
+    </div>
+  );
+};
+
+const CompanyItem = styled(ListItem)`
+  span {
+    color: var(--main-60);
+  }
+`;
+
+export default CompanyInfo;
--- src/01/_pages/Settings/index.js
+++ src/01/_pages/Settings/index.js
@@ -14,6 +14,7 @@ import ModalAddStaff from './components/Modals/ModalAddStaff';
 import ModalAddContractor from './components/Modals/ModalAddContractor';
 import HeaderButton from './components/HeaderButton';
 import { Loader } from '../../_components/Loader';
+import CompanyInfo from './components/CompanyInfo';
 
 export const SettingsContext = createContext();
 export const Settings = () => {
@@ -130,7 +131,8 @@ export const Settings = () => {
           handleChangeTab={handleChangeTab}
         />
         <Route path="/settings" exact>
-          <Common />
+          {/*<Common />*/}
+          <CompanyInfo />
         </Route>
         <Route path="/settings/staff" exact>
           <Staff />
