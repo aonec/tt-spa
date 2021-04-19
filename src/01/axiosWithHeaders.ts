05bda65bafe4652f47ee5daa228d04713fbfca4b
makeevvd <70840382+makeevvd@users.noreply.github.com>
2021-04-19 12:22:15 +0300

    Update axiosWithHeaders.ts

--- src/01/axiosWithHeaders.ts
+++ src/01/axiosWithHeaders.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // const baseURL = process.env.REACT_APP_URL
 
-const baseURL = 'https://transparent-staging.herokuapp.com/api';
-// const baseURL = 'https://transparent-production.herokuapp.com/api';
+//const baseURL = 'https://transparent-staging.herokuapp.com/api';
+const baseURL = 'https://transparent-production.herokuapp.com/api';
 
 // axios.defaults.baseURL = baseURL;
 
