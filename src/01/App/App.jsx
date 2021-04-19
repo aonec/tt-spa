d34ff19f50891dcbbcd4d2f35813754ca23fc742
makeevvd <makeevvd1@gmail.com>
2021-04-19 12:29:48 +0300

    remove metrica

--- src/01/App/App.jsx
+++ src/01/App/App.jsx
@@ -174,11 +174,6 @@ export function App() {
     <Provider store={store}>
       <AppProvider>
         <ConfigProvider locale={ruRu}>
-          <YMInitializer
-            accounts={[75427876]}
-            options={{ webvisor: true }}
-            version="2"
-          />
           <YMaps>
             <Internal />
           </YMaps>
