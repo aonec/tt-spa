1d4a44afc5eac36fb812704cde3c7dff3b3ad84b
makeevvd <makeevvd1@gmail.com>
2021-06-03 16:06:20 +0300

    improve groupNodesByCalculator function

--- src/01/_pages/ObjectProfile/components/utis/groupNodesByCalculator.js
+++ src/01/_pages/ObjectProfile/components/utis/groupNodesByCalculator.js
@@ -1,27 +1,12 @@
+import _ from 'lodash';
+
 export const groupNodesByCalculator = (nodes) => {
-  return nodes.reduce((acc, node) => {
-    const isNodeWithoutCalc = node.calculatorId === null;
+  const nodesById = _.groupBy(nodes, 'calculatorId');
 
-    if (isNodeWithoutCalc) {
-      const isNodeArrayExists = acc.some((calc) => calc.id === null);
-      if (!isNodeArrayExists) return [...acc, { id: null, nodes: [node] }];
+  const calculators = _.map(nodesById, (value) => ({
+    ...value[0].calculator,
+    nodes: value,
+  }));
 
-      return acc.map((calc) =>
-        calc.id === null ? { ...calc, nodes: [...calc.nodes, node] } : calc
-      );
-    }
-
-    const isExistingCalculatorId = acc.some(
-      (calculator) => calculator.id === node.calculatorId
-    );
-
-    if (!isExistingCalculatorId)
-      return [...acc, { ...node.calculator, nodes: [node] }];
-
-    return acc.map((calc) =>
-      calc.id === node.calculatorId
-        ? { ...calc, nodes: [...calc.nodes, node] }
-        : calc
-    );
-  }, []);
+  return calculators;
 };
