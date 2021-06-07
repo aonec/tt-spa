58caa1dfae73653ffaaa2e79a4b05b3d54cae53e
pronix575 <proenix575@gmail.com>
2021-06-07 11:39:09 +0300

    Add loader

--- src/01/features/contractors/deleteContractor/index.tsx
+++ src/01/features/contractors/deleteContractor/index.tsx
@@ -1,15 +1,18 @@
 import { Footer, Header } from '01/shared/ui/Modal/Modal';
 import { ButtonTT, StyledModal } from '01/tt-components';
+import { Loader } from '01/_components/Loader';
 import { useStore } from 'effector-react';
 import React from 'react';
 import {
   $isDeleteContractorModalVisible,
   deleteContractorCancelButtonClicked,
   deleteContractorConfirmButtonClicked,
+  deleteContractorFx,
 } from './models';
 
 export const DeleteContractorModal = () => {
   const visible = useStore($isDeleteContractorModalVisible);
+  const pending = useStore(deleteContractorFx.pending);
 
   const onCancel = () => deleteContractorCancelButtonClicked();
   const onOk = () => deleteContractorConfirmButtonClicked();
@@ -26,8 +29,13 @@ export const DeleteContractorModal = () => {
           <ButtonTT color={'white'} key="back" onClick={onCancel}>
             Отмена
           </ButtonTT>
-          <ButtonTT color={'red'} key="submit" onClick={onOk}>
-            Удалить
+          <ButtonTT
+            disabled={pending}
+            color={'red'}
+            key="submit"
+            onClick={onOk}
+          >
+            {pending ? <Loader show={true} /> : 'Удалить'}
           </ButtonTT>
         </Footer>
       }
