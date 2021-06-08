818ce0908a2d8119b9a4481b123dfdbc4e105d2b
pronix575 <proenix575@gmail.com>
2021-06-08 10:02:35 +0300

    Update form

--- src/01/features/staff/addStaff/index.tsx
+++ src/01/features/staff/addStaff/index.tsx
@@ -1,16 +1,41 @@
 import { ErrorMessage } from '01/features/contractors/addContractors';
-import { ModalText } from '01/shared/ui/Modal/Modal';
-import { ButtonTT, InputTT, StyledModal } from '01/tt-components';
+import {
+  Footer,
+  Header,
+  ModalText,
+  StyledModal,
+} from '01/shared/ui/Modal/Modal';
+import { ButtonTT, InputTT, MultiSelectTT } from '01/tt-components';
 import { Form } from 'antd';
-import { Footer } from 'antd/lib/layout/layout';
 import { useForm } from 'effector-forms/dist';
 import { useStore } from 'effector-react';
 import React from 'react';
+import styled from 'styled-components';
 import {
   $isAddStaffModalVisible,
   addStaffForm,
   addStaffModalCloseButtonClicked,
 } from './models';
+
+const FormItemsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  .ant-row {
+    width: 100%;
+    margin-right: 20px;
+  }
+
+  .ant-row:last-child {
+    margin-right: 0;
+  }
+`;
+
+const FormContainer = styled.form`
+  .ant-form-item {
+    padding-bottom: 0 !important;
+  }
+`;
 
 export const AddStaffModal: React.FC = () => {
   const visible = useStore($isAddStaffModalVisible);
@@ -22,7 +47,7 @@ export const AddStaffModal: React.FC = () => {
       visible={visible}
       onCancel={onCancel}
       width={800}
-      title="Добавление нового сотрудника"
+      title={<Header>Добавление нового сотрудника</Header>}
       footer={
         <Footer>
           <ButtonTT color={'white'} key="back" onClick={onCancel}>
@@ -34,30 +59,97 @@ export const AddStaffModal: React.FC = () => {
         </Footer>
       }
     >
-      <ModalText style={{ margin: '0 24px' }}>
-        <form onSubmit={() => onSubmit()}>
-          <Form.Item label="Название организации">
-            <InputTT
-              name="name"
-              type="text"
-              value={fields.lastName.value}
-              onChange={(e) => fields.lastName.onChange(e.target.value)}
+      <ModalText>
+        <FormContainer onSubmit={() => onSubmit()}>
+          <FormItemsContainer>
+            <Form.Item label="Фамилия">
+              <InputTT
+                name="lastName"
+                type="text"
+                value={fields.lastName.value}
+                onChange={(e) => fields.lastName.onChange(e.target.value)}
+              />
+              <ErrorMessage>
+                {fields.lastName.errorText({
+                  required: 'Это поле обязательное',
+                })}
+              </ErrorMessage>
+            </Form.Item>
+            <Form.Item label="Имя">
+              <InputTT
+                type="firstName"
+                value={fields.firstName.value}
+                onChange={(e) => fields.firstName.onChange(e.target.value)}
+              />
+              <ErrorMessage>
+                {fields.firstName.errorText({
+                  email: 'Введите корректный адрес электронной почты',
+                })}
+              </ErrorMessage>
+            </Form.Item>
+            <Form.Item label="Отчество">
+              <InputTT
+                name="middleName"
+                type="text"
+                value={fields.middleName.value}
+                onChange={(e) => fields.middleName.onChange(e.target.value)}
+              />
+              <ErrorMessage>
+                {fields.middleName.errorText({
+                  required: 'Это поле обязательное',
+                })}
+              </ErrorMessage>
+            </Form.Item>
+          </FormItemsContainer>
+          <FormItemsContainer>
+            <Form.Item label="Электронная почта">
+              <InputTT
+                name="email"
+                type="text"
+                value={fields.email.value}
+                onChange={(e) => fields.email.onChange(e.target.value)}
+              />
+              <ErrorMessage>
+                {fields.email.errorText({
+                  required: 'Это поле обязательное',
+                })}
+              </ErrorMessage>
+            </Form.Item>
+            <Form.Item label="Контактный телефон">
+              <InputTT
+                maxLength={11}
+                width="100%"
+                name="cellPhone"
+                type="number"
+                value={fields.cellPhone.value}
+                onChange={(e) => fields.cellPhone.onChange(e.target.value)}
+              />
+              <ErrorMessage>
+                {fields.cellPhone.errorText({
+                  required: 'Это поле обязательное',
+                })}
+              </ErrorMessage>
+            </Form.Item>
+          </FormItemsContainer>
+          <Form.Item label="Роль в системе">
+            <MultiSelectTT
+              mode="multiple"
+              options={[{ lable: 'text', value: 1 }]}
             />
             <ErrorMessage>
-              {fields.email.errorText({
+              {fields.cellPhone.errorText({
                 required: 'Это поле обязательное',
               })}
             </ErrorMessage>
           </Form.Item>
-          <Form.Item label="Электронная почта">
-            <InputTT
-              type="email"
-              value={fields.email.value}
-              onChange={(e) => fields.email.onChange(e.target.value)}
+          <Form.Item label="Коипетенции">
+            <MultiSelectTT
+              mode="multiple"
+              options={[{ lable: 'text', value: 1 }]}
             />
             <ErrorMessage>
-              {fields.email.errorText({
-                email: 'Введите корректный адрес электронной почты',
+              {fields.cellPhone.errorText({
+                required: 'Это поле обязательное',
               })}
             </ErrorMessage>
           </Form.Item>
@@ -66,7 +158,7 @@ export const AddStaffModal: React.FC = () => {
             type="submit"
             style={{ display: 'none' }}
           />
-        </form>
+        </FormContainer>
       </ModalText>
     </StyledModal>
   );
