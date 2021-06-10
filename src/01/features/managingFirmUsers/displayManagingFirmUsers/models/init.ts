6c86be112827d0ed3981e1e18f5780ade2c3555d
pronix575 <proenix575@gmail.com>
2021-06-10 10:31:22 +0300

    Rewrite staff

--- src/01/features/managingFirmUsers/displayManagingFirmUsers/models/init.ts
+++
@@ -1,8 +0,0 @@
-import { forward } from 'effector';
-import { fetchManagingFirmUsersFx } from './index';
-import { ManagingFirmUsersGate } from '.';
-
-forward({
-  from: ManagingFirmUsersGate.open,
-  to: fetchManagingFirmUsersFx,
-});
