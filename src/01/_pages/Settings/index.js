be3b32d1502730307e473d7b26be9aadaf065874
Iskander Karimov <proenix575@gmail.com>
2021-06-10 19:14:30 +0300

    Fixes

--- src/01/_pages/Settings/index.js
+++ src/01/_pages/Settings/index.js
@@ -127,7 +127,7 @@ export const Settings = () => {
   };
 
   const addStaffButton = {
-    title: 'Добавить сотрудника'
+    title: 'Добавить сотрудника',
     cb: addStaffButtonClicked,
     show: needShowButton('staff'),
     color: 'default',
