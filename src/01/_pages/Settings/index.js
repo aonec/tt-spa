19edcd6c6907f09c5b2d97665933d2ef3b607ac2
Iskander Karimov <proenix575@gmail.com>
2021-06-18 14:35:27 +0300

    Fix reloading

--- src/01/_pages/Settings/index.js
+++ src/01/_pages/Settings/index.js
@@ -29,7 +29,7 @@ export const Settings = () => {
     getCurrentManagingFirm().then((res) => {
       setFirm(res);
     });
-  }, [firm]);
+  }, []);
 
   function showStaff() {
     setStaff(true);
