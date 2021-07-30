105d4f8f358ee5543ec13097ec68da1eea782687
Iskander Karimov <proenix575@gmail.com>
2021-07-30 17:04:46 +0300

    Fixes

--- src/01/features/housingStocks/displayHousingStocks/components/HousingStockFilter/HousingStockFilter.tsx
+++ src/01/features/housingStocks/displayHousingStocks/components/HousingStockFilter/HousingStockFilter.tsx
@@ -1,37 +1,43 @@
-import { input } from '01/r_comp';
+import { Flex } from '01/shared/ui/Layout/Flex';
 import React from 'react';
-import styled from 'reshadow/macro';
+import styled from 'styled-components';
 import { useFilter } from './useFilter.hook';
 
 export const HousingStockFilter = () => {
   const { filterFields, setValue } = useFilter();
 
-  return styled(input)`
-    filter {
-      margin-bottom: 15px;
-      margin-right: 10px;
-      grid-column: 1 / -1;
-      display: grid;
-      grid-template-columns:
-        minmax(100px, 1fr)
-        minmax(100px, 2fr)
-        minmax(100px, 0.5fr)
-        minmax(100px, 0.5fr);
-      grid-gap: 16px;
-    }
-  `(
-    <filter as="div">
-      {filterFields?.map((field) => (
-        <input_frame key={field.name}>
-          <input
-            key={field.name}
-            value={field.value}
-            onChange={(e) => setValue(field.name, e.target.value)}
-            type="text"
-            placeholder={field.placeholder}
-          />
-        </input_frame>
-      ))}
-    </filter>
+  return (
+    <FieldsWrap>
+      <StyledInput placeholder="Название улицы" />
+      <StyledInput placeholder="дом" />
+      <StyledInput placeholder="Корпус" />
+    </FieldsWrap>
   );
 };
+
+const FieldsWrap = styled(Flex)``;
+const StyledInput = styled.input`
+  border: 1px solid lightgray;
+  margin-left: 15px;
+  padding: 3px 15px;
+  :first-child {
+    margin-left: 0;
+  }
+  box-sizing: border-box;
+  border-radius: 4px;
+
+  --active: var(--primary-100);
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    grid-gap: 8px;
+    border: 1px solid var(--frame);
+    border-radius: 4px;
+    position: relative;
+    height: var(--h-norm);
+    background: #fff;
+    z-index: 10;
+    color: var(--main-80);
+    overflow: hidden;
+    padding: 0 8px;
+    cursor: text;
+`;
--- src/01/features/housingStocks/displayHousingStocks/components/HousingStockFilter/useFilter.hook.ts
+++ src/01/features/housingStocks/displayHousingStocks/components/HousingStockFilter/useFilter.hook.ts
@@ -1,26 +1,18 @@
-import { getArrayByCountRange } from '01/_pages/MetersPage/components/utils';
 import { useState } from 'react';
-const fieldNames = ['City', 'Street', 'HousingStockNumber', 'Corpus'];
-const fieldPlaceholders = ['Город', 'Название улицы', 'Дом', 'Корпус'];
 
 export const useFilter = () => {
-  const [filterFields, setFilterFields] = useState(
-    getArrayByCountRange(fieldNames.length, (index) => ({
-      name: fieldNames[index],
-      value: '',
-    }))
-  );
+  const [filterFields, setFilterFields] = useState({
+    City: '',
+    Street: '',
+    HousingStockNumber: '',
+    Corpus: '',
+  });
 
   const setValue = (name: string, value: string) =>
-    setFilterFields((prev) =>
-      prev.map((elem) => (elem.name === name ? { ...elem, value } : elem))
-    );
+    setFilterFields((prev) => ({ ...prev, [name]: value }));
 
   return {
-    filterFields: filterFields.map((elem, index) => ({
-      ...elem,
-      placeholder: fieldPlaceholders[index],
-    })),
+    filterFields,
     setValue,
   };
 };
