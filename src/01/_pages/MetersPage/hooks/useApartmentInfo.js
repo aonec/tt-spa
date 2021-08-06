32f7051ad8963f98268d5fb7a4d8d7f7e38fb55c
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2021-08-06 09:49:05 +0300

    Add corpus

--- src/01/_pages/MetersPage/hooks/useApartmentInfo.js
+++ src/01/_pages/MetersPage/hooks/useApartmentInfo.js
@@ -1,9 +1,12 @@
 export const useApartmentInfo = ({ apartInfo = {} }) => {
   const { housingStock = {}, apartmentNumber, homeowners = [] } = apartInfo;
+  console.log(apartInfo);
   return {
     title:
       apartmentNumber &&
-      `${housingStock.street}, ${housingStock.number}, кв.${apartmentNumber}`,
+      `${housingStock.street}, ${housingStock.number}, кв.${apartmentNumber} ${
+        housingStock.corpus ? `, к.${housingStock.corpus}` : ''
+      }`,
     userInfo: [
       ['Собственник', homeowners[0]?.firstName],
       ['Лицевой счет', homeowners[0]?.personalAccountNumber],
