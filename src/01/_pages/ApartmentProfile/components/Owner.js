f79edc6a77bb477f720f77f51278f83212f9088c
Evgeny Gorelov <evgenyi.gorelov@ya.ru>
2021-08-18 15:36:05 +0300

    Update Owner.js
    Компонент "Справки о ЛС"

--- src/01/_pages/ApartmentProfile/components/Owner.js
+++ src/01/_pages/ApartmentProfile/components/Owner.js
@@ -9,20 +9,24 @@ class ComponentToPrint extends React.Component {
   render() {
     const {certificate} = this.props;
     const {fullName, address, device} = certificate;
-    const {city, street, houseNumber, apartmentNumber} = address;
+    const {city, street, housingStockNumber, apartmentNumber} = address;
+
+    const nowDate = new Date();
+    const months = [" января "," февраля "," марта "," апреля "," июня "," июля "," августа "," сентября "," октября "," ноября "," декабря "];
+    const strDate = nowDate.getDate() + months[nowDate.getMonth() - 1] + nowDate.getFullYear(); 
     return (
        <div className="messageBox" style={{left: '454px', top: '66px', padding: '12px 24px', }}>
           <div style={{textAlign: 'center', paddingBottom: '12px'}}>Справка о лицевом счете</div>
           <div style={{height: '1px', backgroundColor: 'rgb(204, 204, 204)', marginBottom: '12px'}} />
           <div style={{textAlign: 'left'}}>
             <div style={{fontWeight: 'bold', paddingBottom: '9px'}}>СПРАВКА №______________</div>
-            <div>от <span style={{textDecoration: 'underline'}}>16 августа 2021г.</span></div>
+            <div>от <span style={{textDecoration: 'underline'}}>{strDate}</span></div>
             <div style={{paddingTop: '12px', fontWeight: 'bold', textAlign: 'center'}}>от абонентского отдела<br />АО "НК-Инжиниринг"</div>
             <div style={{paddingTop: '24px', float: 'left'}}>дана: </div>
-            <div style={{paddingTop: '24px', borderBottom: '1px solid black', marginLeft: '80px'}}>{fullName}</div>
+            <div style={{paddingTop: '24px', borderBottom: '1px solid black', marginLeft: '80px'}}><input type={'text'} defaultValue={fullName}/></div>
             <div style={{clear: 'both', float: 'none', width: '0px', padding: '0px', margin: '0px'}} />
-            <div style={{paddingTop: '12px'}}>проживающей(ему) по адресу: <span style={{textDecoration: 'underline'}}> {city} </span>, ул.<span style={{textDecoration: 'underline'}}> {street} </span>, д.<span style={{textDecoration: 'underline'}}> {houseNumber} </span>, кв.<span style={{textDecoration: 'underline'}}> {apartmentNumber} </span></div>
-            <div style={{paddingTop: '12px'}}>о том, что показания квартирных приборов учета на <span style={{textDecoration: 'underline'}}>16 августа 2021г.</span> составляют:</div>
+            <div style={{paddingTop: '12px'}}>проживающей(ему) по адресу: <span style={{textDecoration: 'underline'}}> {city} </span>, ул.<span style={{textDecoration: 'underline'}}> {street} </span>, д.<span style={{textDecoration: 'underline'}}> {housingStockNumber} </span>, кв.<span style={{textDecoration: 'underline'}}> {apartmentNumber} </span></div>
+            <div style={{paddingTop: '12px'}}>о том, что показания квартирных приборов учета на <span style={{textDecoration: 'underline'}}>{strDate}</span> составляют:</div>
             <table cellSpacing="0px" style={{marginTop: '12px', borderTop: '1px solid black', borderRight: '1px solid black', borderBottom: 'none', borderLeft: '1px solid black', borderImage: 'initial', width: '100%'}}>
               <tbody>
                 <tr style={{fontWeight: 'bold'}}>
@@ -37,10 +41,10 @@ class ComponentToPrint extends React.Component {
                   <tr>
                     <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>{x.resource}</td>
                     <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>{x.mountPlace}</td>
-                    <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>{x.title}</td>
+                    <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>{x.model}</td>
                     <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>{x.serialNumber}</td>
-                    <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>570</td>
-                    <td style={{padding: '6px', borderBottom: '1px solid black'}}>1.08.2021г.</td>
+                    <td style={{padding: '6px', borderRight: '1px solid black', borderBottom: '1px solid black'}}>{x.reading}</td>
+                    <td style={{padding: '6px', borderBottom: '1px solid black'}}>{x.readingDate}</td>
                   </tr>
                 ))}
               </tbody>
@@ -98,8 +102,10 @@ const Owner = (props) => {
         <div>
         <Certificate certificate={{ 
           fullName: firstName, 
-          address:{city: 'Нижнекамск', street: 'Тихая Аллея', houseNumber: '4А', apartmentNumber: '143'},
-          device:[{resource: 'ХВС', mountPlace: 'ТУ', title: 'СГВ', serialNumber: '123456'},{resource: 'ГВС', mountPlace: 'ТУ', title: 'СГВ', serialNumber: '456789'},{resource: 'ЭЭ', mountPlace: '--', title: 'СГВ', serialNumber: '987321'}]}}/>
+          address:{city: 'Нижнекамск', street: 'Тихая Аллея', housingStockNumber: '4А', apartmentNumber: '143'},
+          device:[{resource: 'ХВС', mountPlace: 'ТУ', model: 'СГВ', serialNumber: '123456', reading: '12.34', readingDate: '01.08.2021'},
+                  {resource: 'ГВС', mountPlace: 'ТУ', model: 'СГВ', serialNumber: '456789', reading: '87.69', readingDate: '01.08.2021'},
+                  {resource: 'Электроэнергия', mountPlace: '--', model: 'Меркурий', serialNumber: '987321', reading: 'День: 15444 Ночь: 5369', readingDate: '01.08.2021'}]}}/>
         </div>
       </TitleWrap>
       <ListItem>
