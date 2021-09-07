de2ef82655ab3b5f3700f078e387c43d95ceddec
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2021-09-07 14:21:58 +0300

    Update

--- src/01/features/individualDevices/closeIndividualDevice/index.tsx
+++ src/01/features/individualDevices/closeIndividualDevice/index.tsx
@@ -3,9 +3,12 @@ import { ErrorMessage } from '01/features/contractors/addContractors';
 import { useFilesUpload } from '01/hooks/useFilesUpload';
 import { DragAndDrop } from '01/shared/ui/DragAndDrop';
 import { FilesList } from '01/shared/ui/FilesList';
+import { Flex } from '01/shared/ui/Layout/Flex';
+import { Space } from '01/shared/ui/Layout/Space/Space';
 import { Header, ModalText, StyledModal } from '01/shared/ui/Modal/Modal';
 import { ButtonTT, DatePickerTT } from '01/tt-components';
-import { Form } from 'antd';
+import { StyledSelect } from '01/_pages/IndividualDeviceEdit/components/IndividualDeviceEditForm';
+import { Form, Select } from 'antd';
 import { Footer } from 'antd/lib/layout/layout';
 import confirm from 'antd/lib/modal/confirm';
 import { useForm } from 'effector-forms/dist';
@@ -13,6 +16,7 @@ import { useStore } from 'effector-react';
 import moment from 'moment';
 import React from 'react';
 import styled from 'styled-components';
+import { clousingReasons } from '../switchIndividualDevice/components/stages/BaseInfoStage';
 import {
   $closingIndividualDevice,
   $isCloseIndividualDeviceModalOpen,
@@ -80,10 +84,13 @@ export const CloseIndividualDeviceModal = () => {
         </Footer>
       }
     >
-      <ModalText>
-        <Form.Item label="Дата снятия прибора с учета">
+      <Grid style={{ width: '100%', marginBottom: -5 }}>
+        <Form.Item
+          label="Дата снятия прибора с учета"
+          style={{ width: '100%' }}
+        >
           <DatePickerTT
-            style={{ borderRadius: '4px' }}
+            style={{ borderRadius: '4px', width: '100%' }}
             value={
               fields.clousingDate.value
                 ? moment(fields.clousingDate.value)
@@ -100,17 +107,43 @@ export const CloseIndividualDeviceModal = () => {
             })}
           </ErrorMessage>
         </Form.Item>
+        <Form.Item label="Причина замены" style={{ width: '100%' }}>
+          <StyledSelect
+            placeholder="Выберите причину замены"
+            value={fields.closingReason.value || undefined}
+            onChange={fields.closingReason.onChange as any}
+          >
+            {Object.entries(clousingReasons).map(([key, elem]) => (
+              <Select.Option value={key} key={key}>
+                {elem}
+              </Select.Option>
+            ))}
+          </StyledSelect>
+          <ErrorMessage>
+            {fields.closingReason.errorText({
+              required: 'Это поле обязательное',
+            })}
+          </ErrorMessage>
+        </Form.Item>
+      </Grid>
+      {!!fields.documentIds.value.length && (
         <FilesList files={fields.documentIds.value} removeFile={removeFile} />
-        <DragAndDrop
-          style={{ marginTop: 15 }}
-          uniqId="close-individual-device"
-          fileHandler={(files) => addFile(files[0])}
-          text="Добавьте акт снятия прибора с учета"
-        />
-      </ModalText>
+      )}
+      <DragAndDrop
+        style={{ marginTop: !!fields.documentIds.value.length ? 15 : 0 }}
+        uniqId="close-individual-device"
+        fileHandler={(files) => addFile(files[0])}
+        text="Добавьте акт снятия прибора с учета"
+      />
     </StyledModal>
   );
 };
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 15px;
+`;
 
 const UnderModalTitleText = styled.div`
   font-size: 14px;
