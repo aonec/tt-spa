5a04dd70aee7f6e012d4f18329977b4492730af3
Iskander Karimov <proenix575@gmail.com>
2021-09-10 15:02:36 +0300

    Fix margins

--- src/01/tt-components/Icon/index.jsx
+++ src/01/tt-components/Icon/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styledReshadow from 'reshadow/macro';
 import icons from '../../assets/icons.json';
 import { darkIcons } from '01/_components/Icon/Icon';
 import { Space } from '01/shared/ui/Layout/Space/Space';
@@ -19,17 +18,24 @@ export const Icon = ({ size = 16, icon = '', dark = false, ...props }) => {
   }
 
   return (
-    <Space>
-      <svg
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        {...props}
-        width={size}
-        height={size}
-      >
-        <path as="path" clipRule="evenodd" fillRule="evenodd" d={icons[icon]} />
-      </svg>
-    </Space>
+    <Flex style={{ transform: 'translateY(2px)', marginRight: -7 }}>
+      <Space>
+        <svg
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          {...props}
+          width={size}
+          height={size}
+        >
+          <path
+            as="path"
+            clipRule="evenodd"
+            fillRule="evenodd"
+            d={icons[icon]}
+          />
+        </svg>
+      </Space>
+    </Flex>
   );
 };
 
