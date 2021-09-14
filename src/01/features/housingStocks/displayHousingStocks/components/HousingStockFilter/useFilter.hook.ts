542dcc99f0d5c7e59f1a610da900c957edcc5563
Iskander Karimov <proenix575@gmail.com>
2021-09-14 11:34:34 +0300

    Update

--- src/01/features/housingStocks/displayHousingStocks/components/HousingStockFilter/useFilter.hook.ts
+++ src/01/features/housingStocks/displayHousingStocks/components/HousingStockFilter/useFilter.hook.ts
@@ -1,7 +1,4 @@
-import { toArray } from '01/features/individualDevices/addIndividualDevice/components/CheckFormValuesModal';
-import { useDebounce } from '01/hooks/useDebounce';
-import { useState, useEffect } from 'react';
-import { filterFieldHasBeenChanged } from '../../models';
+import { useState } from 'react';
 
 export const filterValuesInit = {
   City: '',
@@ -20,16 +17,6 @@ export const useFilter = () => {
 
   const setValue = (name: string, value: string) =>
     setFilterFields((prev) => ({ ...prev, [name]: value }));
-
-  const debouncedValue = useDebounce(filterFields, 500);
-  const valuesArray = toArray(debouncedValue, false)
-  const valuesString = valuesArray.join()
-
-  useEffect(() => {
-    if (toArray(filterFields).some(Boolean)) {
-      filterFieldHasBeenChanged(filterFields);
-    }
-  }, [valuesString]);
 
   return {
     filterFields,
