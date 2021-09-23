7031944fbd46d9f2925563b577e333cfb0b50443
pronix575 <proenix575@gmsil.com>
2021-09-24 01:19:53 +0300

    Add values

--- src/01/features/readings/accountingNodesReadings/components/MeteringDevicesList/index.tsx
+++ src/01/features/readings/accountingNodesReadings/components/MeteringDevicesList/index.tsx
@@ -3,9 +3,11 @@ import {
   fetchNodes,
   $electricNodes,
 } from '01/features/nodes/displayNodes/models';
+import { getPreviousReadingsMonth } from '01/shared/lib/readings/getPreviousReadingsMonth';
 import { Flex } from '01/shared/ui/Layout/Flex';
 import { Grid } from '01/shared/ui/Layout/Grid';
 import { PendingLoader } from '01/shared/ui/PendingLoader';
+import Arrow from '01/_components/Arrow/Arrow';
 import { useStore } from 'effector-react';
 import React from 'react';
 import styled from 'styled-components';
@@ -14,20 +16,27 @@ import { MeteringDeviceReadingsLine } from '../MeteringDeviceReadingsLine';
 export const MeteringDevicesList = () => {
   const pendingNodes = useStore(fetchNodes.pending);
   const electicNodes = useStore($electricNodes);
-  const { sliderIndex } = useSliderIndex()
+  const { sliderIndex, up, down, canDown, canUp } = useSliderIndex();
 
   const header = (
-    <Header temp={gridTemp} gap="15px">
+    <Header temp={gridTemp} gap="15px" style={{ userSelect: 'none' }}>
       <HeaderTitleElem>Прибор</HeaderTitleElem>
       <HeaderTitleElem>Коэф. трансф.</HeaderTitleElem>
       <HeaderTitleElem>
         <MonthSlider>
-          <div>Январь</div>
+          <ArrowContainer onClick={up}>{canUp && <Arrow />}</ArrowContainer>
+          <div>{getPreviousReadingsMonth(sliderIndex)}</div>
+          <ArrowContainer
+            onClick={down}
+            style={{ transform: 'rotate(180deg)' }}
+          >
+            {canDown && <Arrow />}
+          </ArrowContainer>
         </MonthSlider>
       </HeaderTitleElem>
       <HeaderTitleElem>
-        <MonthSlider>
-          <div>Февраль</div>
+        <MonthSlider style={{ justifyContent: 'center' }}>
+          <div>{getPreviousReadingsMonth(-1)}</div>
         </MonthSlider>
       </HeaderTitleElem>
       <HeaderTitleElem>Общ. потр.</HeaderTitleElem>
@@ -40,8 +49,8 @@ export const MeteringDevicesList = () => {
       {electicNodes?.length === 0
         ? 'Нет нод'
         : electicNodes?.length
-          ? header
-          : null}
+        ? header
+        : null}
       {electicNodes?.map((node) => (
         <MeteringDeviceReadingsLine sliderIndex={sliderIndex} node={node} />
       ))}
@@ -49,8 +58,12 @@ export const MeteringDevicesList = () => {
   );
 };
 
+const ArrowContainer = styled.div`
+  cursor: pointer;
+`;
+
 const MonthSlider = styled(Flex)`
-  justify-content: center;
+  justify-content: space-between;
 `;
 
 export const gridTemp = '0.9fr 0.38fr 0.6fr 0.6fr 0.35fr 0.75fr 20px';
