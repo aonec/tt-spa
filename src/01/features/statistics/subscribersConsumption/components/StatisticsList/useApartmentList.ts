1bb2b82b00d51d7771149a76c533059a90927a49
Iskander Karimov <proenix575@gmail.com>
2021-10-14 17:41:58 +0300

    Add filtration by resource

--- src/01/features/statistics/subscribersConsumption/components/StatisticsList/useApartmentList.ts
+++ src/01/features/statistics/subscribersConsumption/components/StatisticsList/useApartmentList.ts
@@ -8,17 +8,55 @@ import _ from 'lodash';
 type IterateArrayCallback<T> = ((apartment: T) => any)[];
 
 export function useApartmentList() {
-  const apartmentsList = useStore($consumptionStatistics);
+  const apartmentList = useStore($consumptionStatistics);
   const { fields } = useForm(subscribersConsumptionFilterForm);
 
   const sortCallbacks: IterateArrayCallback<SubscriberStatisticsСonsumptionResponse> = [];
 
   const filterCallbacks: IterateArrayCallback<SubscriberStatisticsСonsumptionResponse> = [];
 
-  //   if (fields.coldOpen && fields.cold) filterCallbacks.push((apartment) => apartment.)
+  if (fields.coldOpen.value && fields.cold.value.from && fields.cold.value.to) {
+    filterCallbacks.push(
+      (apartment) =>
+        apartment.coldWaterSupplyСonsumption &&
+        apartment.coldWaterSupplyСonsumption >=
+          Number(fields.cold.value.from) &&
+        apartment.coldWaterSupplyСonsumption <= Number(fields.cold.value.to)
+    );
+  }
+
+  if (fields.heatOpen.value && fields.heat.value.from && fields.heat.value.to) {
+    filterCallbacks.push(
+      (apartment) =>
+        apartment.hotWaterSupplyСonsumption &&
+        apartment.hotWaterSupplyСonsumption >= Number(fields.cold.value.from) &&
+        apartment.hotWaterSupplyСonsumption <= Number(fields.heat.value.to)
+    );
+  }
+
+  if (
+    fields.electricityOpen.value &&
+    fields.electricity.value.from &&
+    fields.electricity.value.to
+  ) {
+    filterCallbacks.push(
+      (apartment) =>
+        apartment.electricitySupplyСonsumption &&
+        apartment.electricitySupplyСonsumption >=
+          Number(fields.electricity.value.from) &&
+        apartment.electricitySupplyСonsumption <=
+          Number(fields.electricity.value.to)
+    );
+  }
+
+  const filteredApartmentList = apartmentList?.filter((elem) => {
+    const checks = filterCallbacks.map((callback) => Boolean(callback(elem)));
+
+    return checks.every(Boolean);
+  });
 
   const sortedApartmentList = _.sortBy(
-    apartmentsList,
+    filteredApartmentList,
     [(apartment) => Number(apartment.apartmentNumber), ...sortCallbacks],
     []
   );
