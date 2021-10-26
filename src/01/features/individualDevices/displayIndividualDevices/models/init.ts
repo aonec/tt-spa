3b3b17293a7861cee6f19275af8b5bcca014a5fd
Iskander Karimov <proenix575@gmail.com>
2021-10-26 12:45:29 +0300

    Fix

--- src/01/features/individualDevices/displayIndividualDevices/models/init.ts
+++ src/01/features/individualDevices/displayIndividualDevices/models/init.ts
@@ -1,11 +1,15 @@
 import { getIndividualDevices } from '01/_api/individualDevices';
-import { guard, sample } from 'effector';
+import { forward, guard, sample } from 'effector';
 import {
   $individualDevices,
   $isShownClosedDevices,
+  $pagedIndividualDevices,
   fetchIndividualDevicesFx,
+  fetchNextPageOfIndividualDevices,
+  fetchNextPageOfIndividualDevicesFx,
   hideClosedDevices,
   IndividualDevicesGate,
+  PagedIndividualDevicesGate,
   refetchIndividualDevices,
   resetIndividualDevices,
   showClosedDevices,
@@ -13,13 +17,14 @@ import {
 import { toArray } from '../../addIndividualDevice/components/CheckFormValuesModal';
 
 fetchIndividualDevicesFx.use(getIndividualDevices);
+fetchNextPageOfIndividualDevicesFx.use(getIndividualDevices);
 
 $individualDevices
   .on(fetchIndividualDevicesFx.doneData, (_, devices) => devices)
   .reset(resetIndividualDevices, IndividualDevicesGate.close);
 
 guard({
-  source: IndividualDevicesGate.state.map((elem) => elem),
+  source: IndividualDevicesGate.state,
   clock: IndividualDevicesGate.state,
   filter: (value) => toArray(value).some(Boolean),
   target: fetchIndividualDevicesFx,
@@ -34,3 +39,21 @@ sample({
 $isShownClosedDevices
   .on(showClosedDevices, () => true)
   .reset(hideClosedDevices);
+
+forward({
+  from: PagedIndividualDevicesGate.open,
+  to: fetchNextPageOfIndividualDevices,
+});
+
+sample({
+  clock: fetchNextPageOfIndividualDevices,
+  source: PagedIndividualDevicesGate.state,
+  target: [fetchNextPageOfIndividualDevicesFx],
+});
+
+$pagedIndividualDevices
+  .on(PagedIndividualDevicesGate.close, () => [])
+  .on(fetchNextPageOfIndividualDevicesFx.doneData, (prevElems, nextElems) => [
+    ...prevElems,
+    ...nextElems,
+  ]);
