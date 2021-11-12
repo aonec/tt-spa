d78a2cd0286ab3756095399d0995b18e4614312b
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2021-11-12 09:13:46 +0300

    Fix

--- src/01/_api/homeowners.ts
+++ src/01/_api/homeowners.ts
@@ -12,11 +12,11 @@ import axios from '01/axios';
 export const getHomeownerCertificate = (
   id: number
 ): Promise<HomeownerCertificateResponse> =>
-  axios.get(`HomeownerAccounts/${id}/Certificate`);
+  axios.get(`HomeownerAccount/${id}/Certificate`);
 
 export const getHomeownerAccount = (
   id: string
-): Promise<HomeownerAccountResponse> => axios.get(`HomeownerAccounts/${id}`);
+): Promise<HomeownerAccountResponse> => axios.get(`HomeownerAccount/${id}`);
 
 export const putHomeownerAccount = ({
   id,
@@ -24,20 +24,20 @@ export const putHomeownerAccount = ({
 }: {
   id: string;
   data: HomeownerAccountUpdateRequest;
-}): Promise<void> => axios.put(`HomeownerAccounts/${id}`, data);
+}): Promise<void> => axios.put(`HomeownerAccount/${id}`, data);
 
 export const replaceHomeownerAccount = (
   requestPayload: HomeownerAccountReplaceRequest
-): Promise<void> => axios.post('HomeownerAccounts/Replace', requestPayload);
+): Promise<void> => axios.post('HomeownerAccount/Replace', requestPayload);
 
 export const closeHomeownerAccount = (
   payload: HomeownerAccountCloseRequest
-): Promise<void> => axios.post('HomeownerAccounts/Close', payload);
+): Promise<void> => axios.post('HomeownerAccount/Close', payload);
 
 export const addHomeowner = (
   reqestPayload: HomeownerAccountCreateServiceModel
-): Promise<void> => axios.post('HomeownerAccounts', reqestPayload);
+): Promise<void> => axios.post('HomeownerAccount', reqestPayload);
 
 export const splitHomeownerAccount = (
   data: HomeownerAccountSplitRequest
-): Promise<void> => axios.post('HomeownerAccounts/Split', data);
+): Promise<void> => axios.post('HomeownerAccount/Split', data);
