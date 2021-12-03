e25a28025b1fd2701d359264e7425200b4714aab
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2021-12-03 11:13:16 +0300

    Add checking dates to reading history modal

--- src/01/features/readings/displayReadingHistory/components/Header.tsx
+++ src/01/features/readings/displayReadingHistory/components/Header.tsx
@@ -3,11 +3,12 @@ import { HousingStockAddress } from '01/features/individualDevices/addIndividual
 import { $individualDevice } from '01/features/individualDevices/displayIndividualDevice/models';
 import { DeviceDataString } from '01/features/individualDevices/switchIndividualDevice/components/DeviceDataString';
 import { Flex } from '01/shared/ui/Layout/Flex';
-import { Space } from '01/shared/ui/Layout/Space/Space';
+import { Space, Spaces } from '01/shared/ui/Layout/Space/Space';
 import { Breadcrumb } from '01/tt-components';
 import { IsActiveBool } from '01/tt-components/IsActive';
 import { HeaderWrap, Title } from '01/_components/Headers';
 import { useStore } from 'effector-react';
+import moment from 'moment';
 import React from 'react';
 
 interface Props {
@@ -17,6 +18,8 @@ interface Props {
 export const ReadingHistoryHeader: React.FC<Props> = ({ isModal }) => {
   const device = useStore($individualDevice);
   const address = useApartmentAddressString();
+  const checkingDates = useDeviceCheckingDates();
+
   return (
     <>
       <HeaderWrap
@@ -29,13 +32,14 @@ export const ReadingHistoryHeader: React.FC<Props> = ({ isModal }) => {
           {!isModal && <Breadcrumb />}
           <div style={{ marginTop: 5 }}>
             {!isModal && <Title>История показаний</Title>}
-            <Flex>
-              <DeviceDataString />
-              <Space />
-              <IsActiveBool active={device?.closingDate === null} />
-              <Space />
-              {address}
-            </Flex>
+            <Spaces flex>
+              {[
+                <DeviceDataString />,
+                <IsActiveBool active={device?.closingDate === null} />,
+                address,
+                <b>{checkingDates}</b>,
+              ]}
+            </Spaces>
           </div>
         </div>
       </HeaderWrap>
@@ -48,8 +52,19 @@ export function useApartmentAddressString() {
 
   return (
     apartment &&
-    `ул. ${apartment.housingStock?.street}, д. ${apartment.housingStock?.number}${
-      apartment.housingStock?.corpus || ''
-    }, кв. ${apartment.apartmentNumber}`
+    `ул. ${apartment.housingStock?.street}, д. ${
+      apartment.housingStock?.number
+    }${apartment.housingStock?.corpus || ''}, кв. ${apartment.apartmentNumber}`
+  );
+}
+
+export function useDeviceCheckingDates() {
+  const device = useStore($individualDevice);
+
+  return (
+    device &&
+    `${moment(device.lastCheckingDate).format('DD.MM.YYYY')} — ${moment(
+      device.futureCheckingDate
+    ).format('DD.MM.YYYY')}`
   );
 }
