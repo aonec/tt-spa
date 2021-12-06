2c8fff390d79f495cc8c36b185902ccd29cbb7f0
Iskander Karimov <proenix575@gmail.com>
2021-12-06 14:03:54 +0300

    Fix dates

--- src/01/features/apartments/displayProblemDevices/models/init.ts
+++ src/01/features/apartments/displayProblemDevices/models/init.ts
@@ -1,16 +1,28 @@
-import { getProblemDevices, GetProblemDevicesRequestPayload } from './../../../../_api/apartments';
+import { getProblemDevices } from './../../../../_api/apartments';
 import {
   ProblemDevicesGate,
   fetchProblemDevicesFx,
   $problemDevices,
 } from './index';
 import { forward } from 'effector';
+import moment from 'moment';
 
 fetchProblemDevicesFx.use(getProblemDevices);
 
 $problemDevices.on(fetchProblemDevicesFx.doneData, (_, devices) => devices);
 
 forward({
-  from: ProblemDevicesGate.state,
+  from: ProblemDevicesGate.state.map((values) => ({
+    ...values,
+    requestPayload: {
+      ...values.requestPayload,
+      fromDate:
+        values.requestPayload?.fromDate &&
+        moment(values.requestPayload?.fromDate).format('YYYY-MM-DD'),
+      toDate:
+        values.requestPayload?.toDate &&
+        moment(values.requestPayload?.toDate).format('YYYY-MM-DD'),
+    },
+  })),
   to: fetchProblemDevicesFx,
 });
