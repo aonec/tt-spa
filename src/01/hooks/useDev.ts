60f87d67a5179162a35f79441dc766a8bd15844e
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2021-12-23 11:11:28 +0300

    Fix

--- src/01/hooks/useDev.ts
+++ src/01/hooks/useDev.ts
@@ -1,9 +1,10 @@
 export function useIsDev() {
-
   const isDev =
     process.env.NODE_ENV === 'development' ||
     process.env.REACT_APP_API_URL ===
-      'https://transparent-staging.herokuapp.com/api';
+      'https://transparent-staging.herokuapp.com/api' ||
+    process.env.REACT_APP_API_URL ===
+      'https://management.transparent-technology.ru:1443/api/HousingStocks';
 
   return isDev;
 }
