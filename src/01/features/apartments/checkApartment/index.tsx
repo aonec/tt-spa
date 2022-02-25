fe5a387286c5fc058a5d063a7fb7f4eb96d83cf2
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2022-02-25 14:02:47 +0300

    Update

--- src/01/features/apartments/checkApartment/index.tsx
+++ src/01/features/apartments/checkApartment/index.tsx
@@ -23,6 +23,10 @@ import moment from 'moment';
 import { ErrorMessage } from '01/features/contractors/addContractors';
 import { FilesList } from '01/shared/ui/FilesList';
 import { combine } from 'effector';
+import {
+  ApartmentActTypesGate,
+  $actTypes,
+} from '../../actsJournal/displayActTypes/models';
 
 export const CheckApartmentModal = () => {
   const visible = useStore($isCheckApartmentModalOpen);
@@ -47,101 +51,106 @@ export const CheckApartmentModal = () => {
     []
   );
 
+  const actTypes = useStore($actTypes);
+
   return (
-    <ModalTT
-      title={isEditOpen ? 'Редактировать проверку' : 'Создать проверку'}
-      visible={visible || isEditOpen}
-      onCancel={closeCheckApartmentModal}
-      saveBtnText={isEditOpen ? 'Сохранить изменения' : 'Создать проверку'}
-      onSubmit={submit}
-      loading={pending}
-    >
-      <Grid temp="1fr 1fr 1fr" gap="15px">
-        <Form.Item label="Дата проверки">
-          <DatePickerTT
-            format="DD.MM.YYYY"
-            onChange={fields.checkingDate.onChange as any}
-            value={
-              fields.checkingDate.value
-                ? moment(fields.checkingDate.value)
-                : null
-            }
-          />
-          <ErrorMessage>
-            {fields.checkingDate.errorText({
-              required: 'Это поле обязательное',
-            })}
-          </ErrorMessage>
-        </Form.Item>
-        <Form.Item label="Тип проверки">
-          <StyledSelect
-            placeholder="Выберите тип проверки"
-            value={fields.checkType.value || undefined}
-            onChange={fields.checkType.onChange as any}
-          >
-            <StyledSelect.Option
-              value={ECheckType.Planned}
-              key={ECheckType.Planned}
+    <>
+      <ApartmentActTypesGate />
+      <ModalTT
+        title={isEditOpen ? 'Редактировать проверку' : 'Создать проверку'}
+        visible={visible || isEditOpen}
+        onCancel={closeCheckApartmentModal}
+        saveBtnText={isEditOpen ? 'Сохранить изменения' : 'Создать проверку'}
+        onSubmit={submit}
+        loading={pending}
+      >
+        <Grid temp="1fr 1fr 1fr" gap="15px">
+          <Form.Item label="Дата проверки">
+            <DatePickerTT
+              format="DD.MM.YYYY"
+              onChange={fields.checkingDate.onChange as any}
+              value={
+                fields.checkingDate.value
+                  ? moment(fields.checkingDate.value)
+                  : null
+              }
+            />
+            <ErrorMessage>
+              {fields.checkingDate.errorText({
+                required: 'Это поле обязательное',
+              })}
+            </ErrorMessage>
+          </Form.Item>
+          <Form.Item label="Тип проверки">
+            <StyledSelect
+              placeholder="Выберите тип проверки"
+              value={fields.checkType.value || undefined}
+              onChange={fields.checkType.onChange as any}
             >
-              Плановая
-            </StyledSelect.Option>
-            <StyledSelect.Option
-              value={ECheckType.Unplanned}
-              key={ECheckType.Unplanned}
-            >
-              Внеплановая
-            </StyledSelect.Option>
-          </StyledSelect>
-          <ErrorMessage>
-            {fields.checkType.errorText({
-              required: 'Это поле обязательное',
-            })}
-          </ErrorMessage>
-        </Form.Item>
-        <Form.Item label="Номер документа">
-          <InputTT
-            value={fields.registryNumber.value}
-            onChange={(e: SyntheticEvent<HTMLInputElement>) =>
-              fields.registryNumber.onChange(e.currentTarget.value)
-            }
-            placeholder="Введите номер"
-          />
-          <ErrorMessage>
-            {fields.registryNumber.errorText({
-              required: 'Это поле обязательное',
-            })}
-          </ErrorMessage>
-        </Form.Item>
-      </Grid>
-      {payload?.checkingAct ? (
-        <FilesList
-          files={[
-            {
-              id: 432,
-              fileResponse: payload?.checkingAct,
-              onRemove: () => {
-                fields.documentIds.onChange([]);
-                clearPayloadFile();
+              <StyledSelect.Option
+                value={ECheckType.Planned}
+                key={ECheckType.Planned}
+              >
+                Плановая
+              </StyledSelect.Option>
+              <StyledSelect.Option
+                value={ECheckType.Unplanned}
+                key={ECheckType.Unplanned}
+              >
+                Внеплановая
+              </StyledSelect.Option>
+            </StyledSelect>
+            <ErrorMessage>
+              {fields.checkType.errorText({
+                required: 'Это поле обязательное',
+              })}
+            </ErrorMessage>
+          </Form.Item>
+          <Form.Item label="Номер документа">
+            <InputTT
+              value={fields.registryNumber.value}
+              onChange={(e: SyntheticEvent<HTMLInputElement>) =>
+                fields.registryNumber.onChange(e.currentTarget.value)
+              }
+              placeholder="Введите номер"
+            />
+            <ErrorMessage>
+              {fields.registryNumber.errorText({
+                required: 'Это поле обязательное',
+              })}
+            </ErrorMessage>
+          </Form.Item>
+        </Grid>
+        {payload?.checkingAct ? (
+          <FilesList
+            files={[
+              {
+                id: 432,
+                fileResponse: payload?.checkingAct,
+                onRemove: () => {
+                  fields.documentIds.onChange([]);
+                  clearPayloadFile();
+                },
               },
-            },
-          ]}
-        />
-      ) : (
-        <FilesUpload
-          text="Добавьте заключение проверки"
-          uniqId="check-apartments"
-          filesInit={fields.documentIds.value}
-          onChange={(value) => {
-            fields.documentIds.onChange(value);
-          }}
-          max={1}
-        />
-      )}
-      <ErrorMessage>
-        {fields.documentIds.errorText({
-          required: 'Это поле обязательное',
-        })}
-      </ErrorMessage>
-    </ModalTT>
+            ]}
+          />
+        ) : (
+          <FilesUpload
+            text="Добавьте заключение проверки"
+            uniqId="check-apartments"
+            filesInit={fields.documentIds.value}
+            onChange={(value) => {
+              fields.documentIds.onChange(value);
+            }}
+            max={1}
+          />
+        )}
+        <ErrorMessage>
+          {fields.documentIds.errorText({
+            required: 'Это поле обязательное',
+          })}
+        </ErrorMessage>
+      </ModalTT>
+    </>
   );
 };
