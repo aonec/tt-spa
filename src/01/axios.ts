3f1b5c92cd3d7b1794dd2405d390ce79515ca804
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2022-03-11 19:28:41 +0300

    Update

--- src/01/axios.ts
+++ src/01/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { createEvent, createStore } from 'effector';
 
+
 const devUrl = 'https://management.staging.transparent-technology.ru/';
 const baseURL = process.env.REACT_APP_API_URL || devUrl;
 
--- src/01/axiosWithHeaders.ts
+++ src/01/axiosWithHeaders.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 const devUrl = 'https://management.staging.transparent-technology.ru/';
 const baseURL = process.env.REACT_APP_API_URL || devUrl;
 
@@ -12,7 +13,7 @@ axiosWithHeaders.interceptors.request.use((req) => {
   if (req.baseURL === 'http://84.201.132.164:8080/api') {
     delete req.headers.Authorization;
   } else {
-    
+
     req.headers.Authorization = `Bearer ${takeFromLocStor('token')}`;
   }
 
