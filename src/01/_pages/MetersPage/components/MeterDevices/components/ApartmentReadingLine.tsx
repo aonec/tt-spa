d75f351a621199fa14da4dcf34388b8d122b82f4
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2022-04-13 19:52:46 +0300

    remove unused styled components

--- src/01/_pages/MetersPage/components/MeterDevices/components/ApartmentReadingLine.tsx
+++ src/01/_pages/MetersPage/components/MeterDevices/components/ApartmentReadingLine.tsx
@@ -315,45 +315,4 @@ export const DeviceReadingsContainer = styled.div`
   }
 `;
 
-const Footer = styled.div`
-  background-color: var(--bg);
-  height: 96px;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding-right: 32px;
-  font-weight: 700;
-`;
-
-const Header = styled.h1`
-  font-size: 32px;
-  line-height: 1.5;
-  font-weight: 300;
-  margin: 0;
-`;
-
-const StyledModal = styled(Modal)`
-  .ant-modal-header {
-    padding: 24px 32px;
-    border: 0;
-  }
-
-  .ant-modal-body {
-    padding: 0 32px 32px 32px;
-  }
-
-  .ant-modal-footer {
-    padding: 0;
-  }
-
-  .ant-modal-close-x {
-    fill: var(--main-100);
-  }
-
-  .ant-modal-footer button + button {
-    margin-bottom: 0;
-    margin-left: 16px;
-  }
-`;
-
 export default ApartmentReadingLine;
