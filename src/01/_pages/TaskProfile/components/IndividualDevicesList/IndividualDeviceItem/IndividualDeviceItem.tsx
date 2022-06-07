84025db79a8824e7d90ecc21c0199b7dee0ab8be
Iskander Karimov <proenix575@gmail.com>
2022-06-07 15:14:34 +0300

    [R2-1935]: Refactor

--- src/01/_pages/TaskProfile/components/IndividualDevicesList/IndividualDeviceItem/IndividualDeviceItem.tsx
+++ src/01/_pages/TaskProfile/components/IndividualDevicesList/IndividualDeviceItem/IndividualDeviceItem.tsx
@@ -1,6 +1,6 @@
+import { actResourceNamesLookup } from '01/_pages/ApartmentProfile/components/ChecksHistory/Components/CheckHistoryDocument/components/ResourceInfo/ResourceInfo.utils';
 import moment from 'moment';
-import { EActResourceType } from 'myApi';
-import React, { FC, useState } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { ChevronDown, ChevronUp } from 'react-bootstrap-icons';
 import { IndividualDeviceInfo } from 'ui-kit/shared_components/IndividualDeviceInfo';
 import { ReadingsHistoryButton } from 'ui-kit/shared_components/reading_history_button';
@@ -16,14 +16,6 @@ import {
 } from './IndividualDeviceItem.styled';
 import { IndividualDeviceItemProps } from './IndividualDeviceItem.types';
 
-export const actResourceTypeNames: { [key: string]: string } = {
-  [EActResourceType.ColdWaterSupply]: 'Холодная вода',
-  [EActResourceType.HotWaterSupply]: 'Горячая вода',
-  [EActResourceType.Electricity]: 'Электричество',
-  [EActResourceType.Heat]: 'Тепло',
-  [EActResourceType.All]: 'Все',
-};
-
 export const IndividualDeviceItem: FC<IndividualDeviceItemProps> = ({
   device,
 }) => {
@@ -36,46 +28,56 @@ export const IndividualDeviceItem: FC<IndividualDeviceItemProps> = ({
   const objectId = device.address?.id;
   const apartmentId = device.address?.apartmentId;
 
-  const deviceInfos = [
-    {
-      title: 'Тип ресурса',
-      value: actResourceTypeNames[device.resource],
+  const deviceInfos = useMemo(
+    () => {
+
+      if (isOpen) return null 
+      
+      return [
+        {
+          title: 'Тип ресурса',
+          value: actResourceNamesLookup[device.resource],
+        },
+        {
+          title: 'Место установки',
+          value: device.mountPlace?.description,
+        },
+        {
+          title: 'Разрядность',
+          value: device.bitDepth,
+        },
+        {
+          title: 'Множитель',
+          value: device.scaleFactor,
+        },
+        {
+          title: 'Дата ввода в эксплуатацию',
+          value:
+            device.openingDate &&
+            moment(device.openingDate).format('DD.MM.YYYY'),
+        },
+        {
+          title: 'Дата начальной поверки',
+          value: moment(device.lastCheckingDate).format('DD.MM.YYYY'),
+        },
+        {
+          title: 'Дата следующей поверки',
+          value: moment(device.futureCheckingDate).format('DD.MM.YYYY'),
+        },
+      ]
     },
-    {
-      title: 'Место установки',
-      value: device.mountPlace?.description,
-    },
-    {
-      title: 'Разрядность',
-      value: device.bitDepth,
-    },
-    {
-      title: 'Множитель',
-      value: device.scaleFactor,
-    },
-    {
-      title: 'Дата ввода в эксплуатацию',
-      value:
-        device.openingDate && moment(device.openingDate).format('DD.MM.YYYY'),
-    },
-    {
-      title: 'Дата начальной поверки',
-      value: moment(device.lastCheckingDate).format('DD.MM.YYYY'),
-    },
-    {
-      title: 'Дата следующей поверки',
-      value: moment(device.futureCheckingDate).format('DD.MM.YYYY'),
-    },
-  ];
+    [device]
+  );
 
   const deviceExtendedInfo = (
     <div>
-      {deviceInfos.map(({ title, value }) => (
-        <DeviceInfoItem>
-          <DeviceInfoItemLabel>{title}:</DeviceInfoItemLabel>
-          <div>{value || '—'}</div>
-        </DeviceInfoItem>
-      ))}
+      {deviceInfos &&
+        deviceInfos.map(({ title, value }) => (
+          <DeviceInfoItem>
+            <DeviceInfoItemLabel>{title}:</DeviceInfoItemLabel>
+            <div>{value || '—'}</div>
+          </DeviceInfoItem>
+        ))}
     </div>
   );
 
