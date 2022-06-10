010d749adc5e0f01bc4f1f870594848611b08ed1
Rinasik <mubarakshin199999@mail.ru>
2022-06-10 14:55:10 +0300

    [R2-1590]: Fiz zero display in actsList

--- src/services/apartments/apartmentActsListService/view/ApartmentActsList.tsx
+++ src/services/apartments/apartmentActsListService/view/ApartmentActsList.tsx
@@ -14,17 +14,17 @@ export const ApartmentActsList: FC<ApartmentActsListProps> = ({
   handleOpeningCreateActModal,
   handleOpeningDeleteActModal,
   handleOpeningEditActModal,
-  actTypes
+  actTypes,
 }) => {
-  const isShowactsList = acts && acts.length && !isLoading;
+  const isShowActsList = Boolean(acts?.length && !isLoading);
 
   const actsList = useMemo(
     () =>
-      acts &&
-      acts.map((act) => (
+      Boolean(acts?.length) &&
+      acts?.map((act) => (
         <ApartmentActItem
           act={act}
-          actTypes= {actTypes}
+          actTypes={actTypes}
           openDeleteActModal={handleOpeningDeleteActModal}
           openEditActModal={handleOpeningEditActModal}
         />
@@ -35,30 +35,33 @@ export const ApartmentActsList: FC<ApartmentActsListProps> = ({
   return (
     <>
       <CreateApartmentActContainer />
-      <EditApartmentActContainer/>
+      <EditApartmentActContainer />
       <DeleteApartmentActContainer />
       {isLoading && <PendingLoader loading={isLoading} />}
-      {acts && !acts.length && !isLoading && (
+      {!acts?.length && !isLoading && (
         <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Нет актов" />
       )}
-      {isShowactsList && (
-        <Wrapper>
-          <ListHeader>
-            <div>Дата</div>
-            <div>№ акта</div>
-            <div>Название документа</div>
-            <div>Ресурс</div>
-            <div>Тип</div>
-          </ListHeader>
-          {actsList}
-          <AddButton
-            className="ant-btn-link"
-            onClick={handleOpeningCreateActModal}
-          >
-            + Добавить акт
-          </AddButton>
-        </Wrapper>
-      )}
+
+      <Wrapper>
+        {isShowActsList && (
+          <>
+            <ListHeader>
+              <div>Дата</div>
+              <div>№ акта</div>
+              <div>Название документа</div>
+              <div>Ресурс</div>
+              <div>Тип</div>
+            </ListHeader>
+            {actsList}
+          </>
+        )}
+        <AddButton
+          className="ant-btn-link"
+          onClick={handleOpeningCreateActModal}
+        >
+          + Добавить акт
+        </AddButton>
+      </Wrapper>
     </>
   );
 };
