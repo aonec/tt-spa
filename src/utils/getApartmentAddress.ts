9d002b739400285305e5acd8714796bf2240a2b4
Rinasik <mubarakshin199999@mail.ru>
2022-06-27 14:43:53 +0300

    [R2-2046]: Add corpus in objects list profile

--- src/utils/getApartmentAddress.ts
+++ src/utils/getApartmentAddress.ts
@@ -1,11 +1,14 @@
-import { ApartmentResponse } from 'myApi';
+import { ApartmentListResponse, ApartmentResponse } from 'myApi';
 
-export const getApartmentAddressString = (apartment: ApartmentResponse) => {
+export const getApartmentAddressString = (
+  apartment: ApartmentResponse | ApartmentListResponse,
+  isCityNeeded?: boolean
+) => {
   const housingStock = apartment.housingStock;
 
   const address = housingStock?.address?.mainAddress;
+  const city = isCityNeeded ? address?.city : '';
+  const corpusText = address?.corpus ? ` корпус ${address?.corpus},` : '';
 
-  return `${address?.city} ул. ${address?.street}, д. ${address?.number}, кв. ${
-    apartment.apartmentNumber
-  }${address?.corpus || ''}`;
+  return `${city} ул. ${address?.street}, д. ${address?.number},${corpusText} кв. ${apartment.apartmentNumber}`;
 };
