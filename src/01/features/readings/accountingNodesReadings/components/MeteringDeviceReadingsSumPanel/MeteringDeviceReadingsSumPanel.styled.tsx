be41a020a93b3591d0bea5a4f30cd3b0838b5902
Rinasik <mubarakshin199999@mail.ru>
2022-06-28 15:25:05 +0300

    [R2-1640]: Add background blur in sumPanel

--- src/01/features/readings/accountingNodesReadings/components/MeteringDeviceReadingsSumPanel/MeteringDeviceReadingsSumPanel.styled.tsx
+++ src/01/features/readings/accountingNodesReadings/components/MeteringDeviceReadingsSumPanel/MeteringDeviceReadingsSumPanel.styled.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   position: relative;
+  height: 64px;
 `;
 
 export const Panel = styled.div`
@@ -10,8 +11,9 @@ export const Panel = styled.div`
   height: 64px;
   left: 208px;
   width: calc(100% - 208px);
-  background-color: white;
+  background-color: #ffffff74;
   box-shadow: 0px -4px 8px rgba(78, 93, 146, 0.16);
+  backdrop-filter: blur(3px);
 
   display: flex;
   align-items: center;
