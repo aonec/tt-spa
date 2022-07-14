2ce657f7ba5aa6dd8fb6c21ac9a30c2851dd2b3d
Nikita Karpushev <83674486+Nikita99dev@users.noreply.github.com>
2022-07-14 21:22:53 +0300

    [R2-2053] refactoring

--- src/services/tasks/exportTasksListService/view/ExportTasksListForm/index.ts
+++ src/services/tasks/exportTasksListService/view/ExportTasksListForm/index.ts
@@ -1,2 +1,1 @@
 export { ExportTasksListForm } from './ExportTasksListForm'
-console.log('a')
--- src/services/tasks/tasksProfileService/view/SearchTasks/SearchTasks.tsx
+++ src/services/tasks/tasksProfileService/view/SearchTasks/SearchTasks.tsx
@@ -93,7 +93,6 @@ export const SearchTasks: FC<SearchTasksProps> = ({
   return (
     <ExtendedSearch
       isOpen={isExtendedSearchOpen}
-      // disabled={!(currentFilter?.GroupType === 'Archived')}
       handleApply={handleSubmit}
       handleClear={clearAllFilters}
       handleClose={closeExtendedSearch}
