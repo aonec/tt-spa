99830577a505af866d8677e5a7838efb1fb7e922
Nikita Karpushev <83674486+Nikita99dev@users.noreply.github.com>
2022-07-15 12:19:33 +0300

    [R2-2084] phase-add

--- src/services/devices/ChangeODPUService/view/ChangeODPUPage/ChangeODPUPage.tsx
+++ src/services/devices/ChangeODPUService/view/ChangeODPUPage/ChangeODPUPage.tsx
@@ -11,10 +11,16 @@ import { PageHeader } from '01/shared/ui/PageHeader';
 import { GoBack } from 'ui-kit/shared_components/GoBack';
 import { ResourceIconLookup } from 'ui-kit/shared_components/ResourceIconLookup';
 import { getHousingStockAddress } from 'utils/getHousingStockAddress';
+import { ElectricityPhases } from './Phases/Phases';
 
 export const ChangeODPUPage: FC<ChangeODPUPageProps> = ({ oldDevice }) => {
   const { resource, address, serialNumber, model } = oldDevice || {};
   const hosuingstockAddress = address && getHousingStockAddress(address, true);
+  let status = {
+    first: true,
+    second: false,
+    third: true,
+  };
 
   return (
     <Wrapper>
@@ -24,6 +30,15 @@ export const ChangeODPUPage: FC<ChangeODPUPageProps> = ({ oldDevice }) => {
         <AddressWrapper>{hosuingstockAddress}</AddressWrapper>
         {resource && <ResourceIconLookup resource={resource} />}
         <SerialNumberWrapper>{serialNumber}</SerialNumberWrapper>
+
+        {/* {
+          <ElectricityPhases
+            setFieldValue={(a, b) => console.log('b')}
+            values={'a'}
+            phasesStatus={status}
+            amountOfPhases={3}
+          />
+        } */}
         <ModelWrapper>{model}</ModelWrapper>
       </TitleWrapper>
     </Wrapper>
