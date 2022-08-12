954736643eaa695da61451eebfb8faf035a45430
Iskander Karimov <proenix575@gmail.com>
2022-08-12 12:33:35 +0300

    [R2-2229]: Fix imports

--- src/services/meters/individualDeviceMetersInputService/individualDeviceMetersInputService.container.tsx
+++ src/services/meters/individualDeviceMetersInputService/individualDeviceMetersInputService.container.tsx
@@ -2,7 +2,7 @@ import { message } from 'antd';
 import { useEvent, useStore } from 'effector-react';
 import moment from 'moment';
 import { IndividualDeviceReadingsResponse } from 'myApi';
-import React, { FC, useCallback, useEffect, useMemo } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import { individualDeviceMetersInputService } from './individualDeviceMetersInputService.model';
 import {
   CompareReadingsStatus,
