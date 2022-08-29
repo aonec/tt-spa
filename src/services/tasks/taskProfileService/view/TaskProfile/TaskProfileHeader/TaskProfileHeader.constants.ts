4671ee24af10b8d10a9423c2187d02845ed44405
Rinasik <mubarakshin199999@mail.ru>
2022-08-29 19:16:02 +0300

    [R3-141]: Add hex colors instead sass styles

--- src/services/tasks/taskProfileService/view/TaskProfile/TaskProfileHeader/TaskProfileHeader.constants.ts
+++ src/services/tasks/taskProfileService/view/TaskProfile/TaskProfileHeader/TaskProfileHeader.constants.ts
@@ -1,8 +1,9 @@
-import { TimerClosingStatus } from "ui-kit/shared_components/Timer/Timer.types";
+import { TimerClosingStatus } from 'ui-kit/shared_components/Timer/Timer.types';
 
-export const LineColors : {[key in keyof typeof TimerClosingStatus]: string}= {
-    [TimerClosingStatus.ClosedAutomatically]: 'var(--bg)',
-    [TimerClosingStatus.Done]: 'var(--success)',
-    [TimerClosingStatus.Overdue]: 'var(--error)',
-
+export const LineColors: {
+  [key in keyof typeof TimerClosingStatus]: string;
+} = {
+  [TimerClosingStatus.ClosedAutomatically]: '#f3f5f6',
+  [TimerClosingStatus.Done]: '#17b45a',
+  [TimerClosingStatus.Overdue]: '#fc525b',
 };
