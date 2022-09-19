346a0c51cdec585e60133011d48bc033a1bde482
Iskander Karimov <proenix575@gmail.com>
2022-09-19 16:08:18 +0300

    Fix date in archives

--- src/01/_api/node_readings_page.ts
+++ src/01/_api/node_readings_page.ts
@@ -24,8 +24,8 @@ export const requestNodeReadings = async (
 ): Promise<RequestNodeReadingsFunctionInterface> => {
   const params = {
     ...searchQuery,
-    from: searchQuery.from.add(moment().utcOffset(), 'minute').toISOString(true),
-    to: searchQuery.to.add(moment().utcOffset(), 'minute').toISOString(true),
+    from: searchQuery.from.add(moment().utcOffset(), 'minute').toISOString(),
+    to: searchQuery.to.add(moment().utcOffset(), 'minute').toISOString(),
   };
 
   const readings = await axios.get<any, ReadingsInterface>('Reports/Archives', {
