39d66fa9349c1f39a9eac7633b8567d642b0e4e5
Iskander Karimov <proenix575@gmail.com>
2022-09-27 16:01:02 +0300

    [R3-407]: Fixes

--- src/01/_pages/EditNode/components/Modals/ModalAddDevice/ModalAddDeviceForm.tsx
+++ src/01/_pages/EditNode/components/Modals/ModalAddDevice/ModalAddDeviceForm.tsx
@@ -255,22 +255,6 @@ const ModalAddDeviceForm = ({
                   />
                 </Form.Item>
 
-                {/*<Form.Item*/}
-                {/*  name="calculatorId"*/}
-                {/*  label="Вычислитель, к которому подключен прибор"*/}
-                {/*  style={styles.w49}*/}
-                {/*>*/}
-                {/*  <InputFormik name="calculatorId" disabled />*/}
-                {/*</Form.Item>*/}
-
-                {/*<Form.Item*/}
-                {/*  name="entryNumber"*/}
-                {/*  label="Номер ввода"*/}
-                {/*  style={styles.w100}*/}
-                {/*>*/}
-                {/*  <InputFormik name="entryNumber" disabled />*/}
-                {/*</Form.Item>*/}
-
                 <Form.Item name="number" label="Номер узла" style={styles.w49}>
                   <InputFormik name="number" disabled />
                 </Form.Item>
@@ -363,6 +347,11 @@ const ModalAddDeviceForm = ({
                     name="lastCheckingDate"
                     format="DD.MM.YYYY"
                     allowClear={false}
+                    onChange={(date) => {
+                      if (!date) return;
+
+                      setFieldValue('futureCheckingDate', date.add('year', 4));
+                    }}
                   />
                 </Form.Item>
 
--- src/01/_pages/EditNode/components/Modals/ModalAddDevice/apiModalAddDevice.ts
+++ src/01/_pages/EditNode/components/Modals/ModalAddDevice/apiModalAddDevice.ts
@@ -1,5 +1,6 @@
 import axios from '../../../../../axios';
 import { CreatePipeHousingMeteringDeviceRequest } from '../../../../../../myApi';
+import { message } from 'antd';
 
 export async function addHousingMeteringDevice(
   form: CreatePipeHousingMeteringDeviceRequest
@@ -9,6 +10,8 @@ export async function addHousingMeteringDevice(
     alert('ОДПУ успешно создан !');
     return res;
   } catch (error) {
+    message.error((error as any)?.response?.data?.error?.Text);
+    console.log({ ...(error as object) });
     throw {
       resource: 'device',
       message: 'Произошла ошибка добавления ОДПУ',
