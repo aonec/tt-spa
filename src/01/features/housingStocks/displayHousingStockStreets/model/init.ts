24cf3de584df11908f122a4bc300880cbb5871d0
Rinasik <mubarakshin199999@mail.ru>
2022-09-29 01:29:10 +0300

    [R3-393]: reloadReload page after logout

--- src/01/features/housingStocks/displayHousingStockStreets/model/init.ts
+++ src/01/features/housingStocks/displayHousingStockStreets/model/init.ts
@@ -1,12 +1,19 @@
 import { getExistingStreets } from '01/_api/existingStreets';
-import { forward } from 'effector';
+import { guard, sample } from 'effector';
 import { $existingStreets, ExistingStreetsGate, fetchExistingStreets } from '.';
 
 fetchExistingStreets.use(getExistingStreets);
 
 $existingStreets.on(fetchExistingStreets.doneData, (_, payload) => payload);
 
-forward({
-  from: ExistingStreetsGate.state.map((values) => values),
-  to: fetchExistingStreets,
+sample({
+  source: guard({
+    source: $existingStreets,
+    filter: (streets) => !Boolean(streets.length),
+  }),
+  clock: ExistingStreetsGate.state.map((values) => values),
+  fn: (_, params) => {
+    return params;
+  },
+  target: fetchExistingStreets,
 });
