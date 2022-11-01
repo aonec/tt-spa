7487110126f9dd0fe8d65622e99e768a210986d5
Iskander Karimov <proenix575@gmail.com>
2022-11-01 23:27:56 +0300

    [R3-459]: Update

--- src/services/devices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/individualDevicesViewByAddressService/view/IndividualDevicesAddressSearch/IndividualDevicesAddressSearch.tsx
+++ src/services/devices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/individualDevicesViewByAddressService/view/IndividualDevicesAddressSearch/IndividualDevicesAddressSearch.tsx
@@ -1,12 +1,25 @@
 import { ExtendedSearch } from '01/shared/ui/ExtendedSearch';
 import React, { FC } from 'react';
+import { AddressSearchContainer } from 'services/addressSearchService';
+import { SearchFieldType } from 'services/addressSearchService/view/AddressSearch/AddressSearch.types';
 import { Wrapper } from './IndividualDevicesAddressSearch.styled';
 import { IndividualDevicesAddressSearchProps } from './IndividualDevicesAddressSearch.types';
 
 export const IndividualDevicesAddressSearch: FC<IndividualDevicesAddressSearchProps> = ({}) => {
   return (
     <Wrapper>
-      <ExtendedSearch />
+      <ExtendedSearch>
+        <AddressSearchContainer
+          fields={[
+            SearchFieldType.City,
+            SearchFieldType.Street,
+            SearchFieldType.House,
+            SearchFieldType.Corpus,
+            SearchFieldType.Apartment,
+          ]}
+          handleSubmit={console.log}
+        />
+      </ExtendedSearch>
     </Wrapper>
   );
 };
