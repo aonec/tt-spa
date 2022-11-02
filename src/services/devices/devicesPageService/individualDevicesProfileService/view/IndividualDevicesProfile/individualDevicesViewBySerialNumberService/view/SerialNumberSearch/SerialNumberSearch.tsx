7f6dd0b45a46978f7e6499c9f1631d2bb7ddb770
Rinasik <mubarakshin199999@mail.ru>
2022-11-02 21:01:28 +0300

    [R3-523]: Add handling enter key down

--- src/services/devices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/individualDevicesViewBySerialNumberService/view/SerialNumberSearch/SerialNumberSearch.tsx
+++ src/services/devices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/individualDevicesViewBySerialNumberService/view/SerialNumberSearch/SerialNumberSearch.tsx
@@ -1,10 +1,11 @@
+import { useSwitchInputOnEnter } from '01/features/individualDevices/switchIndividualDevice/components/stages/BaseInfoStage.hook';
 import { fromEnter } from '01/shared/ui/DatePickerNative';
 import { ExtendedSearch } from '01/shared/ui/ExtendedSearch';
 import { InputSC, SelectSC } from '01/shared/ui/Fields';
 import { Checkbox } from 'antd';
 import { useFormik } from 'formik';
 import { EActResourceType, EApartmentStatus } from 'myApi';
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { SearchIcon } from 'ui-kit/icons';
 import { ResourceInfo } from 'ui-kit/shared_components/ResourceInfo';
 import { IndividualDeviceSearchbySerialNumberPayload } from '../../individualDevicesViesBySerialNumberService.types';
@@ -15,6 +16,8 @@ export const IndividualDevicesViewBySerialNumberSearch: FC<IndividualDevicesView
   filter,
   setFilter,
 }) => {
+  const next = useSwitchInputOnEnter('searchBySerialNumber', true);
+
   const {
     values,
     setFieldValue,
@@ -30,22 +33,41 @@ export const IndividualDevicesViewBySerialNumberSearch: FC<IndividualDevicesView
     onSubmit: setFilter,
   });
 
+  const handleEnter = useCallback(
+    (index: number) =>
+      fromEnter(() => {
+        next(1);
+        handleSubmit();
+      }),
+    [next, handleSubmit]
+  );
+
   return (
     <Wrapper>
       <ExtendedSearch />
       <SearchFieldsWrapper>
         <InputSC
+          data-reading-input={'searchBySerialNumber'}
           prefix={<SearchIcon />}
           type="number"
           placeholder="Введите серийный номер пробера"
           onChange={(e) => setFieldValue('SerialNumber', e.target.value)}
           value={values.SerialNumber}
-          onKeyDown={fromEnter(handleSubmit)}
+          onKeyDown={fromEnter(() => {
+            handleSubmit();
+            next(0);
+          })}
         />
         <SelectSC
+          data-reading-input={'searchBySerialNumber'}
           placeholder="Ресурс"
           value={values.Resource || EActResourceType.All}
-          onChange={(value) => setFieldValue('Resource', value)}
+          onChange={(value) => {
+            setFieldValue('Resource', value);
+            handleSubmit();
+          }}
+          onKeyDown={handleEnter(1)}
+          showAction={['focus']}
         >
           {Object.values(EActResourceType).map((elem) => (
             <SelectSC.Option key={elem} value={elem}>
@@ -54,9 +76,15 @@ export const IndividualDevicesViewBySerialNumberSearch: FC<IndividualDevicesView
           ))}
         </SelectSC>
         <SelectSC
+          data-reading-input={'searchBySerialNumber'}
           placeholder="Статус кв."
           value={values.ApartmentStatus}
-          onChange={(value) => setFieldValue('ApartmentStatus', value)}
+          onChange={(value) => {
+            setFieldValue('ApartmentStatus', value);
+            handleSubmit();
+          }}
+          onKeyDown={fromEnter(() => next(2))}
+          showAction={['focus']}
         >
           {Object.values(EApartmentStatus).map((elem) => (
             <SelectSC.Option key={elem} value={elem}>
@@ -66,7 +94,10 @@ export const IndividualDevicesViewBySerialNumberSearch: FC<IndividualDevicesView
         </SelectSC>
         <Checkbox
           value={values.IsAlsoClosing}
-          onChange={(e) => setFieldValue('IsAlsoClosing', e.target.checked)}
+          onChange={(e) => {
+            setFieldValue('IsAlsoClosing', e.target.checked);
+            handleSubmit();
+          }}
           style={{
             color: '#272f5ae5',
             fontWeight: 500,
