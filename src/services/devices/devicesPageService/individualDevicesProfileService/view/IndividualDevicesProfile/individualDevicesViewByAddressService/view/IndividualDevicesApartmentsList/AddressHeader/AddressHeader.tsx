0db7bdf8e9db5aa1916caf99202a0dab2e67f2e8
Iskander Karimov <pronix432@gmail.com>
2022-11-17 17:49:31 +0300

    [R3-571]: Add Swithcers

--- src/services/devices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/individualDevicesViewByAddressService/view/IndividualDevicesApartmentsList/AddressHeader/AddressHeader.tsx
+++ src/services/devices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/individualDevicesViewByAddressService/view/IndividualDevicesApartmentsList/AddressHeader/AddressHeader.tsx
@@ -1,5 +1,13 @@
 import React, { FC } from 'react';
-import { Wrapper } from './AddressHeader.styled';
+import { getHousingStockAddressString } from 'utils/getHousingStockAddress';
+import {
+  ChevronLeft,
+  ChevronRight,
+  CurrentHousingStock,
+  HousingStocksSwitcher,
+  SwitcherWrapper,
+  Wrapper,
+} from './AddressHeader.styled';
 import { AddressHeaderProps } from './AddressHeader.types';
 
 export const AddressHeader: FC<AddressHeaderProps> = ({ housingsByFilter }) => {
@@ -7,8 +15,24 @@ export const AddressHeader: FC<AddressHeaderProps> = ({ housingsByFilter }) => {
 
   return (
     <Wrapper>
-      {address?.city}, ул. {address?.street} {address?.houseNumber}
-      {address?.houseCorpus}
+      <CurrentHousingStock>
+        {address?.city}, ул. {address?.street} {address?.houseNumber}
+        {address?.houseCorpus}
+      </CurrentHousingStock>
+      <SwitcherWrapper>
+        {housingsByFilter.previous?.address && (
+          <HousingStocksSwitcher>
+            <ChevronLeft />
+            {getHousingStockAddressString(housingsByFilter.previous.address)}
+          </HousingStocksSwitcher>
+        )}
+        {housingsByFilter.next?.address && (
+          <HousingStocksSwitcher>
+            {getHousingStockAddressString(housingsByFilter.next.address)}
+            <ChevronRight />
+          </HousingStocksSwitcher>
+        )}
+      </SwitcherWrapper>
     </Wrapper>
   );
 };
