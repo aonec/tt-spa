b5bc1bcb529f57abfed1154bfb1c547806ec42a2
anotherIlnaz <mmmr.sh.i@gmail.com>
2022-11-23 14:04:43 +0300

    [R-604]: Implement modal windows

--- src/services/objects/createObjectService/view/CreateObjectPage/CreateObjectMainInfoStage/CreateObjectMainInfoStage.styled.ts
+++ src/services/objects/createObjectService/view/CreateObjectPage/CreateObjectMainInfoStage/CreateObjectMainInfoStage.styled.ts
@@ -16,3 +16,18 @@ export const RightButtonBlock = styled.div`
   display: flex;
   align-items: center;
 `;
+
+export const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 4px 20px;
+`;
+
+export const AddTPButton = styled.div`
+  margin-top: 42px;
+  cursor: pointer;
+  color: #189ee9;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 32px;
+`;
--- src/services/objects/createObjectService/view/CreateObjectPage/CreateObjectMainInfoStage/CreateObjectMainInfoStage.tsx
+++ src/services/objects/createObjectService/view/CreateObjectPage/CreateObjectMainInfoStage/CreateObjectMainInfoStage.tsx
@@ -1,43 +1,128 @@
+import { SpaceLine } from '01/shared/ui/Layout/Space/Space';
 import { StyledSelect } from '01/shared/ui/Select/components';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Button } from 'ui-kit/Button';
 import { FormItem } from 'ui-kit/FormItem';
 import { Select } from 'ui-kit/Select';
 import { PageTitle } from '../CreateObjectPage.styled';
 import {
+  AddTPButton,
   ButtonPadding,
   Footer,
+  GridContainer,
   RightButtonBlock,
   Wrapper,
 } from './CreateObjectMainInfoStage.styled';
 import { CreateObjectMainInfoStageProps } from './CreateObjectMainInfoStage.types';
+import { CreateNewHeatingPointModal } from './CreateNewHeatingPointModal/CreateNewHeatingPointModal';
+import { EditNewHeatingPointModal } from './EditNewHeatingPointModal';
 
-export const CreateObjectMainInfoStage: FC<CreateObjectMainInfoStageProps> = ({houseManagements}) => {
-  return (<>
+export const CreateObjectMainInfoStage: FC<CreateObjectMainInfoStageProps> = ({
+  houseManagements,
+}) => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
-    <Wrapper>
-      <PageTitle>Основная информация </PageTitle>
+  return (
+    <>
+      <CreateNewHeatingPointModal
+        isModalOpen={isModalOpen}
+        setModalOpen={setModalOpen}
+      />
+      <EditNewHeatingPointModal />
+      <Wrapper>
+        <PageTitle>Основная информация </PageTitle>
 
-      <FormItem label="Город">
-        <StyledSelect placeholder="Выберите из списка" onChange={(value) => {}}>
-          {houseManagements?.map((houseManagement) => (
-            <Select.Option value={houseManagement}>
-              {houseManagement}
-            </Select.Option>
-          ))}
-        </StyledSelect>
-      </FormItem>
-      <Footer>
-        <Button type="ghost">Назад</Button>
-        <RightButtonBlock>
-          <ButtonPadding>
-            <Button type="ghost">Отмена</Button>
-          </ButtonPadding>
-          <Button sidePadding={25} onClick={() => {}}>
-            Далее
-          </Button>
-        </RightButtonBlock>
-      </Footer>
-    </Wrapper></>
+        <FormItem label="Домоуправления">
+          <StyledSelect
+            placeholder="Выберите из списка"
+            onChange={(value) => {}}
+          >
+            {houseManagements?.map(
+              (houseManagement) =>
+                houseManagement.name && (
+                  <Select.Option value={houseManagement.name}>
+                    {houseManagement.name}
+                  </Select.Option>
+                )
+            )}
+          </StyledSelect>
+        </FormItem>
+
+        <SpaceLine />
+
+        <GridContainer>
+          <FormItem label="Категория объекта">
+            <StyledSelect
+              placeholder="Выберите из списка"
+              onChange={(value) => {}}
+            >
+              {houseManagements?.map(
+                (houseManagement) =>
+                  houseManagement.name && (
+                    <Select.Option value={houseManagement.name}>
+                      {houseManagement.name}
+                    </Select.Option>
+                  )
+              )}
+            </StyledSelect>
+          </FormItem>
+
+          <FormItem label="Тип объекта">
+            <StyledSelect
+              placeholder="Выберите из списка"
+              onChange={(value) => {}}
+            >
+              {houseManagements?.map(
+                (houseManagement) =>
+                  houseManagement.name && (
+                    <Select.Option value={houseManagement.name}>
+                      {houseManagement.name}
+                    </Select.Option>
+                  )
+              )}
+            </StyledSelect>
+          </FormItem>
+        </GridContainer>
+
+        <SpaceLine />
+
+        <GridContainer>
+          <FormItem label="Тепловой пункт">
+            <StyledSelect
+              placeholder="Выберите из списка"
+              onChange={(value) => {}}
+            >
+              {houseManagements?.map(
+                (houseManagement) =>
+                  houseManagement.name && (
+                    <Select.Option value={houseManagement.name}>
+                      {houseManagement.name}
+                    </Select.Option>
+                  )
+              )}
+            </StyledSelect>
+          </FormItem>
+
+          <AddTPButton
+            className="ant-btn-link"
+            onClick={() => setModalOpen((prev) => !prev)}
+          >
+            + Добавить новый ТП
+          </AddTPButton>
+        </GridContainer>
+
+        <Footer>
+          <Button type="ghost">Назад</Button>
+          <RightButtonBlock>
+            <ButtonPadding>
+              <Button type="ghost">Отмена</Button>
+            </ButtonPadding>
+            <Button sidePadding={25} onClick={() => {}}>
+              Далее
+            </Button>
+          </RightButtonBlock>
+        </Footer>
+      </Wrapper>
+    </>
   );
 };
