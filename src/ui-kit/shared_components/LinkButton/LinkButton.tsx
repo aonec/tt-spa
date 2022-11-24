b00097fb3b04d6e0207a5e3fa2b603d301b1d208
Iskander Karimov <pronix432@gmail.com>
2022-11-24 14:56:42 +0300

    [R3-606]: Add link button

--- src/ui-kit/shared_components/LinkButton/LinkButton.tsx
+++ src/ui-kit/shared_components/LinkButton/LinkButton.tsx
@@ -2,6 +2,6 @@ import React, { FC } from 'react';
 import { Wrapper } from './LinkButton.styled';
 import { LinkButtonProps } from './LinkButton.types';
 
-export const LinkButton: FC<LinkButtonProps> = ({ children, onClick }) => {
-  return <Wrapper onClick={onClick}>{children}</Wrapper>;
+export const LinkButton: FC<LinkButtonProps> = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
 };
