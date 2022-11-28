86fcaf07743ef5bfd4453a645a198745a404af65
Iskander Karimov <pronix432@gmail.com>
2022-11-28 15:46:32 +0300

    [R3-606]: Add editing of homeowner

--- src/services/apartments/editApartmentProfileService/view/EditApartmentPage/EditHomeownersList/EditHomeownerForm/EditHomeownerForm.types.ts
+++ src/services/apartments/editApartmentProfileService/view/EditApartmentPage/EditHomeownersList/EditHomeownerForm/EditHomeownerForm.types.ts
@@ -1,6 +1,8 @@
 import { HomeownerAccountCreateServiceModel } from 'myApi';
+import { formInitialValues } from './EditHomeownerForm.constants';
 
 export type EditHomeownerFormProps = {
   formId: string;
   handleSubmit: (data: HomeownerAccountCreateServiceModel) => void;
+  initialValues?: typeof formInitialValues;
 };
--- src/services/apartments/editApartmentProfileService/view/EditApartmentPage/EditHomeownersList/editHomeownerService/editHomeownerService.api.ts
+++ src/services/apartments/editApartmentProfileService/view/EditApartmentPage/EditHomeownersList/editHomeownerService/editHomeownerService.api.ts
@@ -1,8 +1,1 @@
 import { axios } from '01/axios';
-import { EditHomeownerRequestPayload } from './editHomeownerService.types';
-
-export const putHomeowner = ({
-  id,
-  ...payload
-}: EditHomeownerRequestPayload): Promise<void> =>
-  axios.put(`/HomeownerAccounts/${id}`, payload);
