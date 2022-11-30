30a63d3767f0caa65aaf8617dc2824f8e4c05b33
Rinasik <mubarakshin199999@mail.ru>
2022-11-30 23:51:07 +0300

    [R3-592]: Add node statistics table column

--- src/services/displayNodesStatisticsService/displayNodesStatisticsService.container.tsx
+++ src/services/displayNodesStatisticsService/displayNodesStatisticsService.container.tsx
@@ -3,10 +3,12 @@ import { GraphView } from '01/_pages/Graph/components/GraphView';
 import { Skeleton } from 'antd';
 import { useEvent, useStore } from 'effector-react';
 import React, { FC, useMemo } from 'react';
+import { WithLoader } from 'ui-kit/shared_components/WithLoader';
 import { displayNodesStatisticsService } from './displayNodesStatisticsService.model';
 import { Title, Wrapper } from './displayNodesStatisticsService.styled';
 import { DisplayNodesStatisticsContainerProps } from './displayNodesStatisticsService.types';
 import { GraphEmptyData } from './view/GraphEmptyData';
+import { NodeStatisticsTable } from './view/NodeStatisticsTable';
 
 const { inputs, outputs, gates } = displayNodesStatisticsService;
 const { NodeInfoGate } = gates;
@@ -51,19 +53,25 @@ export const DisplayNodesStatisticsContainer: FC<DisplayNodesStatisticsContainer
           setArchiveFilter={setArchiveFilter}
         />
 
-        {isLoading && <Skeleton active />}
-        {!isLoading && archiveData && archiveReadingExist && (
-          <>
-            <Wrapper>
-              <GraphView
-                graphParam={graphType}
-                data={archiveData}
+        <WithLoader isLoading={isLoading}>
+          {archiveData && archiveReadingExist && (
+            <>
+              <Wrapper>
+                <GraphView
+                  graphParam={graphType}
+                  data={archiveData}
+                  reportType={currentArhiveFilter.ReportType}
+                />
+              </Wrapper>
+              <NodeStatisticsTable
+                graphType={graphType}
                 reportType={currentArhiveFilter.ReportType}
+                archiveData={archive}
               />
-            </Wrapper>
-          </>
-        )}
-        {!isLoading && !archiveReadingExist && <GraphEmptyData />}
+            </>
+          )}
+          {!archiveReadingExist && <GraphEmptyData />}
+        </WithLoader>
       </div>
     </>
   );
