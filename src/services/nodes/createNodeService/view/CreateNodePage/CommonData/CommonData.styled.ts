d1b94ea1a472c0e3b589614a2aea97a93382f867
Iskander Karimov <pronix432@gmail.com>
2022-12-01 17:38:31 +0300

    [R3-650]

--- src/services/nodes/createNodeService/view/CreateNodePage/CommonData/CommonData.styled.ts
+++ src/services/nodes/createNodeService/view/CreateNodePage/CommonData/CommonData.styled.ts
@@ -1,3 +1,23 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const FirstLineWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 0.3fr;
+  grid-gap: 16px;
+`;
+
+export const SecondLineWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+`;
+
+export const ThirdLineWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+`;
+
+export const FilesUploaderWrapper = styled.div`
+    margin-top: 16px;
+`;
--- src/services/nodes/createNodeService/view/CreateNodePage/CommonData/CommonData.tsx
+++ src/services/nodes/createNodeService/view/CreateNodePage/CommonData/CommonData.tsx
@@ -1,12 +1,57 @@
 import React, { FC } from 'react';
 import { Button } from 'ui-kit/Button';
+import { DatePicker } from 'ui-kit/DatePicker';
+import { DocumentsUploadContainer } from 'ui-kit/DocumentsService';
+import { FormItem } from 'ui-kit/FormItem';
+import { Input } from 'ui-kit/Input';
+import { Select } from 'ui-kit/Select';
+import { Title } from 'ui-kit/Title';
 import { Footer } from '../CreateNodePage.styled';
-import { Wrapper } from './CommonData.styled';
+import {
+  FilesUploaderWrapper,
+  FirstLineWrapper,
+  SecondLineWrapper,
+  ThirdLineWrapper,
+} from './CommonData.styled';
 import { CommonDataProps } from './CommonData.types';
 
 export const CommonData: FC<CommonDataProps> = ({ goPrevStep }) => {
   return (
-    <Wrapper>
+    <div>
+      <Title>Общие данные об узле</Title>
+      <FirstLineWrapper>
+        <FormItem label="Ресурс">
+          <Select placeholder="Выберите"></Select>
+        </FormItem>
+        <FormItem label="Номер узла">
+          <Input placeholder="Введите" />
+        </FormItem>
+      </FirstLineWrapper>
+      <SecondLineWrapper>
+        <FormItem label="Зона">
+          <Select placeholder="Выберите"></Select>
+        </FormItem>
+      </SecondLineWrapper>
+      <FormItem label="Коммерческий учет показателей приборов">
+        <Select placeholder="Выберите"></Select>
+      </FormItem>
+      <ThirdLineWrapper>
+        <FormItem label="Дата начала действия акта-допуска">
+          <DatePicker format="DD.MM.YYYY" placeholder="Введите дату" />
+        </FormItem>
+        <FormItem label="Дата окончания действия акта-допуска">
+          <DatePicker format="DD.MM.YYYY" placeholder="Введите дату" />
+        </FormItem>
+      </ThirdLineWrapper>
+      <FilesUploaderWrapper>
+        <DocumentsUploadContainer
+          label="Добавьте акт-допуска"
+          documents={[]}
+          uniqId="edit-apartment-act-form"
+          onChange={() => {}}
+          max={1}
+        />
+      </FilesUploaderWrapper>
       <Footer>
         <Button type="ghost" onClick={goPrevStep}>
           Назад
@@ -15,6 +60,6 @@ export const CommonData: FC<CommonDataProps> = ({ goPrevStep }) => {
           Далее
         </Button>
       </Footer>
-    </Wrapper>
+    </div>
   );
 };
