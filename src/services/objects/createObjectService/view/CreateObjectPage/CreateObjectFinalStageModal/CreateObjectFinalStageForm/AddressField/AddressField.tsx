b45c2dedc55e4358c36fc67ee030fc8d3731fd76
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2022-12-09 00:37:18 +0700

    [R3-626]: fix addresses field

--- src/services/objects/createObjectService/view/CreateObjectPage/CreateObjectFinalStageModal/CreateObjectFinalStageForm/AddressField/AddressField.tsx
+++ src/services/objects/createObjectService/view/CreateObjectPage/CreateObjectFinalStageModal/CreateObjectFinalStageForm/AddressField/AddressField.tsx
@@ -3,22 +3,16 @@ import { Field, SpacedIndex, SpacesHouseNumber } from './AddressField.styled';
 import { AddressFieldProps } from './AddressField.types';
 
 export const AddressField: FC<AddressFieldProps> = ({ createObjectData }) => {
+  if (!createObjectData) {
+    return (<Field>-</Field>);
+  }
+
   return (
-    <>
-      {createObjectData ? (
-        <Field>
-          {createObjectData.city}, ул. {createObjectData.street},
-          <SpacesHouseNumber>{createObjectData.house}</SpacesHouseNumber>
-          {createObjectData.corpus ? `к. ${createObjectData.corpus} ` : ''}
-          {createObjectData.index ? (
-            <SpacedIndex>({createObjectData?.index})</SpacedIndex>
-          ) : (
-            ''
-          )}
-        </Field>
-      ) : (
-        <Field>-</Field>
-      )}
-    </>
+    <Field>
+      {createObjectData.city}, ул. {createObjectData.street},
+      <SpacesHouseNumber>{createObjectData.house}</SpacesHouseNumber>
+      {createObjectData.corpus && `к. ${createObjectData.corpus} `}
+      {createObjectData?.index && <SpacedIndex>({createObjectData?.index})</SpacedIndex>}
+    </Field>
   );
 };
