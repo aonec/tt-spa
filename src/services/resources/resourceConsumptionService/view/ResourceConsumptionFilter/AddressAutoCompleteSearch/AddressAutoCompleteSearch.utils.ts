9ffde65367177c52a35b348b8d0b94f17b9d3010
Rinasik <mubarakshin199999@mail.ru>
2022-12-12 20:17:19 +0300

    [R3-668]: Add sort options by similarity

--- src/services/resources/resourceConsumptionService/view/ResourceConsumptionFilter/AddressAutoCompleteSearch/AddressAutoCompleteSearch.utils.ts
+++ src/services/resources/resourceConsumptionService/view/ResourceConsumptionFilter/AddressAutoCompleteSearch/AddressAutoCompleteSearch.utils.ts
@@ -9,24 +9,32 @@ export const prepareOptionsForAutoComplete = (
     return [];
   }
   const similarOptions =
-    streets?.filter((elem) =>
+    streets.filter((elem) =>
       Boolean(countSimilarityPoints(addressSearch, elem.addressString))
     ) || [];
 
-  const sortOptionsBySimilarity = similarOptions?.sort((a, b) => {
-    const aPoints = countSimilarityPoints(addressSearch, a.addressString);
-    const bPoints = countSimilarityPoints(addressSearch, b.addressString);
+  const sortedOptionsBySimilarity = similarOptions.sort((a, b) =>
+    sortOptionsBySimilarity(a, b, addressSearch)
+  );
 
-    if (aPoints < bPoints) return 1;
-
-    if (aPoints > bPoints) return -1;
-
-    return 0;
-  });
-
-  const firstMostSimilarOptions = sortOptionsBySimilarity.slice(0, 5);
+  const firstMostSimilarOptions = sortedOptionsBySimilarity.slice(0, 5);
 
   return firstMostSimilarOptions.map((street) => ({
     value: street.addressString,
   }));
+};
+
+const sortOptionsBySimilarity = (
+  a: AddressWithSearchString,
+  b: AddressWithSearchString,
+  addressSearch: string
+) => {
+  const aPoints = countSimilarityPoints(addressSearch, a.addressString);
+  const bPoints = countSimilarityPoints(addressSearch, b.addressString);
+
+  if (aPoints < bPoints) return 1;
+
+  if (aPoints > bPoints) return -1;
+
+  return 0;
 };
