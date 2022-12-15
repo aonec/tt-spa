e37a782b8d5d85a7afc2effab34f246170364fb3
anotherIlnaz <mmmr.sh.i@gmail.com>
2022-12-15 14:19:34 +0300

    [R3-693: Start commentPanel component]

--- src/services/devices/housingMeteringDevices/housingMeteringDeviceProfileService/view/HousingMeteringDeviceProfile/HousingMeteringDeviceProfile.tsx
+++ src/services/devices/housingMeteringDevices/housingMeteringDeviceProfileService/view/HousingMeteringDeviceProfile/HousingMeteringDeviceProfile.tsx
@@ -1,14 +1,11 @@
 import { PageHeader } from '01/shared/ui/PageHeader';
-import { EResourceType } from 'myApi';
 import React, { FC } from 'react';
 import { useHistory } from 'react-router-dom';
-import { TrashIconSC } from 'ui-kit/DocumentsService/view/DocumentsLineUpload/DocumentsLineUpload.styled';
-import { UserIcon } from 'ui-kit/icons';
+import { CommentPanel } from 'ui-kit/shared_components/CommentPanel';
 import { GoBack } from 'ui-kit/shared_components/GoBack';
 import { HeaderInfoString } from 'ui-kit/shared_components/HeaderInfoString';
 import { DeviceStatus } from 'ui-kit/shared_components/IndividualDeviceInfo/DeviceStatus';
 import { ResourceIconLookup } from 'ui-kit/shared_components/ResourceIconLookup';
-import { PencilIconSC } from 'ui-kit/shared_components/SelectedEntityPanel/SelectedEntityPanel.styled';
 import { Tabs } from 'ui-kit/Tabs';
 import { getHousingStockItemAddress } from 'utils/getHousingStockItemAddress';
 import { HousingProfileTabs } from '../../housingMeteringDeviceProfileService.types';
@@ -16,25 +13,16 @@ import { CommonInfoTab } from './CommonInfo/CommonInfoTab';
 import { ConnectionSettings } from './ConnectionSettings';
 import { Documents } from './Documents';
 import {
-  CommentComponent,
-  CommentDate,
-  CommentHeader,
-  CommentInfo,
-  CommentText,
-  CommentTitle,
   DeviceModel,
   DeviceNumber,
   DeviceTitle,
-  IconSubstrate,
   LinkSC,
   MockComponent,
   PageGridContainer,
   PageTitle,
   RightBlock,
-  RightButtonsBlock,
   Tasks,
   TasksWrapper,
-  UserName,
   Wrapper,
 } from './HousingMeteringDeviceProfile.styled';
 import { HousingMeteringDeviceProfileProps } from './HousingMeteringDeviceProfile.types';
@@ -128,28 +116,7 @@ export const HousingMeteringDeviceProfile: FC<HousingMeteringDeviceProfileProps>
         {currentTab === HousingProfileTabs.Documents && <Documents />}
 
         <RightBlock>
-          <CommentComponent>
-            <CommentHeader>
-              <CommentTitle>Комментарий</CommentTitle>
-              <RightButtonsBlock>
-                <PencilIconSC />
-                <TrashIconSC />
-              </RightButtonsBlock>
-            </CommentHeader>
-
-            <CommentText>
-              Прибор иногда выходит из строя и сбиваются настройки соединения.
-              Прошу коллег быть с ним более внимательными
-            </CommentText>
-
-            <CommentInfo>
-              <IconSubstrate>
-                <UserIcon />
-              </IconSubstrate>
-              <UserName>Филиппов А.А.</UserName>
-              <CommentDate>12.08.2019 10:36 </CommentDate>
-            </CommentInfo>
-          </CommentComponent>
+          <CommentPanel />
 
           <MockComponent>
             <TasksWrapper>
