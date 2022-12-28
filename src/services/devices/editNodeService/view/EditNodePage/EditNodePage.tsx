a6e84405997261c72d2f41c1d06e46b00ee92fe6
Rinasik <mubarakshin199999@mail.ru>
2022-12-28 23:26:05 +0300

    [R3-720]: Remove switch in edit node page

--- src/services/devices/editNodeService/view/EditNodePage/EditNodePage.tsx
+++ src/services/devices/editNodeService/view/EditNodePage/EditNodePage.tsx
@@ -2,6 +2,7 @@ import { EditNodeCalculatorConnectionContainer } from '01/features/nodes/editNod
 import { PageHeader } from '01/shared/ui/PageHeader';
 import React, { FC } from 'react';
 import { GoBack } from 'ui-kit/shared_components/GoBack';
+import { HeaderInfoString } from 'ui-kit/shared_components/HeaderInfoString';
 import { ResourceIconLookup } from 'ui-kit/shared_components/ResourceIconLookup';
 import { getHousingStockAddress } from 'utils/getHousingStockAddress';
 import { NodeEditGrouptype } from '../../editNodeService.constants';
@@ -28,7 +29,7 @@ export const EditNodePage: FC<EditNodePageProps> = ({
   refetchNode,
   updateNode,
 }) => {
-  const { number, address, resource } = node;
+  const { number, address, resource, commercialStatus } = node;
 
   const formId = 'edit-node-page';
 
@@ -42,7 +43,10 @@ export const EditNodePage: FC<EditNodePageProps> = ({
         <PageHeader title={`Узел ${number}. Редактирование`} />
       </HeaderWrapper>
       <AddressWrapper to={`/objects/profile/${address?.id}`}>
-        {getHousingStockAddress(address, true)}
+        <HeaderInfoString>
+          <>{getHousingStockAddress(address, true)}</>
+          <></>
+        </HeaderInfoString>
       </AddressWrapper>
 
       <TabsSC
