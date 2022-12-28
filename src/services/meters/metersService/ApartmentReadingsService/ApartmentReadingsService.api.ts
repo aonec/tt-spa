2716bb910fc2f2d2e68e8efb2ea21d6c481510ea
Iskander Karimov <pronix432@gmail.com>
2022-12-28 20:42:35 +0300

    [R3-706]: Fixes

--- src/services/meters/metersService/ApartmentReadingsService/ApartmentReadingsService.api.ts
+++ src/services/meters/metersService/ApartmentReadingsService/ApartmentReadingsService.api.ts
@@ -2,27 +2,31 @@ import { axios } from '01/axios';
 import { ApartmentListResponsePagedList, ApartmentResponse } from 'myApi';
 import { GetApartmentsRequestPayload } from './ApartmentReadingsService.types';
 
+const getApartmentId = async (
+  params: Omit<GetApartmentsRequestPayload, 'ApartmentId'>
+) => {
+  const apartments: ApartmentListResponsePagedList | null = await axios.get(
+    'Apartments',
+    { params }
+  );
+
+  const apartmentItem = apartments?.items?.[0];
+
+  if (!apartmentItem) return null;
+
+  const { id } = apartmentItem;
+
+  return id;
+};
+
 export const getApartment = async ({
   ApartmentId,
   ...params
 }: GetApartmentsRequestPayload): Promise<ApartmentResponse | null> => {
-  if (!ApartmentId) {
-    const apartments: ApartmentListResponsePagedList | null = await axios.get(
-      'Apartments',
-      { params }
-    );
+  const id = ApartmentId || (await getApartmentId(params));
 
-    const apartmentItem = apartments?.items?.[0];
-
-    if (!apartmentItem) return null;
-
-    const { id } = apartmentItem;
-
-    ApartmentId = id;
-  }
-  
   const apartment: ApartmentResponse | null = await axios.get(
-    `/Apartments/${ApartmentId}`
+    `/Apartments/${id}`
   );
 
   return apartment;
