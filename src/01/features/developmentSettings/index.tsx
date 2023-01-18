1aef67e3b89636ddaf10f45c0f7fcab9a1c654d3
Iskander Karimov <pronix432@gmail.com>
2023-01-18 09:47:33 +0300

    Add reset button and saving api url in local storage for dev settings

--- src/01/features/developmentSettings/index.tsx
+++ src/01/features/developmentSettings/index.tsx
@@ -9,6 +9,11 @@ import {
 } from './models';
 import { Form } from 'antd';
 import { useForm } from 'effector-forms/dist';
+import { Flex } from '01/shared/ui/Layout/Flex';
+import { Space } from '01/shared/ui/Layout/Space/Space';
+import { Button } from 'ui-kit/Button';
+import { SettingsIcon } from 'ui-kit/icons';
+import { baseURL } from '01/axios';
 
 export const DevSettingsModal = () => {
   const visible = useStore($isDevSettingsModalOpen);
@@ -23,10 +28,20 @@ export const DevSettingsModal = () => {
       onCancel={closeDevSettingsModal}
     >
       <Form.Item label="URL">
-        <InputSC
-          value={fields.devUrl.value}
-          onChange={(e: any) => fields.devUrl.onChange(e.target.value)}
-        />
+        <Flex>
+          <InputSC
+            value={fields.devUrl.value}
+            onChange={(e) => fields.devUrl.onChange(e.target.value)}
+          />
+          <Space />
+          <Button
+            size="small"
+            icon={<SettingsIcon />}
+            onClick={() => fields.devUrl.onChange(baseURL)}
+          >
+            Reset
+          </Button>
+        </Flex>
       </Form.Item>
     </ModalTT>
   );
