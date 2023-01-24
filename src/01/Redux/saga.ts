feb5045e9b204d4b32c2840014e9bdce58595be1
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2023-01-25 04:32:58 +0700

    remove redux-saga

--- src/01/Redux/saga.ts
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-function* test() {
-}
-
-export default function* rootSaga() {
-  // yield all([tweetsSaga(), tagsSaga(), tweetSaga(), userSaga(), usersSaga()]);
-  yield all([test()]);
-}
