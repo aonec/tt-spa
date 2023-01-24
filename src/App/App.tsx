1ec846d27d6a3b45fecdaf9732876682546d3528
Rinasik <mubarakshin199999@mail.ru>
2023-01-24 14:33:08 +0300

    [R3-511]: Update antd

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { Bootstrap } from './Bootstrap';
 import { RouterContainer } from './routerService';
 import 'moment/locale/ru';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 import '01/features/init';
 import '01/css/index.scss';
 import '01/css/styles.css';
