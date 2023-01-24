e7ce1a43692616f5a39a74b02e0fd7c5732946a4
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-01-24 09:29:58 +0300

    [R3-793]

--- src/App/routerService/view/Router/Router.tsx
+++ src/App/routerService/view/Router/Router.tsx
@@ -4,7 +4,6 @@ import { Layout, PageWrapper, Wrapper } from './Router.styled';
 import { RouterProps } from './Router.types';
 import {
   AccessDeniedPage,
-  Contractor,
   ErrorPage,
   IndividualDevice,
   IndividualDeviceEdit,
