54a21c5595440b1cc1b5ad822e4efacb3c825cf3
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2023-01-25 04:19:09 +0700

    fix yup

--- src/services/devices/сhangeODPUService/view/ChangeODPUPage/ChangeODPUForm/ChangeODPUForm.utils.tsx
+++ src/services/devices/сhangeODPUService/view/ChangeODPUPage/ChangeODPUForm/ChangeODPUForm.utils.tsx
@@ -1,6 +1,4 @@
-import { SelectValue } from 'antd/lib/select';
 import moment from 'moment';
-import { NotRequiredNullableArraySchema } from 'yup';
 
 export const checkIsDateNotFuture = (date: moment.Moment): boolean => {
   const diff = date.diff(moment());
