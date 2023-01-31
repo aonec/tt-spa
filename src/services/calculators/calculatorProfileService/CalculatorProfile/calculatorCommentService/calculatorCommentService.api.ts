405a82cac7ffb78bf8bd7e95fa1c870794d1f1fa
Rinasik <mubarakshin199999@mail.ru>
2023-01-31 16:51:16 +0300

    [R3-821]: Finish calculator comment panel

--- src/services/calculators/calculatorProfileService/CalculatorProfile/calculatorCommentService/calculatorCommentService.api.ts
+++ src/services/calculators/calculatorProfileService/CalculatorProfile/calculatorCommentService/calculatorCommentService.api.ts
@@ -1,4 +1,5 @@
 import { axios } from '01/axios';
+import { CalculatorCommentResponse } from 'myApi';
 import { CalculatorCommentPayload } from './calculatorCommentService.types';
 
 export const fetchRemoveComment = (deviceId: number): Promise<void> =>
@@ -7,11 +8,11 @@ export const fetchRemoveComment = (deviceId: number): Promise<void> =>
 export const fetchCreateComment = ({
   deviceId,
   text,
-}: CalculatorCommentPayload): Promise<void> =>
+}: CalculatorCommentPayload): Promise<CalculatorCommentResponse> =>
   axios.post(`Calculators/${deviceId}/comment`, { text });
 
 export const fetchUpdateComment = ({
   deviceId,
   text,
-}: CalculatorCommentPayload): Promise<void> =>
+}: CalculatorCommentPayload): Promise<CalculatorCommentResponse> =>
   axios.put(`Calculators/${deviceId}/comment`, { text });
--- src/services/calculators/calculatorProfileService/CalculatorProfile/calculatorCommentService/calculatorCommentService.model.ts
+++ src/services/calculators/calculatorProfileService/CalculatorProfile/calculatorCommentService/calculatorCommentService.model.ts
@@ -1,6 +1,11 @@
-import { createDomain, sample } from 'effector';
+import { createDomain, merge, sample } from 'effector';
 import { createGate } from 'effector-react';
-import { fetchCreateComment, fetchRemoveComment, fetchUpdateComment } from './calculatorCommentService.api';
+import { CalculatorCommentResponse } from 'myApi';
+import {
+  fetchCreateComment,
+  fetchRemoveComment,
+  fetchUpdateComment,
+} from './calculatorCommentService.api';
 import { CalculatorCommentPayload } from './calculatorCommentService.types';
 
 const domain = createDomain('calculatorCommentService');
@@ -9,12 +14,21 @@ const removeComment = domain.createEvent();
 const removeCommentFx = domain.createEffect<number, void>(fetchRemoveComment);
 
 const createComment = domain.createEvent<string>();
-const createCommentFx = domain.createEffect<CalculatorCommentPayload, void>(fetchCreateComment);
+const createCommentFx = domain.createEffect<
+  CalculatorCommentPayload,
+  CalculatorCommentResponse
+>(fetchCreateComment);
 
 const editComment = domain.createEvent<string>();
-const editCommentFx = domain.createEffect<CalculatorCommentPayload, void>(fetchUpdateComment);
+const editCommentFx = domain.createEffect<
+  CalculatorCommentPayload,
+  CalculatorCommentResponse
+>(fetchUpdateComment);
 
 const CalculatorIdGate = createGate<{ calculatorId: number }>();
+
+const commentEdited = merge([createCommentFx.doneData, editCommentFx.doneData]);
+const commentDelited = removeCommentFx.doneData;
 
 sample({
   source: CalculatorIdGate.state.map(({ calculatorId }) => calculatorId),
@@ -41,6 +55,8 @@ export const calculatorCommentService = {
     removeComment,
     editComment,
     createComment,
+    commentEdited,
+    commentDelited,
   },
   gates: { CalculatorIdGate },
 };
