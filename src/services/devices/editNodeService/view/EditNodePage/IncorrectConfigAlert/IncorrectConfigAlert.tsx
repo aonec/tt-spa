82b2c31458d20aae0737e0956afb280906430b9f
Rinasik <mubarakshin199999@mail.ru>
2023-02-02 09:56:49 +0300

    [R3-837]: Add incorrect config alert

---
+++ src/services/devices/editNodeService/view/EditNodePage/IncorrectConfigAlert/IncorrectConfigAlert.tsx
@@ -0,0 +1,39 @@
+import { Alert } from '01/shared/ui/Alert';
+import React, { FC, useState } from 'react';
+import {
+  ErrorContentWrapper,
+  TextWrapper,
+  WarningsList,
+} from './IncorrectConfigAlert.styled';
+import { IncorrectConfigAlertProps } from './IncorrectConfigAlert.types';
+
+export const IncorrectConfigAlert: FC<IncorrectConfigAlertProps> = ({
+  validationResultArray,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Alert type="incorrect" color="FC525B">
+      <ErrorContentWrapper>
+        <span>
+          Данные с вычислителя не обрабатываются, так как узел не соответствует
+          выбранной конфигурации. Исправьте следующие ошибки:
+        </span>
+
+        {!isOpen && (
+          <TextWrapper onClick={() => setIsOpen(true)}>Показать</TextWrapper>
+        )}
+        {isOpen && (
+          <>
+            <WarningsList>
+              {validationResultArray.map((warning) => (
+                <li>{warning.value}</li>
+              ))}
+            </WarningsList>
+            <TextWrapper onClick={() => setIsOpen(false)}>Скрыть</TextWrapper>
+          </>
+        )}
+      </ErrorContentWrapper>
+    </Alert>
+  );
+};
