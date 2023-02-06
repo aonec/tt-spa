c0a4c67de74f531e51ee1a6d463a80b11b92a626
Iskander Karimov <pronix432@gmail.com>
2023-02-06 16:21:16 +0300

    [R3-861]: Updates

--- src/services/reportsService/reportViewService/reportViewService.utils.ts
+++ src/services/reportsService/reportViewService/reportViewService.utils.ts
@@ -1,12 +1,54 @@
+import moment from 'moment';
 import {
   IndividualDeviceReportRequestPaload,
+  ReportDatePeriod,
   ReportFiltrationFormValues,
 } from './reportViewService.types';
+
+const getDatePeriod = (
+  reportDatePeriod: ReportDatePeriod,
+  dates: { from: moment.Moment | null; to: moment.Moment | null },
+) => {
+  let from = moment(),
+    to = moment();
+
+  if (reportDatePeriod === ReportDatePeriod.FromStartOfMonth) {
+    from = moment().startOf('month');
+  }
+
+  if (reportDatePeriod === ReportDatePeriod.PreviousMonth) {
+    from = moment().add(-1, 'month').startOf('month');
+    to = moment().add(-1, 'month').endOf('month');
+  }
+
+  if (reportDatePeriod === ReportDatePeriod.LastDay) {
+    from = moment().startOf('day');
+    to = moment().endOf('day');
+  }
+
+  if (reportDatePeriod === ReportDatePeriod.LastSevenDays) {
+    from = moment().add(-1, 'week').startOf('week');
+  }
+
+  if (reportDatePeriod === ReportDatePeriod.AnyPeriod) {
+    from = moment(dates.from);
+    to = moment(dates.to);
+  }
+
+  return { from: from?.format('YYYY-MM-DD'), to: to?.format('YYYY-MM-DD') };
+};
 
 export const prepareIndividualDevicesReportData = (
   values: ReportFiltrationFormValues,
 ): IndividualDeviceReportRequestPaload | null => {
   if (!values.reportOption) return null;
+
+  const dates =
+    values.reportDatePeriod &&
+    getDatePeriod(values.reportDatePeriod, {
+      from: values.from,
+      to: values.to,
+    });
 
   return {
     HousingStockId: values.housingStockId || undefined,
@@ -15,8 +57,8 @@ export const prepareIndividualDevicesReportData = (
       : values.houseManagement || undefined,
     ReportOption: values.reportOption,
     Resources: values.resources,
-    From: values.from?.format('YYYY-MM-DD'),
-    To: values.to?.format('YYYY-MM-DD'),
+    From: dates?.from,
+    To: dates?.to,
     ClosingReasons: values.closingReasons,
   };
 };
---
+++ src/services/reportsService/reportViewService/view/ReportViewPage/ReportViewTable/ActsJournalReport/ActsJournalReport.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+import { Wrapper } from './ActsJournalReport.styled';
+import { ActsJournalReportProps } from './ActsJournalReport.types';
+
+export const ActsJournalReport: FC<ActsJournalReportProps> = ({}) => {
+  return <Wrapper></Wrapper>
+};
