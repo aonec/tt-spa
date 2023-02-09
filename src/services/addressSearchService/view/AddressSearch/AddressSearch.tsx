18428c5e130158a73bfeb0cd3a437dfd776b8b36
Rinasik <mubarakshin199999@mail.ru>
2023-02-09 19:21:09 +0300

    [Rel4-201]: Fix streets search

--- src/services/addressSearchService/view/AddressSearch/AddressSearch.tsx
+++ src/services/addressSearchService/view/AddressSearch/AddressSearch.tsx
@@ -23,7 +23,7 @@ export const AddressSearch: FC<AddressSearchProps> = ({
 
   const { match: streetMatch, options } = useAutocomplete(
     values.street,
-    streets
+    streets,
   );
 
   function clearFields(index: number) {
@@ -49,11 +49,13 @@ export const AddressSearch: FC<AddressSearchProps> = ({
     </SelectSC>
   );
 
+  console.log(values.street);
+
   const streetSearch = (index: number, isDisabled?: boolean) => (
     <StyledAutocomplete
       placeholder="Улица"
       ref={refs[index]}
-      value={values.street}
+      value={values.street || ''}
       onChange={(value) =>
         handleChange(SearchFieldType.Street, value.toString())
       }
@@ -154,7 +156,7 @@ export const AddressSearch: FC<AddressSearchProps> = ({
   const searchFields = fields.map((fieldType, index) => {
     const field = fieldsLookup[fieldType](
       index,
-      disabledFields?.includes(fieldType)
+      disabledFields?.includes(fieldType),
     );
 
     return (
