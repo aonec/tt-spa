a90f43a70813914df2f929c81164d0c56e583395
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-02-16 14:11:28 +0300

    [REL4-236]: Fix date and add comment resonse data

--- src/services/calculators/calculatorProfileService/CalculatorProfile/calculatorCommentService/calculatorCommentService.container.tsx
+++ src/services/calculators/calculatorProfileService/CalculatorProfile/calculatorCommentService/calculatorCommentService.container.tsx
@@ -1,22 +1,32 @@
-import { useEvent } from 'effector-react';
+import { useEvent, useStore } from 'effector-react';
 import React, { FC } from 'react';
 import { CommentPanel } from 'ui-kit/shared_components/CommentPanel';
 import { calculatorCommentService } from './calculatorCommentService.model';
 import { CalculatorCommentContainerProps } from './calculatorCommentService.types';
 
-const { inputs, gates } = calculatorCommentService;
+const { outputs, inputs, gates } = calculatorCommentService;
 const { CalculatorIdGate } = gates;
 
 export const CalculatorCommentContainer: FC<
   CalculatorCommentContainerProps
 > = ({ comment, calculatorId }) => {
-  const { lastModifiedDateTime, lastModifiedUser } = comment || {};
+  const lastModifiedDateTimeFromCalculatorData = comment?.lastModifiedDateTime;
+  const lastModifiedUserFromCalculatorData = comment?.lastModifiedUser;
 
   const handleRemoveComment = useEvent(inputs.removeComment);
   const handleCreateComment = useEvent(inputs.createComment);
   const handleEditComment = useEvent(inputs.editComment);
 
+  const commentResponseData = useStore(outputs.$commentResponseData);
+
   const isCommentExist = Boolean(comment);
+
+  const lastModifiedDateTime =
+    commentResponseData?.lastModifiedDateTime ||
+    lastModifiedDateTimeFromCalculatorData;
+
+  const lastModifiedUser =
+    commentResponseData?.lastModifiedUser || lastModifiedUserFromCalculatorData;
 
   return (
     <>
