0f281ee46de0c4779e3c00b24d60e81d12beef02
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-02-17 09:45:43 +0300

    [REL4-109]: Update

---
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/TasksMapFiltration.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+import { Wrapper } from './TasksMapFiltration.styled';
+import { TasksMapFiltrationProps } from './TasksMapFiltration.types';
+
+export const TasksMapFiltration: FC<TasksMapFiltrationProps> = () => {
+  return <Wrapper></Wrapper>;
+};
--- src/services/tasks/tasksMapService/view/TasksMapView/TasksMapView.tsx
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapView.tsx
@@ -1,21 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import { Wrapper } from './TasksMapView.styled';
 import { TasksMapViewProps } from './TasksMapView.types';
-import { Map, Placemark } from '@pbe/react-yandex-maps';
+import {
+  Map,
+  Placemark,
+  FullscreenControl,
+  ZoomControl,
+} from '@pbe/react-yandex-maps';
 import { getTaskIcon } from './TasksMapView.utils';
 
 export const TasksMapView: FC<TasksMapViewProps> = ({ tasks }) => {
+  const map = useRef<null | { getBounds: () => number[][] }>(null);
+
   const data = tasks[0]?.housingStockCoordinates;
+
+  // useEffect(() => {
+  //   console.log(map.current?.getBounds());
+  // }, [map.current]);
 
   return (
     <Wrapper>
       <Map
+        instanceRef={map}
         width={'100%'}
-        height={'780px'}
+        height={'calc(100vh - 130px)'}
         defaultState={{
           center: [data?.latitude || 55.75, data?.longitude || 37.57],
           zoom: 14,
         }}
+        onLoad={(api) => console.log(api)}
       >
         {tasks.map((task) => (
           <Placemark
@@ -34,6 +47,15 @@ export const TasksMapView: FC<TasksMapViewProps> = ({ tasks }) => {
             }}
           />
         ))}
+        <FullscreenControl />
+        <ZoomControl
+          options={{
+            position: {
+              top: 50,
+              right: 10,
+            },
+          }}
+        />
       </Map>
     </Wrapper>
   );
