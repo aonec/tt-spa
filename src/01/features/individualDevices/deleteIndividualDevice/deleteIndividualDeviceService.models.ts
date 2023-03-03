c3295bdfec38530a6e075401d3536eb3acc82b5c
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-03-03 10:42:46 +0300

    [R3-591]: move last 403

--- src/01/features/individualDevices/deleteIndividualDevice/deleteIndividualDeviceService.models.ts
+++ src/01/features/individualDevices/deleteIndividualDevice/deleteIndividualDeviceService.models.ts
@@ -34,11 +34,6 @@ forward({
 });
 
 deleteIndividualDeviceFx.failData.watch((error) => {
-  if (error.response.status === 403) {
-    return message.error(
-      'У вашего аккаунта нет доступа к выбранному действию. Уточните свои права у Администратора',
-    );
-  }
   return message.error(
     error.response.data.error.Text ||
       error.response.data.error.Message ||
