5cf0183c42a8eae984c7584a67502ec7b8409c24
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-03-03 12:08:54 +0300

    [REL4-283]: Add calculator icon

--- src/services/tasks/tasksMapService/view/TasksMapView/TasksMapView.tsx
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapView.tsx
@@ -8,7 +8,10 @@ import {
   Placemark,
 } from '@pbe/react-yandex-maps';
 import { TasksMapFiltration } from './TasksMapFiltration';
-import { ResourcesPlacemarksLookup } from './TasksMapView.constants';
+import {
+  CalculatorPlacemark,
+  ResourcesPlacemarksLookup,
+} from './TasksMapView.constants';
 import { EActResourceType, EResourceType } from 'myApi';
 
 export const TasksMapView: FC<TasksMapViewProps> = ({
@@ -47,34 +50,37 @@ export const TasksMapView: FC<TasksMapViewProps> = ({
       >
         {housingStocksWithTasks.map((housingStockWithTask) => {
           const textPosition =
-            String(housingStockWithTask.tasks?.length).length === 2 ? 23 : 25.6;
+            String(housingStockWithTask.tasks?.length).length === 2
+              ? 24.5
+              : 27.3;
 
           const allTasksResources = housingStockWithTask.tasks?.reduce(
             (acc, elem) => [...acc, ...(elem.resourceTypes || [])],
             [] as EResourceType[],
           );
 
-          const placemarkSvgCodeText = allTasksResources
+          const placemarkSvgCodeText = allTasksResources?.length
             ? ResourcesPlacemarksLookup[
                 (allTasksResources?.length || 1) > 1
                   ? EActResourceType.All
                   : allTasksResources?.[0]
               ]
-            : '';
+            : CalculatorPlacemark;
 
           const svgCodeText = `
             <svg width="36" height="48" viewBox="0 0 36 48" fill="none" xmlns="http://www.w3.org/2000/svg">
               ${placemarkSvgCodeText}
               ${
                 (housingStockWithTask.tasks?.length || 0) > 1 &&
-                `<path d="M21 8C21 4.13401 24.134 1 28 1C31.866 1 35 4.13401 35 8C35 11.866 31.866 15 28 15C24.134 15 21 11.866 21 8Z" fill="#272F5A" stroke="#272F5A" stroke-width="2"/>
-                   <text x="${textPosition}px" y="11px" font-family="PTRootUIWeb" fill="white" style="font: 9px sans-serif;">${housingStockWithTask.tasks?.length}</text>      
+                `<path d="M22.3147 8.5C22.3147 4.35786 25.6726 1 29.8147 1V1C33.9569 1 37.3147 4.35786 37.3147 8.5V8.5C37.3147 12.6421 33.9569 16 29.8147 16V16C25.6726 16 22.3147 12.6421 22.3147 8.5V8.5Z" fill="#272F5A" stroke="white"/>
+                   <text x="${textPosition}px" y="11.4px" font-family="PTRootUIWeb" fill="white" style="font: 9px sans-serif;">${housingStockWithTask.tasks?.length}</text>      
                 `
               }
-              
             </svg>`;
 
           const iconHrev = 'data:image/svg+xml;base64,' + btoa(svgCodeText);
+
+          console.log(placemarkSvgCodeText, svgCodeText);
 
           return (
             <Placemark
