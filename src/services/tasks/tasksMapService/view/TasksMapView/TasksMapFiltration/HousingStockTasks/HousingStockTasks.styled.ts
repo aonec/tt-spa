c121bf8c42e7d233eabd42ba1e0d6124323e2bef
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-03-03 14:28:36 +0300

    [REL4-283]: Update

--- src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/HousingStockTasks.styled.ts
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/HousingStockTasks.styled.ts
@@ -4,12 +4,14 @@ import { ChevronIcon } from 'ui-kit/icons';
 export const HousingStockWrapper = styled.div`
   border-top: 1px solid #f3f5f6;
   padding: 16px 16px 0 16px;
-  height: 400px;
+  max-height: 560px;
+  overflow-y: auto;
 `;
 
 export const Header = styled.div`
   display: flex;
   gap: 16px;
+  padding-bottom: 16px;
 `;
 
 export const Address = styled.div`
@@ -28,4 +30,41 @@ export const City = styled.div`
 export const ChevronIconSC = styled(ChevronIcon)`
   transform: translateY(3px);
   cursor: pointer;
+`;
+
+export const TaskItem = styled.a`
+  border-top: 1px solid #f3f5f6;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 8px 16px 0;
+  cursor: pointer;
+
+  &:hover {
+    * {
+      color: #189ee9 !important;
+    }
+
+    .chevron-icon-right {
+      transform: rotate(180deg) translateX(-8px);
+    }
+  }
+`;
+
+export const TaskInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  transition: 0.2s;
+`;
+
+export const TaskTitle = styled.div`
+  color: rgba(39, 47, 90, 0.9);
+  font-weight: 500;
+  font-size: 16px;
+`;
+
+export const ChevronRightIcon = styled(ChevronIcon)`
+  transform: rotate(180deg);
+  transition: 0.2s;
 `;
--- src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/HousingStockTasks.tsx
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/HousingStockTasks.tsx
@@ -1,10 +1,17 @@
+import { EActResourceType } from 'myApi';
 import React, { FC } from 'react';
+import { CalculatorIcon } from 'ui-kit/icons';
+import { ResourceIconLookup } from 'ui-kit/shared_components/ResourceIconLookup';
 import {
   Address,
   ChevronIconSC,
+  ChevronRightIcon,
   City,
   Header,
   HousingStockWrapper,
+  TaskInfo,
+  TaskItem,
+  TaskTitle,
 } from './HousingStockTasks.styled';
 import { HousingStockTasksProps } from './HousingStockTasks.types';
 
@@ -22,6 +29,30 @@ export const HousingStockTasks: FC<HousingStockTasksProps> = ({
           <City>{`${address?.city}`}</City>
         </Address>
       </Header>
+      {selectedHousingStock?.tasks?.map((task) => {
+        const resource =
+          (task.resourceTypes?.length || 1) > 1
+            ? EActResourceType.All
+            : task.resourceTypes?.[0];
+
+        return (
+          <TaskItem
+            key={task.id}
+            href={`/tasks/profile/${task.id}`}
+            target="_blank"
+          >
+            <TaskInfo>
+              {resource ? (
+                <ResourceIconLookup resource={resource} />
+              ) : (
+                <CalculatorIcon />
+              )}
+              <TaskTitle>{task.type}</TaskTitle>
+            </TaskInfo>
+            <ChevronRightIcon className="chevron-icon-right" />
+          </TaskItem>
+        );
+      })}
     </HousingStockWrapper>
   );
 };
