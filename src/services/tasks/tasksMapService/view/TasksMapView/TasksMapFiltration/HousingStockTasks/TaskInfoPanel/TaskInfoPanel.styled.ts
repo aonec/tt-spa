87bdd1959e746128ba70edd0a3c00cb404e570a5
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-03-05 04:49:43 +0300

    [REL4-283]: Add some fixes

--- src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/TaskInfoPanel/TaskInfoPanel.styled.ts
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/TaskInfoPanel/TaskInfoPanel.styled.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components';
+import { Footer } from '../../TasksMapFiltration.styled';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  padding-top: 12px;
+  border-top: 1px solid #f3f5f6;
+`;
+
+export const Title = styled.div`
+  color: rgba(39, 47, 90, 0.9);
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+`;
+
+export const Description = styled.div`
+  color: rgba(39, 47, 90, 0.8);
+  font-weight: 300;
+  font-size: 12px;
+  margin-top: 4px;
+`;
+
+export const FooterWrapper = styled(Footer)`
+  margin-top: 16px;
+  width: calc(100% + 32px);
+  transform: translateX(-16px);
+`;
+
+export const TimelineWrapper = styled.div`
+  margin-top: 8px;
+`;
+
+export const TimerWrapper = styled.div`
+  margin-top: 8px;
+`;
+
+export const ListWrapper = styled.div`
+  margin-top: 12px;
+`;
--- src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/TaskInfoPanel/TaskInfoPanel.tsx
+++ src/services/tasks/tasksMapService/view/TasksMapView/TasksMapFiltration/HousingStockTasks/TaskInfoPanel/TaskInfoPanel.tsx
@@ -1,7 +1,62 @@
+import moment from 'moment';
 import React, { FC } from 'react';
-import { Wrapper } from './TaskInfoPanel.styled';
+import { Link } from 'react-router-dom';
+import {
+  createTimeline,
+  createTimer,
+} from 'services/tasks/tasksProfileService/tasksProfileService.utils';
+import { Button } from 'ui-kit/Button';
+import { CommonInfo } from 'ui-kit/shared_components/CommonInfo';
+import { TimeLine } from 'ui-kit/shared_components/TimeLine';
+import { Timer } from 'ui-kit/shared_components/Timer';
+import {
+  Description,
+  FooterWrapper,
+  ListWrapper,
+  TimelineWrapper,
+  TimerWrapper,
+  Title,
+  Wrapper,
+} from './TaskInfoPanel.styled';
 import { TaskInfoPanelProps } from './TaskInfoPanel.types';
 
 export const TaskInfoPanel: FC<TaskInfoPanelProps> = ({ task }) => {
-  return <Wrapper>{task.id}</Wrapper>;
+  const timeline = createTimeline(task);
+  const timer = createTimer(task);
+
+  return (
+    <Wrapper>
+      <Title>{task.name}</Title>
+      <Description>{task.currentStage?.name}</Description>
+      <TimelineWrapper>
+        {timeline && <TimeLine timeline={timeline} isShowInfo={false} />}
+      </TimelineWrapper>
+      <TimerWrapper>
+        <Timer timer={timer} />
+      </TimerWrapper>
+      <ListWrapper>
+        <CommonInfo
+          items={[
+            {
+              key: 'Тип неисправности',
+              value: task.creationReason,
+            },
+            {
+              key: 'Дата создания',
+              value: moment(task.creationTime).format('DD.MM.YYYY'),
+            },
+            {
+              key: 'Исполнитель',
+              value: task.perpetrator?.name,
+            },
+          ]}
+        />
+      </ListWrapper>
+      <FooterWrapper>
+        <Link to={`/tasks/profile/${task.id}`}>
+          <Button size="small">Перейти к задаче</Button>
+        </Link>
+      </FooterWrapper>
+    </Wrapper>
+  );
 };
