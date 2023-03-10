c3ea8c7366561dd2f956528613e7a24aaa2ff646
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-03-10 19:10:40 +0300

    [REL4-211]: fixes

--- src/App/routerService/routerService.container.tsx
+++ src/App/routerService/routerService.container.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useStore } from 'effector-react';
 import { Router } from './view/Router';
 import { routerService } from './routerService.model';
+import { ESecuredIdentityRoleName } from 'myApi';
 
 const { outputs } = routerService;
 
@@ -10,11 +11,13 @@ export const RouterContainer = () => {
 
   const isRolesLoadded = useStore(outputs.$isCurrentUserLoading);
 
-  const roles = currentUserRoles?.map(({ key }) => key!) || [];
+  const roles =
+    currentUserRoles.reduce((acc, { key }) => {
+      if (!key) {
+        return acc;
+      }
+      return [...acc, key];
+    }, [] as ESecuredIdentityRoleName[]) || [];
 
-  return (
-    <>
-      <Router roles={roles} isRolesLoadded={isRolesLoadded} />
-    </>
-  );
+  return <Router roles={roles} isRolesLoadded={isRolesLoadded} />;
 };
