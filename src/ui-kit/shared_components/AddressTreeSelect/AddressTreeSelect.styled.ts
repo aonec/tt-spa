62029a56913d02e90faae049e1275ff596a5c851
Rinasik <mubarakshin199999@mail.ru>
2023-03-13 21:04:27 +0300

    [Rel4-127]: Add styles in constant

--- src/ui-kit/shared_components/AddressTreeSelect/AddressTreeSelect.styled.ts
+++ src/ui-kit/shared_components/AddressTreeSelect/AddressTreeSelect.styled.ts
@@ -1,31 +1,30 @@
 import { TreeSelect } from 'ui-kit/TreeSelect';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const TreeSelectSmallStyles = css`
+  height: 32px;
+
+  * {
+    font-size: 14px !important;
+    line-height: 22px;
+  }
+  .ant-select-selection-placeholder {
+    line-height: 22px;
+    padding-left: 0px;
+  }
+  .ant-select-selection-item {
+    padding: 0px;
+  }
+
+  .ant-select-selection-search {
+    transform: translateY(-6px);
+  }
+
+  .ant-select-arrow {
+    margin-right: 0px;
+  }
+`;
 
 export const TreeSelectSC = styled(TreeSelect)<{ small: boolean }>`
-  ${({ small }) =>
-    small &&
-    `
-      height: 32px;
-
-      * {
-        font-size: 14px !important;
-        line-height: 22px;
-      }
-      .ant-select-selection-placeholder {
-        line-height: 22px;
-        padding-left: 0px;
-      }
-      .ant-select-selection-item {
-        padding: 0px;
-      }
-
-      .ant-select-selection-search {
-        transform: translateY(-6px);
-      }
-
-      .ant-select-arrow {
-        margin-right: 0px;
-      }
- 
-    `}
+  ${({ small }) => small && TreeSelectSmallStyles}
 `;
