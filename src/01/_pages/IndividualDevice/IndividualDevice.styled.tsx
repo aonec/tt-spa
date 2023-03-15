bce6d0e358398ad16c355257acc7255c4bc15e91
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-03-15 16:17:28 +0300

    [REL4-325]: delete some 01/_components

--- src/01/_pages/IndividualDevice/IndividualDevice.styled.tsx
+++ src/01/_pages/IndividualDevice/IndividualDevice.styled.tsx
@@ -8,3 +8,10 @@ export const TabsSC = styled(Tabs)`
     font-size: 16px;
   }
 `;
+
+export const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 8fr 4fr;
+  grid-gap: 16px;
+  align-content: start;
+`;
