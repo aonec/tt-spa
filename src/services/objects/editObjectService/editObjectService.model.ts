0e10e100dd79204a3f3d71a17fdb9994201c0de1
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-03-29 14:26:38 +0300

    [REL4-381]

--- src/services/objects/editObjectService/editObjectService.model.ts
+++ src/services/objects/editObjectService/editObjectService.model.ts
@@ -1,16 +1,7 @@
-import { createDomain, sample } from 'effector';
-import { createGate } from 'effector-react';
+import { createDomain } from 'effector';
 import { objectProfileService } from '../objectProfileService';
 
 const domain = createDomain('editObjectService');
-const FetchObjectGate = createGate<{ objectId: string }>();
-
-// sample({
-//   clock: FetchObjectGate.open,
-//   source: FetchObjectGate.state,
-//   fn: (gateState) => gateState.objectId,
-//   target: 
-// });
 
 export const editObjectService = {
   inputs: {},
