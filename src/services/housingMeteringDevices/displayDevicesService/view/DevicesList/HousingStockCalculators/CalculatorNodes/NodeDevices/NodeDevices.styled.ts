82f62f35209ff195ee346003ef5a90eac3ff8828
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-04-06 18:06:38 +0300

    [REL4-429]

--- src/services/housingMeteringDevices/displayDevicesService/view/DevicesList/HousingStockCalculators/CalculatorNodes/NodeDevices/NodeDevices.styled.ts
+++ src/services/housingMeteringDevices/displayDevicesService/view/DevicesList/HousingStockCalculators/CalculatorNodes/NodeDevices/NodeDevices.styled.ts
@@ -1,3 +1,32 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const MeteringDeviceWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 4.5fr 3fr 1.5fr 2fr 1fr;
+  margin-bottom: 24px;
+  margin-left: 24px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TitleWrapper = styled.div`
+  margin-left: 24px;
+  display: flex;
+  align-items: center;
+`;
+
+export const ServiceZone = styled.span`
+  font-size: 16px;
+  line-height: 2;
+  color: var(--main-70);
+`;
+
+export const CommercialAct = styled.div`
+  font-size: 12px;
+  color: var(--main-70);
+  padding-left: 48px;
+`;
+
+export const Diameter = styled.div`
+  margin: 0 auto;
+`;
--- src/services/housingMeteringDevices/displayDevicesService/view/DevicesList/HousingStockCalculators/CalculatorNodes/NodeDevices/NodeDevices.tsx
+++ src/services/housingMeteringDevices/displayDevicesService/view/DevicesList/HousingStockCalculators/CalculatorNodes/NodeDevices/NodeDevices.tsx
@@ -1,7 +1,67 @@
 import React, { FC } from 'react';
-import { Wrapper } from './NodeDevices.styled';
 import { NodeDevicesProps } from './NodeDevices.types';
+import {
+  CommercialAct,
+  Diameter,
+  MeteringDeviceWrapper,
+  ServiceZone,
+  TitleWrapper,
+} from './NodeDevices.styled';
+import { DeviceLink, SerialNumber } from '../CalculatorNodes.styled';
+import { DateRange } from 'ui-kit/shared_components/DateRange';
+import { ResourceIconLookup } from 'ui-kit/shared_components/ResourceIconLookup';
 
 export const NodeDevices: FC<NodeDevicesProps> = ({ node }) => {
-  return <Wrapper></Wrapper>;
+  const housingDevices = node.communicationPipes?.map((pipe) => {
+    const devices = pipe.devices?.map((housingDevice) => {
+      return (
+        <MeteringDeviceWrapper key={housingDevice.id}>
+          <div>
+            <TitleWrapper>
+              <DeviceLink to={`/housingMeteringDevices/${housingDevice.id}`}>
+                {`${housingDevice.model} `}
+                <SerialNumber>({housingDevice.serialNumber})</SerialNumber>
+              </DeviceLink>
+            </TitleWrapper>
+          </div>
+
+          <DateRange
+            firstDate={housingDevice.lastCheckingDate}
+            lastDate={housingDevice.futureCheckingDate}
+          />
+
+          <Diameter>
+            {housingDevice.diameter ? housingDevice.diameter + ' мм' : ''}
+          </Diameter>
+        </MeteringDeviceWrapper>
+      );
+    });
+    return devices;
+  });
+
+  return (
+    <div>
+      <div style={{ marginBottom: 24 }}>
+        <div>
+          <TitleWrapper>
+            <DeviceLink to={`/nodes/${node.id}`}>
+              <ResourceIconLookup resource={node.resource} />
+              <span>{`Узел ${node.number}`}</span>
+            </DeviceLink>
+            <ServiceZone>{node.nodeServiceZone?.name}</ServiceZone>
+          </TitleWrapper>
+        </div>
+
+        <CommercialAct>
+          <span>Акт-допуска </span>
+          <DateRange
+            firstDate={node.lastCommercialAccountingDate}
+            lastDate={node.futureCommercialAccountingDate}
+          />
+        </CommercialAct>
+      </div>
+
+      {housingDevices}
+    </div>
+  );
 };
