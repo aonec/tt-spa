80878dd09b03c49e241c7d342d134221d8f3e302
Rinasik <mubarakshin199999@mail.ru>
2023-04-11 20:05:40 +0300

    [Rel4-449]: Add sorting icon

--- src/01/features/nodes/nodeArchiveService/view/NodeArchivePage/components/NodeInfo/NodeInfo.tsx
+++ src/01/features/nodes/nodeArchiveService/view/NodeArchivePage/components/NodeInfo/NodeInfo.tsx
@@ -1,9 +1,9 @@
-import { IconTT } from '01/tt-components';
 import React, { FC } from 'react';
-import { NodeName, Wrap } from './NodeInfo.styled';
+import { Wrap } from './NodeInfo.styled';
 import { NodeInfoProps } from './NodeInfo.types';
 import { LoadingSkeleton } from './components/LoadingSkeleton';
 import { getHousingStockAddress } from 'utils/getHousingStockAddress';
+import { ResourceIconLookup } from 'ui-kit/shared_components/ResourceIconLookup';
 
 export const NodeInfo: FC<NodeInfoProps> = ({ node, loading }) => {
   const address = node?.address && getHousingStockAddress(node.address, true);
@@ -17,9 +17,9 @@ export const NodeInfo: FC<NodeInfoProps> = ({ node, loading }) => {
 
   return (
     <Wrap>
-      {address}
-      <IconTT style={{ marginLeft: 10 }} icon={node.resource.toLowerCase()} />
-      <NodeName>Узел {node.number}</NodeName>
+      <div>{address}</div>
+      <ResourceIconLookup resource={node.resource} />
+      <div>Узел {node.number}</div>
     </Wrap>
   );
 };
