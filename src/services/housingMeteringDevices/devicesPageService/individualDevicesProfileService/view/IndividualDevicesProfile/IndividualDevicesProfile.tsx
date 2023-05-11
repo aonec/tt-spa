6031c40f9c047e992706c50b38ef324a6253becd
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-05-11 15:21:34 +0300

    [REL4-516]: Complete

--- src/services/housingMeteringDevices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/IndividualDevicesProfile.tsx
+++ src/services/housingMeteringDevices/devicesPageService/individualDevicesProfileService/view/IndividualDevicesProfile/IndividualDevicesProfile.tsx
@@ -1,30 +1,44 @@
 import { Radio } from 'antd';
-import React, { FC } from 'react';
+import React, { FC, ReactElement, ReactNode } from 'react';
 import { DevicesSearchType } from 'services/housingMeteringDevices/devicesPageService/devicesPageService.types';
 import { IndividualDevicesViewComponents } from './IndividualDevicesProfile.constants';
 import { Wrapper } from './IndividualDevicesProfile.styled';
 import { IndividualDevicesProfileProps } from './IndividualDevicesProfile.types';
+import { HeaderInject } from 'services/objects/objectsProfileService/view/ObjectsProfile/ObjectsProfile.types';
 
-export const IndividualDevicesProfile: FC<IndividualDevicesProfileProps> = ({
-  devicesSearchType,
-  setDevicesSearchType,
-}) => {
+export const IndividualDevicesProfile: FC<
+  IndividualDevicesProfileProps & HeaderInject
+> = ({ devicesSearchType, setDevicesSearchType, Header }) => {
   const Component = IndividualDevicesViewComponents[devicesSearchType];
+
+  const CustomHeader: FC<{ children: ReactNode }> = ({ children }) => {
+    return (
+      <Header>
+        <Radio.Group
+          value={devicesSearchType}
+          onChange={(value) =>
+            setDevicesSearchType(value.target.value as DevicesSearchType)
+          }
+        >
+          <Radio value={DevicesSearchType.SearialNumber}>
+            Поиск по прибору
+          </Radio>
+          <Radio value={DevicesSearchType.Address}>Поиск по адресу</Radio>
+        </Radio.Group>
+        {children}
+      </Header>
+    );
+  };
 
   return (
     <Wrapper>
-      <Radio.Group
-        value={devicesSearchType}
-        onChange={(value) =>
-          setDevicesSearchType(value.target.value as DevicesSearchType)
+      <Component
+        Header={
+          CustomHeader as unknown as (props: {
+            children: ReactNode;
+          }) => ReactElement
         }
-      >
-        <Radio value={DevicesSearchType.SearialNumber}>Поиск по прибору</Radio>
-        <Radio value={DevicesSearchType.Address}>Поиск по адресу</Radio>
-      </Radio.Group>
-      <div>
-        <Component />
-      </div>
+      />
     </Wrapper>
   );
 };
--- src/services/housingMeteringDevices/devicesPageService/view/DevicesPageProfile/DevicesPageProfile.styled.ts
+++ src/services/housingMeteringDevices/devicesPageService/view/DevicesPageProfile/DevicesPageProfile.styled.ts
@@ -1,9 +1,4 @@
-import { Tabs } from 'antd';
 import styled from 'styled-components';
-
-export const TabsSC = styled(Tabs)`
-  padding: 0 0 16px 8px;
-`;
 
 export const FiltrationWrapper = styled.div`
   position: sticky;
