900033a7f59eb1825362ab65597105e75f444de5
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-05-23 13:31:45 +0300

    fix date in add PN form

--- src/services/homeowner/personalNumber/components/PersonalNumberForm/PersonalNumberForm.tsx
+++ src/services/homeowner/personalNumber/components/PersonalNumberForm/PersonalNumberForm.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+import moment from 'moment';
 import {
   DeleteButton,
   FlexContainer,
@@ -40,7 +41,8 @@ export const PersonalNumberForm: FC<PersonalNumberFormProps> = ({
       initialValues: {
         name: (isEdit && homeowner?.name) || '',
         phoneNumber: (isEdit && homeowner?.phoneNumber) || '',
-        openAt: (isEdit && homeowner?.openAt) || '',
+        openAt:
+          (isEdit && moment(homeowner?.openAt).format('YYYY-MM-DD')) || '',
         personalAccountNumber:
           (isEdit && homeowner?.personalAccountNumber) || '',
         paymentCode: (isEdit && homeowner?.paymentCode) || '',
@@ -77,6 +79,8 @@ export const PersonalNumberForm: FC<PersonalNumberFormProps> = ({
       },
     });
 
+  console.log(values.openAt);
+
   useEffect(() => {
     return handleForced?.watch(() => handleSubmit()).unsubscribe;
   }, [handleForced, handleSubmit]);
@@ -86,7 +90,9 @@ export const PersonalNumberForm: FC<PersonalNumberFormProps> = ({
       <FormItem label="Дата открытия лицевого счета">
         <DatePickerNative
           value={values.openAt}
-          onChange={(value) => setFieldValue('openAt', value)}
+          onChange={(value) =>
+            setFieldValue('openAt', moment(value).format('YYYY-MM-DD'))
+          }
           disabled={isEdit}
         />
         <ErrorMessage>{errors.openAt}</ErrorMessage>
