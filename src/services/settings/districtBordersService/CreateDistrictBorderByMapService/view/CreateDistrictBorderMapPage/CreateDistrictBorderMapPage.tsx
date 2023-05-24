67703c4ba6e4dc85026ecf56794daf15eda8d193
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-05-25 02:25:14 +0300

    [R5-30]: Add polygonj

--- src/services/settings/districtBordersService/CreateDistrictBorderByMapService/view/CreateDistrictBorderMapPage/CreateDistrictBorderMapPage.tsx
+++ src/services/settings/districtBordersService/CreateDistrictBorderByMapService/view/CreateDistrictBorderMapPage/CreateDistrictBorderMapPage.tsx
@@ -16,7 +16,15 @@ export const CreateDistrictBorderMapPage: FC<
 
   const [, setMap] = useState<ymaps.Map | null>(null);
 
-  const ymaps = useYMaps(['Map', 'Placemark', 'Clusterer']);
+  const ymaps = useYMaps([
+    'Map',
+    'Polygon',
+    'map.GeoObjects',
+    'geometryEditor.Polygon',
+    'Monitor',
+  ]);
+
+  const [district, setDistrict] = useState<ymaps.Polygon | null>(null);
 
   useEffect(() => {
     if (!ymaps || !mapRef.current) {
@@ -28,14 +36,58 @@ export const CreateDistrictBorderMapPage: FC<
       zoom: 15,
     });
 
+    const district = new ymaps.Polygon(
+      [
+        // Координаты внешнего контура.
+        [
+          [55.6366, 51.8245],
+          [55.6586, 51.8445],
+        ],
+        // Координаты внутреннего контура.
+        [
+          [55.6586, 51.8445],
+          [55.6566, 51.884],
+        ],
+        [
+          [55.6566, 51.8845],
+          [55.6866, 51.9848],
+        ],
+      ],
+      {},
+      {
+        // Курсор в режиме добавления новых вершин.
+        cursor: 'crosshair',
+        // Цвет заливки.
+        fillColor: '#00FF00',
+        // Цвет обводки.
+        strokeColor: '#0000FF',
+        // Ширина обводки.
+        strokeWidth: 5,
+      },
+    );
+    // Добавляем многоугольник на карту.
+    map.geoObjects.add(district);
+
+    setDistrict(district);
     setMap(map);
   }, [ymaps, mapRef]);
+
+  useEffect(() => {
+    console.log(district);
+  }, [district]);
 
   return (
     <Wrapper>
       <Header>
         <GoBack />
-        <Button>Создать район</Button>
+        <Button
+          onClick={() => {
+            console.log(district);
+            (district?.editor as any)?.startDrawing?.();
+          }}
+        >
+          Создать район
+        </Button>
       </Header>
       <MapWrapper>
         <div ref={mapRef} style={{ width: '100%', height: '86vh' }} />
