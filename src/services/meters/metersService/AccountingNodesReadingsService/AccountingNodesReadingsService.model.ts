84c10f32f0873108735d8e976fdf8555c84a43cd
Rinasik <mubarakshin199999@mail.ru>
2023-06-09 10:42:20 +0300

    [R5-128]: Create accountingNodesService

---
+++ src/services/meters/metersService/AccountingNodesReadingsService/AccountingNodesReadingsService.model.ts
@@ -0,0 +1,14 @@
+import { createDomain } from 'effector';
+
+const domain = createDomain(
+  'AccountingNodesReadingsService'
+);
+
+export const AccountingNodesReadingsService = {
+  inputs: {
+  
+  },
+  outputs: {
+
+  },
+};
