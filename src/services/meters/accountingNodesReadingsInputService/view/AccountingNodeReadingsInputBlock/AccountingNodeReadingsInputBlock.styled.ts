3ae70c2d86e455fdc4ef67851802252d271b2f04
Rinasik <mubarakshin199999@mail.ru>
2023-06-15 12:18:11 +0300

    [R5-154]: Delete accounting node readings input block

--- src/services/meters/accountingNodesReadingsInputService/view/AccountingNodeReadingsInputBlock/AccountingNodeReadingsInputBlock.styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const ReadingDate = styled.div`
-  line-height: 0;
-  margin-top: 12px;
-  text-align: right;
-  color: rgba(39, 47, 90, 0.5);
-`;
--- src/services/meters/accountingNodesReadingsInputService/view/AccountingNodeReadingsInputBlock/index.ts
+++
@@ -1,1 +0,0 @@
-export { AccountingNodeReadingsInputBlock } from './AccountingNodeReadingsInputBlock';
