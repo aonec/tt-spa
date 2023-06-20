efe6d770e0b44b36b3d7be5be48a4b4ae8e24acc
Rinasik <mubarakshin199999@mail.ru>
2023-06-20 11:33:34 +0300

    [R5-141]: Add has fault and edit view of data list

--- src/services/nodes/nodeArchiveService/displayNodeArchiveService/view/NodeArchiveList/NodeArchiveList.styled.tsx
+++ src/services/nodes/nodeArchiveService/displayNodeArchiveService/view/NodeArchiveList/NodeArchiveList.styled.tsx
@@ -1,46 +1,31 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { getFilledArray } from 'utils/getFilledArray';
+import { Checkbox } from 'antd';
 
 export const columnWidth = `120px`;
 
-export const Wrap = styled.div`
-  width: 800px;
+export const ListWrapper = styled.div`
+  width: 100%;
   overflow-x: auto;
+`;
+
+export const Wrapper = styled.div`
+  width: 800px;
 `;
 
 export const Header = styled.div<{ columnsCount: number }>`
   width: max-content;
-  height: 50px;
+  height: 48px;
   background: rgba(243, 245, 246, 1);
   display: grid;
   grid-template-columns: ${({ columnsCount }) =>
     getFilledArray(columnsCount, () => columnWidth).join(' ')};
-  grid-gap: 10px;
+  grid-gap: 8px;
   align-items: center;
-  padding: 0 10px;
+  padding: 0 8px;
+  margin-top: 8px;
 `;
 
-const slide = keyframes`
-  0% {
-    background-position: 100% 0%;
-  }
-  
-  100% {
-    background-position: 0% 0%;
-  } 
-`;
-
-export const GradientLoader = styled.div`
-  width: 800px;
-  background: repeating-linear-gradient(
-    45deg,
-    #e8ebff,
-    #e8ebff 10px,
-    #7584d6 10px,
-    #7584d6 20px
-  );
-  height: 5px;
-  background-size: 400% 400%;
-  animation: ${slide} 40s linear infinite;
-  transform: scale(1, -1);
+export const CheckboxSC = styled(Checkbox)`
+  color: #272f5ae5;
 `;
