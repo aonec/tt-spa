dbed8dfec9a041beb148a5abd1b2518bafe6e798
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-07-06 15:53:29 +0300

    [R5-213]: replace ModalTT

--- src/services/resources/chooseTypeOfResourceDisconnectionModalService/chooseTypeOfResourceDisconnectionModalService.container.tsx
+++ src/services/resources/chooseTypeOfResourceDisconnectionModalService/chooseTypeOfResourceDisconnectionModalService.container.tsx
@@ -1,4 +1,3 @@
-import { ModalTT } from '01/shared/ui/ModalTT';
 import { useEvent, useStore } from 'effector-react';
 import React from 'react';
 import { ActionButton } from 'ui-kit/shared_components/ActionButton';
@@ -8,6 +7,7 @@ import {
   HeatIconSC,
   TimerIconSC,
 } from './chooseTypeOfResourceDisconnectionModalService.styled';
+import { FormModal } from 'ui-kit/Modals/FormModal';
 
 const { inputs, outputs } = chooseTypeOfResourceDisconnectionModalService;
 
@@ -21,27 +21,29 @@ export const ChooseTypeOfResourceDisconnectionModalContainer = () => {
   const submitModal = useEvent(inputs.submitModal);
 
   return (
-    <ModalTT
+    <FormModal
+      formId="choose-type-of-resource-disconnection-modal"
       visible={isModalOpen}
       onCancel={() => closeModal()}
       onSubmit={() => submitModal()}
       title="Выберите тип отключения"
-      saveBtnText="Далее"
-    >
-      <ButtonsWrapper>
-        <ActionButton
-          onClick={() => clearInterHeatingSeason()}
-          active={!isInterHeatingSeason}
-          icon={<TimerIconSC />}
-          text="Временное отключение"
-        />
-        <ActionButton
-          onClick={() => setInterHeatingSeason()}
-          active={isInterHeatingSeason}
-          icon={<HeatIconSC />}
-          text="Межотопительный сезон"
-        />
-      </ButtonsWrapper>
-    </ModalTT>
+      submitBtnText="Далее"
+      form={
+        <ButtonsWrapper>
+          <ActionButton
+            onClick={() => clearInterHeatingSeason()}
+            active={!isInterHeatingSeason}
+            icon={<TimerIconSC />}
+            text="Временное отключение"
+          />
+          <ActionButton
+            onClick={() => setInterHeatingSeason()}
+            active={isInterHeatingSeason}
+            icon={<HeatIconSC />}
+            text="Межотопительный сезон"
+          />
+        </ButtonsWrapper>
+      }
+    />
   );
 };
