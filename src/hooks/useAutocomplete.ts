e0903143700c995845a7ce4cbbc4a223cc7ff51e
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-07-06 11:42:54 +0300

    [R5-213]: change countSimilarity

--- src/hooks/useAutocomplete.ts
+++ src/hooks/useAutocomplete.ts
@@ -1,63 +1,27 @@
-import stringSimilarity from 'string-similarity';
+import { countSimilarityPoints } from 'utils/countSimilarityPoints';
 
-export function useAutocomplete(
-  street: string | undefined,
-  streets: string[],
-  optionsNumber = 1,
-) {
-  if (typeof street === 'undefined') return;
-
-  if (street?.toUpperCase() === 'ЛЕ') {
-    street = 'лес';
+export function useAutocomplete(street: string | undefined, streets: string[]) {
+  if (street === undefined) {
+    return null;
   }
 
-  const matches =
-    typeof street === 'string' && Array.isArray(streets)
-      ? stringSimilarity.findBestMatch(
-          street,
-          typeof streets[0] === 'string' ? streets : [''],
-        )
-      : null;
-
-  const matchesRating = matches?.ratings || [];
-
-  const matchesArrayFilteredByWords = matchesRating.filter((value) => {
-    const formatedSearchString = street!.toUpperCase();
-    const formatedStreetString = value.target.toUpperCase();
-
-    const wordsInStreetName = formatedStreetString.split(' ');
-
-    return wordsInStreetName.some((elem) =>
-      elem.startsWith(formatedSearchString),
-    );
-  });
-
-  const matchesArrayFilteredByFullString = matchesRating.filter((value) => {
-    const formatedSearchString = street!.toUpperCase();
-    const formatedStreetString = value.target.toUpperCase();
-
-    const isRequestStringSimilarToStreet =
-      formatedStreetString.includes(formatedSearchString);
-
-    return isRequestStringSimilarToStreet;
-  });
-  const matchesArray = matchesArrayFilteredByWords.length
-    ? matchesArrayFilteredByWords
-    : matchesArrayFilteredByFullString;
-
-  const preparedMatchesArray = matchesArray
-    .sort((a, b) => b.rating - a.rating)
-    .map(({ target }) => ({ value: target }));
-  const match = preparedMatchesArray[0]?.value;
-
-  const options =
-    preparedMatchesArray?.length && street
-      ? preparedMatchesArray.slice(0, optionsNumber)
-      : [];
+  const sortedAddress = sortAddressBySimilarity(street, streets);
 
   return {
-    bestMatch: options[0]?.value,
-    match,
-    options,
+    bestMatch: sortedAddress[0]?.value || '',
+    options: street ? sortedAddress : [],
   };
+}
+
+function sortAddressBySimilarity(addressSearch: string, streets: string[]) {
+  return streets
+    .sort((a, b) => {
+      const aPoints = countSimilarityPoints(addressSearch, a);
+      const bPoints = countSimilarityPoints(addressSearch, b);
+
+      return bPoints - aPoints;
+    })
+    .map((street) => ({
+      value: street,
+    }));
 }
