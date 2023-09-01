e3c67f8f72cf4d0d46faf462684e369168b4c6ba
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-09-01 13:38:35 +0300

    [RE6-115]: fix act journal

--- src/services/actsJournalService/view/ActsJournalProfile/ActsJournalProfile.tsx
+++ src/services/actsJournalService/view/ActsJournalProfile/ActsJournalProfile.tsx
@@ -1,5 +1,10 @@
 import React, { FC, useCallback } from 'react';
-import { ButtonSC, HeaderWrapper, Wrapper } from './ActsJournalProfile.styled';
+import {
+  ButtonSC,
+  HeaderWrapper,
+  StickyWrapper,
+  Wrapper,
+} from './ActsJournalProfile.styled';
 import { ActsJournalProfileProps } from './ActsJournalProfile.types';
 import { AddressSearchContainer } from 'services/addressSearchService';
 import { SearchFieldType } from 'services/addressSearchService/view/AddressSearch/AddressSearch.types';
@@ -30,49 +35,57 @@ export const ActsJournalProfile: FC<ActsJournalProfileProps> = ({
 
   return (
     <>
-      <HeaderWrapper>
-        <AddressSearchContainer
-          fields={[
-            SearchFieldType.City,
-            SearchFieldType.Street,
-            SearchFieldType.House,
-            SearchFieldType.Corpus,
-            SearchFieldType.Apartment,
-          ]}
-          customTemplate={[
-            { fieldType: SearchFieldType.City, templateValue: '0.75fr' },
-            { fieldType: SearchFieldType.Street, templateValue: '1.5fr' },
-            { fieldType: SearchFieldType.House, templateValue: '0.75fr' },
-            { fieldType: SearchFieldType.Apartment, templateValue: '0.75fr' },
-          ]}
-          initialValues={{
-            city: actsFilter.City,
-            house: actsFilter.HousingStockNumber,
-            street: actsFilter.Street,
-            corpus: actsFilter.Corpus,
-            apartment: actsFilter.ApartmentNumber,
-          }}
-          handleSubmit={(values) =>
-            updateActsFilter({
-              City: values.city,
-              Street: values.street,
-              HousingStockNumber: values.house,
-              Corpus: values.corpus,
-              ApartmentNumber: values.apartment,
-            })
-          }
-        />
-        <ButtonSC type="ghost" size="small" onClick={handleClearAddress}>
-          Очистить
-        </ButtonSC>
-      </HeaderWrapper>
       <Wrapper>
-        <ActsListHeader setActsFilter={updateActsFilter} filter={actsFilter} />
-        <AddNewActForm
-          addNewAct={handleCreateAct}
-          isCreateLoading={isCreateLoading}
-          actCreated={actCreated}
-        />
+        <StickyWrapper>
+          <HeaderWrapper>
+            <AddressSearchContainer
+              fields={[
+                SearchFieldType.City,
+                SearchFieldType.Street,
+                SearchFieldType.House,
+                SearchFieldType.Corpus,
+                SearchFieldType.Apartment,
+              ]}
+              customTemplate={[
+                { fieldType: SearchFieldType.City, templateValue: '0.75fr' },
+                { fieldType: SearchFieldType.Street, templateValue: '1.5fr' },
+                { fieldType: SearchFieldType.House, templateValue: '0.75fr' },
+                {
+                  fieldType: SearchFieldType.Apartment,
+                  templateValue: '0.75fr',
+                },
+              ]}
+              initialValues={{
+                city: actsFilter.City,
+                house: actsFilter.HousingStockNumber,
+                street: actsFilter.Street,
+                corpus: actsFilter.Corpus,
+                apartment: actsFilter.ApartmentNumber,
+              }}
+              handleSubmit={(values) =>
+                updateActsFilter({
+                  City: values.city,
+                  Street: values.street,
+                  HousingStockNumber: values.house,
+                  Corpus: values.corpus,
+                  ApartmentNumber: values.apartment,
+                })
+              }
+            />
+            <ButtonSC type="ghost" size="small" onClick={handleClearAddress}>
+              Очистить
+            </ButtonSC>
+          </HeaderWrapper>
+          <ActsListHeader
+            setActsFilter={updateActsFilter}
+            filter={actsFilter}
+          />
+          <AddNewActForm
+            addNewAct={handleCreateAct}
+            isCreateLoading={isCreateLoading}
+            actCreated={actCreated}
+          />
+        </StickyWrapper>
         <ActsList
           actsPagedData={actsPagedData}
           isActsLoading={isActsLoading}
