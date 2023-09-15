69ccd8207669f80215acbb03106674fc939e1ad4
Rinasik <mubarakshin199999@mail.ru>
2023-09-15 15:21:47 +0300

    Edit text

--- src/services/services/sealActionSelectService/view/SealActionSelectProfile/SealActionSelectProfile.tsx
+++ src/services/services/sealActionSelectService/view/SealActionSelectProfile/SealActionSelectProfile.tsx
@@ -47,7 +47,7 @@ export const SealActionSelectProfile: FC<SealProfileProps> = ({
                   <>
                     <TextWrapper>{totalAppointmentsDate}:</TextWrapper>
                     <TextWrapper>
-                      {nearestTotalAppointments.notDistributed} из {totalCount} записей
+                      {nearestTotalAppointments.notDistributed} из {totalCount}
                     </TextWrapper>
                   </>
                 )}
