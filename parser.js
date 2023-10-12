9eec6322d7a7debaa2dc0a40e20bc09c6a5651d3
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-10-12 14:47:37 +0300

    [RE6-317]

--- parser.js
+++ parser.js
@@ -10,13 +10,29 @@ function traverseDirectory(dir) {
       // Read file contents
       let contents = fs.readFileSync(filePath, 'utf8');
       if (contents.indexOf('createDomain') !== -1) {
+        contents = contents
+          .split('\n')
+          .filter((line, index, arr) => {
+            if (line.includes('domain = createDomain')) {
+              arr[index - 1] = '';
+              arr[index] = '';
+              arr[index + 1] = '';
+              return false;
+            }
+            return true;
+          })
+          .join('\n');
 
-      contents = contents.split('\n').filter(line => !line.includes('domain = createDomain')).join('\n');
+        contents = contents
+          .replace(/domain(\s*\.\s*)?/g, '')
+          .replace(/domain\./g, '')
+          .replace(/createDomain\s*,?/g, '')
+          .replace(/import { createDomain } from 'effector';/g, '')
+          .replace(/import { } from 'effector';/g, '');
 
-      contents = contents.replace(/domain\./g, '').replace(/createDomain\s*,?/g, '').replace(/import { createDomain } from 'effector';/g, '');
-       
-      contents = "import { createEffect, createEvent, createStore } from 'effector';\n" + contents;
-
+        contents =
+          "import { createEffect, createEvent, createStore } from 'effector';\n" +
+          contents;
       }
       fs.writeFileSync(filePath, contents);
     } else if (stats.isDirectory()) {
