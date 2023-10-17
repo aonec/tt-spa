438e36e4296f57401c680c19d25aecc42d661a2a
anotherIlnaz <mmmr.sh.i@gmail.com>
2023-10-17 16:02:08 +0300

    [RE6-341]: delete expra louder

--- src/services/nodes/nodeArchiveService/view/NodeArchivePage/NodeArchivePage.tsx
+++ src/services/nodes/nodeArchiveService/view/NodeArchivePage/NodeArchivePage.tsx
@@ -10,12 +10,8 @@ import { NodeInfo } from './components/NodeInfo/';
 import { SearchNodeArchiveFiltersContainer } from '../../searchNodeArchiveFiltersService';
 import { NodeArchiveContainer } from '../../displayNodeArchiveService';
 import { GoBack } from 'ui-kit/shared/GoBack';
-import { WithLoader } from 'ui-kit/shared/WithLoader';
 
-export const NodeArchivePage: FC<NodeArchivePageProps> = ({
-  node,
-  loading,
-}) => {
+export const NodeArchivePage: FC<NodeArchivePageProps> = ({ node }) => {
   return (
     <>
       <StickyWrapper>
@@ -24,9 +20,7 @@ export const NodeArchivePage: FC<NodeArchivePageProps> = ({
         <NodeInfo node={node} />
       </StickyWrapper>
       <Wrap>
-        <WithLoader isLoading={loading} maxWidth="300px">
-          <NodeArchiveContainer />
-        </WithLoader>
+        <NodeArchiveContainer />
 
         <FiltersWrap>
           <SearchNodeArchiveFiltersContainer />
