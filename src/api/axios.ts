cd22a1529fbb9380df862de51461039c2a4c7e78
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-01-25 11:16:58 +0300

    Fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,7 +8,7 @@ import { cancellableUrl } from 'services/cancelRequestService/cancelRequestServi
 import { cancelRequestService } from 'services/cancelRequestService';
 import { isUndefined } from 'lodash/fp';
 
-export const isDevMode = false;
+export const isDevMode = process.env.DEV_SETTINGS !== 'DISABLED';
 
 axios.defaults.baseURL = apiService.outputs.$devUrl.getState();
 
