6b239d1c744915610ccb81920d0a6c9545311e67
anotherIlnaz <mmmr.sh.i@gmail.com>
2024-02-02 13:13:52 +0300

    fix coordinates

--- src/services/currentOrganizationService/currentOrganizationService.api.ts
+++ src/services/currentOrganizationService/currentOrganizationService.api.ts
@@ -1,6 +1,7 @@
 import { axios } from 'api/axios';
 import { OrganizationResponse } from 'api/types';
 
-export function getCurrentManagingFirm(): Promise<OrganizationResponse | null> {
-  return axios.get('Organizations/current');
-}
+export const getCurrentManagingFirm =
+  (): Promise<OrganizationResponse | null> => {
+    return axios.get('Organizations/current');
+  };
--- src/services/objects/objectsProfileService/buildingsMapService/BuildingsMap/BuildingsMap.types.ts
+++ src/services/objects/objectsProfileService/buildingsMapService/BuildingsMap/BuildingsMap.types.ts
@@ -1,6 +1,8 @@
 import { BuildingListResponsePagedList } from 'api/types';
+import { OrganizationCoordinates } from 'services/currentOrganizationService/currentOrganizationService.types';
 
 export type Props = {
   isLoading: boolean;
   existingHousingStocks: BuildingListResponsePagedList | null;
+  organizationCoordinates: OrganizationCoordinates | null;
 };
