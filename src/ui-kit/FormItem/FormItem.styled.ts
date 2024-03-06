cd1858917eae87043ad92d5fcb7be94f29d60ca9
anotherIlnaz <mmmr.sh.i@gmail.com>
2024-03-06 10:22:12 +0300

    [TT-223]: add attr colon

--- src/ui-kit/FormItem/FormItem.styled.ts
+++ src/ui-kit/FormItem/FormItem.styled.ts
@@ -1,4 +1,4 @@
 import { Form } from 'antd';
 import styled from 'styled-components';
 
-export const FormItem = styled(Form.Item)``;
+export const FormItem = styled(Form.Item).attrs({ colon: false })``;
