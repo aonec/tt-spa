9460347b233553a658fb57c8d84af599b87e377a
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-04-04 15:41:23 +0300

    Fixes

--- src/featureToggles/index.ts
+++ src/featureToggles/index.ts
@@ -5,5 +5,4 @@ export const featureToggles = {
   services: true,
   districtsManage: true,
   controllersDistribution: false,
-  chartAnimation: false,
 };
---
+++ src/services/developmentSettings/developmentSettings.constants.ts
@@ -0,0 +1,12 @@
+import { FeatureToggles } from './developmentSettings.types';
+
+export const FeatureTogglesTranslates: {
+  [key in keyof FeatureToggles]: string;
+} = {
+  reportsConstructor: 'Конструктор отчётов',
+  workingRanges: 'Рабочие диапазоны',
+  dispatcherAddTask: 'Диспетчер: создание заявки',
+  services: 'Услуги',
+  districtsManage: 'Районы',
+  controllersDistribution: 'Распределение контролёров',
+};
