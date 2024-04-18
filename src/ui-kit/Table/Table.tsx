03c133b8677f9db397178f0c0d5b6bc4296ba957
anotherIlnaz <mmmr.sh.i@gmail.com>
2024-04-18 14:25:49 +0300

    [TT-354]

--- src/ui-kit/Table/Table.tsx
+++ src/ui-kit/Table/Table.tsx
@@ -112,9 +112,6 @@ export function Table<T>({
 
   return (
     <div>
-      {/* <OverlayScrollbarsComponent
-        options={{ scrollbars: { autoHide: 'never', visibility: 'visible' } }}
-      > */}
       <Wrapper floating={floating} isSticky={isSticky}>
         <HeaderWrapper isSticky={isSticky}>
           <Header temp={temp} css={headerStyles}>
@@ -129,7 +126,6 @@ export function Table<T>({
         </div>
         {!elements.length && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />}
       </Wrapper>
-      {/* </OverlayScrollbarsComponent> */}
 
       {Boolean(elements.length) && pagination && (
         <PaginationWrapper>
