ee70df28d90067147756cd15e7401083c7f4bd37
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-06-04 23:01:10 +0300

    Fix styles

---
+++ src/ui-kit/FormItem/FormItem.styled.ts
@@ -0,0 +1,8 @@
+import FormItem from 'antd/es/form/FormItem';
+import styled from 'styled-components';
+
+export const FormItemSC = styled(FormItem)`
+  .ant-form-item-label {
+    text-align: start !important;
+  }
+`;
