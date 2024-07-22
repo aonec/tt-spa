c535b5c06403afd3f248794f7b90d322e1c9cc79
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-07-22 18:42:52 +0300

    [TT-639]: Update

--- src/services/settings/mvituService/addNodeToIntegration/AddNodeToIntegrationModal/AddNodeToIntegrationModal.tsx
+++ src/services/settings/mvituService/addNodeToIntegration/AddNodeToIntegrationModal/AddNodeToIntegrationModal.tsx
@@ -1,19 +1,50 @@
-import { FC } from 'react';
+import { FC, useMemo, useState } from 'react';
 import { Wrapper } from './AddNodeToIntegrationModal.styled';
 import { Props } from './AddNodeToIntegrationModal.types';
 import { FormModal } from 'ui-kit/Modals/FormModal';
+import { FormItem } from 'ui-kit/FormItem';
+import { Radio } from 'antd';
+import { Select } from 'ui-kit/Select';
+
+type SearchType = 'AddressTerm' | 'CalculatorSerialNumber';
 
 export const AddNodeToIntegrationModal: FC<Props> = ({
   isModalOpen,
   handleCloseModal,
 }) => {
+  const [searchType, setSearchType] = useState<SearchType>('AddressTerm');
+
+  const searchPlaceholder = useMemo(() => {
+    const placeholders = {
+      AddressTerm: 'Введите адрес...',
+      CalculatorSerialNumber: 'Введите серийный номер вычислителя...',
+    };
+
+    return placeholders[searchType];
+  }, [searchType]);
+
   return (
     <FormModal
       formId="add-node-to-integration-modal"
       title="Добавить узел в интеграцию"
       visible={isModalOpen}
       onCancel={handleCloseModal}
-      form={<Wrapper></Wrapper>}
+      form={
+        <Wrapper>
+          <FormItem label="Поиск узла">
+            <Radio.Group
+              value={searchType}
+              onChange={(e) => setSearchType(e.target.value as SearchType)}
+            >
+              <Radio value={'AddressTerm'}>По адресу</Radio>
+              <Radio value={'CalculatorSerialNumber'}>По вычислителю</Radio>
+            </Radio.Group>
+          </FormItem>
+          <FormItem label="Узел">
+            <Select placeholder={searchPlaceholder} showSearch></Select>
+          </FormItem>
+        </Wrapper>
+      }
     />
   );
 };
