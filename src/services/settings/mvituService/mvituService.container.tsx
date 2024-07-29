807d3777a1ba65de3631a677b7c713d2b6cd905c
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-07-29 11:43:06 +0300

    [TT-672]: Add mvitu integration status checking

--- src/services/settings/mvituService/mvituService.container.tsx
+++ src/services/settings/mvituService/mvituService.container.tsx
@@ -4,22 +4,38 @@ import { CreateMvituIntegrationContainer } from './createMvituIntegration';
 import { createMvituIntegrationService } from './createMvituIntegration/createMvituIntegrationService.models';
 import { MvituIntegrationSectionContainer } from './mvituIntegrationSection';
 import { AddNodeToIntegrationContainer } from './addNodeToIntegration';
+import { mvituService } from './mvituService.models';
+import { mvituIntegrationQuery } from './mvituService.api';
+import { StatusType } from 'api/mvitu.types';
+import { WithLoader } from 'ui-kit/shared/WithLoader';
+
+const {
+  gates: { MvituGate },
+} = mvituService;
 
 export const MvituContainer = () => {
-  const { connectIntegration } = useUnit({
-    connectIntegration: createMvituIntegrationService.inputs.openModal,
-  });
+  const { connectIntegration, mvituIntegration, isLoadingMvituIntegration } =
+    useUnit({
+      connectIntegration: createMvituIntegrationService.inputs.openModal,
+      mvituIntegration: mvituIntegrationQuery.$data,
+      isLoadingMvituIntegration: mvituIntegrationQuery.$pending,
+    });
 
-  const isMvituConnected = true;
+  const isMvituConnected =
+    mvituIntegration?.status &&
+    mvituIntegration?.status !== StatusType.Unconfigured;
 
   return (
     <>
+      <MvituGate />
       <AddNodeToIntegrationContainer />
       <CreateMvituIntegrationContainer />
-      {isMvituConnected && <MvituIntegrationSectionContainer />}
-      {!isMvituConnected && (
-        <ConfigureMvituPanel handleConnect={connectIntegration} />
-      )}
+      <WithLoader isLoading={isLoadingMvituIntegration}>
+        {isMvituConnected && <MvituIntegrationSectionContainer />}
+        {!isMvituConnected && (
+          <ConfigureMvituPanel handleConnect={connectIntegration} />
+        )}
+      </WithLoader>
     </>
   );
 };
--- src/services/settings/mvituService/mvituService.models.ts
+++ src/services/settings/mvituService/mvituService.models.ts
@@ -1,14 +1,20 @@
 import { createEvent, sample } from 'effector';
 import { addNodeToIntegrationService } from './addNodeToIntegration/addNodeToIntegrationService.models';
+import { createGate } from 'effector-react';
+import { mvituIntegrationQuery } from './mvituService.api';
 
 const handleAddNodeToIntegration = createEvent();
+const MvituGate = createGate();
 
 sample({
   clock: handleAddNodeToIntegration,
   target: addNodeToIntegrationService.inputs.handleOpenModal,
 });
 
+sample({ clock: MvituGate.open, target: mvituIntegrationQuery.start });
+
 export const mvituService = {
   inputs: { handleAddNodeToIntegration },
   outputs: {},
+  gates: { MvituGate },
 };
