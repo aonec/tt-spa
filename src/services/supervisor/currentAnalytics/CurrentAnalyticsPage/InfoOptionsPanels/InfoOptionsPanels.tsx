3b18f5547a5a4b0ebfa8f87673010d0c67d1e2ba
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-10-03 13:50:54 +0300

    Fix

--- src/services/supervisor/currentAnalytics/CurrentAnalyticsPage/InfoOptionsPanels/InfoOptionsPanels.tsx
+++ src/services/supervisor/currentAnalytics/CurrentAnalyticsPage/InfoOptionsPanels/InfoOptionsPanels.tsx
@@ -6,9 +6,9 @@ import { OptionPanelButton } from 'ui-kit/shared/OptionPanelButton';
 export const InfoOptionsPanels: FC<Props> = () => {
   return (
     <Wrapper>
-      <OptionPanelButton title="Порыва">53</OptionPanelButton>
-      <OptionPanelButton title="Отключений">30</OptionPanelButton>
-      <OptionPanelButton title="Неисправностей">378</OptionPanelButton>
+      <OptionPanelButton title="Порывы">53</OptionPanelButton>
+      <OptionPanelButton title="Отключения">30</OptionPanelButton>
+      <OptionPanelButton title="Неисправности">378</OptionPanelButton>
       <OptionPanelButton title="Время на задачу">15.3 мин</OptionPanelButton>
       <OptionPanelButton title="Качество услуг">70 задач</OptionPanelButton>
     </Wrapper>
