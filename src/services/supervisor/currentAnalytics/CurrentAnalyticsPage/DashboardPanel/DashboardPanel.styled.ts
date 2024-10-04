2ebcf2e2fa6da25f1e35b196f23219fd82963966
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-10-04 13:59:10 +0300

    [TT-754]: Add margin-top: aout

--- src/services/supervisor/currentAnalytics/CurrentAnalyticsPage/DashboardPanel/DashboardPanel.styled.ts
+++ src/services/supervisor/currentAnalytics/CurrentAnalyticsPage/DashboardPanel/DashboardPanel.styled.ts
@@ -4,7 +4,6 @@ export const Wrapper = styled.div`
   border: 1px solid #dcdee4;
   border-radius: 4px;
   width: 100%;
-  height: 540px;
   padding: 24px;
   display: flex;
   flex-direction: column;
@@ -20,4 +19,19 @@ export const ResourceStatisticsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
+`;
+
+export const RatioWrapper = styled.div`
+  font-weight: 500;
+  color: #272f5a;
+  font-size: 16px;
+`;
+
+export const DangerWrapper = styled.span<{ isPositive: boolean }>`
+  font-size: 32px;
+  color: ${(props) => (props.isPositive ? '#FC525B' : '#272F5A')};
+`;
+
+export const LinkButtonWrapper = styled.div`
+  margin-top: auto;
 `;
--- src/services/supervisor/currentAnalytics/CurrentAnalyticsPage/DashboardPanel/DashboardPanel.tsx
+++ src/services/supervisor/currentAnalytics/CurrentAnalyticsPage/DashboardPanel/DashboardPanel.tsx
@@ -1,21 +1,59 @@
 import { FC } from 'react';
 import {
+  DangerWrapper,
+  LinkButtonWrapper,
+  RatioWrapper,
   ResourceStatisticsWrapper,
   Title,
   Wrapper,
 } from './DashboardPanel.styled';
 import { Props } from './DashboardPanel.types';
 import { ResourceStatistic } from './ResourceStatistic';
+import { LinkButton } from 'ui-kit/shared/LinkButton';
 
 export const DashboardPanel: FC<Props> = ({ data }) => {
+  const ratioOfTasksCount = data.details?.reduce(
+    (acc, elem) => {
+      const res = elem.items?.reduce(
+        (acc, elem) => ({
+          ...acc,
+          danger: acc.danger + (elem?.notClosedTasksCount || 0),
+          all: acc.all + (elem?.totalTasksCount || 0),
+        }),
+        {
+          danger: 0,
+          all: 0,
+        },
+      );
+
+      return {
+        danger: (res?.danger || 0) + acc.danger,
+        all: (res?.all || 0) + acc.all,
+      };
+    },
+    {
+      danger: 0,
+      all: 0,
+    },
+  );
+
   return (
     <Wrapper>
       <Title>{data.title}</Title>
+      <RatioWrapper>
+        <DangerWrapper isPositive={(ratioOfTasksCount?.danger || 0) > 0}>
+          {ratioOfTasksCount?.danger}
+        </DangerWrapper>{' '}
+        / {ratioOfTasksCount?.all}
+      </RatioWrapper>
       <ResourceStatisticsWrapper>
         {data.details?.map((detail) => (
           <ResourceStatistic data={detail} />
         ))}
       </ResourceStatisticsWrapper>
+      <LinkButtonWrapper>
+        <LinkButton onClick={() => void 0}>Подробнее</LinkButton>
+      </LinkButtonWrapper>
     </Wrapper>
   );
 };
