156de8aa68540507210a852a37396d7689ee0cc1
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-11-27 14:48:40 +0300

    [TT-848]: Remove redirect url

--- src/App/Bootstrap/Bootstrap.hook.tsx
+++ src/App/Bootstrap/Bootstrap.hook.tsx
@@ -1,13 +1,9 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { stringifyUrl } from 'query-string';
 import { useUnit } from 'effector-react';
 import { developmentSettingsService } from 'services/developmentSettings/developmentSettings.models';
 import { isDevMode } from 'api/axios';
 
 export function useApp() {
-  const navigate = useNavigate();
-
   const { resetFeatureToggles } = useUnit({
     resetFeatureToggles: developmentSettingsService.inputs.resetFeatureToggles,
   });
@@ -17,17 +13,4 @@ export function useApp() {
 
     resetFeatureToggles();
   }, [resetFeatureToggles]);
-
-  useEffect(() => {
-    if (window.location.href.match(/registration/gi)) {
-      return;
-    }
-
-    const token = localStorage.getItem('token');
-    if (!token) {
-      const redirectUrl = window.location.pathname;
-
-      navigate(stringifyUrl({ url: '/login', query: { redirectUrl } }));
-    }
-  }, [navigate]);
 }
