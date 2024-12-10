74d853600ae295dacd5b429c8737e0827b5eda11
anotherIlnaz <mmmr.sh.i@gmail.com>
2024-12-10 15:11:47 +0300

    [TT-859]

--- src/services/currentUser/currentUserService/UserProfile/RegularReports/regularReportsService.types.ts
+++ src/services/currentUser/currentUserService/UserProfile/RegularReports/regularReportsService.types.ts
@@ -1,3 +1,26 @@
 export {};
 
-export type D ={}
+export type D = {
+  id: 3;
+  reportConfigurationDetails: {
+    emails: 'mmmr.sh.i@gmail.com';
+    contractorIds: '2218237';
+    initialDate: '2024-11-01T00:00:00Z';
+    nextDate: null;
+    reportConfigurationPeriod: 'EveryTwoWeeks';
+  };
+  report: {
+    fileName: 'Групповой_отчёт_01.11.2024';
+    groupReportId: null;
+    managementFirmId: null;
+    houseManagementId: '4e5a1cca-5f4b-447c-9266-b2f97a3e85fb';
+    buildingIds: [];
+    nodeResourceTypes: ['HotWaterSupply'];
+    nodeStatus: null;
+    reportType: 'Daily';
+    from: '2024-10-01T00:00:00';
+    to: '2024-10-31T00:00:00';
+    reportFormat: 'Consumption';
+  };
+  creatorId: 1309662;
+};
