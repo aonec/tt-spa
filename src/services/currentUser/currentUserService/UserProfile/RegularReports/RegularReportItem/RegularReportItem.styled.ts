3b5403af28f862c4df05a451765235d4603c003d
anotherIlnaz <mmmr.sh.i@gmail.com>
2024-12-11 19:43:34 +0300

    [TT-859]

--- src/services/currentUser/currentUserService/UserProfile/RegularReports/RegularReportItem/RegularReportItem.styled.ts
+++ src/services/currentUser/currentUserService/UserProfile/RegularReports/RegularReportItem/RegularReportItem.styled.ts
@@ -1,5 +1,43 @@
 import styled from 'styled-components';
 
+export const PageWrapper = styled.div`
+  width: 680px;
+  color: rgba(39, 47, 90, 1);
+`;
+
+export const PageTitle = styled.div`
+  font-size: 24px;
+  margin: 6px 0px 20px 10px;
+`;
+
 export const Wrapper = styled.div`
-    
+  display: grid;
+  grid-template-columns: 0.1fr 2fr;
+  margin-bottom: 20px;
+  margin-top: 5px;
+  padding: 20px 20px 10px 20px;
+  min-height: 64px;
+  box-shadow: 0px 4px 4px rgba(78, 93, 146, 0.16),
+    0px 8px 16px rgba(78, 93, 146, 0.08);
+
+  gap: 6px;
+`;
+
+export const RightBlock = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const ItemHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ReportName = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+  margin-bottom: 10px;
+  cursor: pointer;
 `;
--- src/services/currentUser/currentUserService/UserProfile/RegularReports/regularReportsService.container.tsx
+++ src/services/currentUser/currentUserService/UserProfile/RegularReports/regularReportsService.container.tsx
@@ -1,15 +1,27 @@
+import { useUnit } from 'effector-react';
 import { regularReportsService } from './regularReportsService.models';
+import { RegularReportItem } from './RegularReportItem';
+import { PageTitle, PageWrapper } from './RegularReportItem/RegularReportItem.styled';
 
 const {
   // inputs,
-  // outputs,
+  outputs,
   gates: { PageGate },
 } = regularReportsService;
 
 export const RegularReportsContainer = () => {
+  const { reportsData } = useUnit({
+    reportsData: outputs.$reportsData,
+  });
+
   return (
-    <>
+    <PageWrapper>
+      <PageTitle>Регулярная выгрузка отчетов</PageTitle>
+
+      {reportsData?.map((report) => (
+        <RegularReportItem key={report.id} report={report} isFirst={ reportsData.indexOf(report) === 0} />
+      ))}
       <PageGate />
-    </>
+    </PageWrapper>
   );
 };
