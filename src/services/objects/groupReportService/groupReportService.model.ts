c565ede9231a23cb88cc5876a61e9f7f42e818de
anotherIlnaz <mmmr.sh.i@gmail.com>
2024-12-13 13:56:22 +0300

    [TT-859]

--- src/services/objects/groupReportService/groupReportService.model.ts
+++ src/services/objects/groupReportService/groupReportService.model.ts
@@ -257,6 +257,14 @@ sendByEmailFx.doneData.watch(() => {
   message.success('Отчёт успешно отправлен на почту');
 });
 
+postRegularUploadFx.failData.watch((error) => {
+  return message.error(
+    error.response.data.error.Text ||
+      error.response.data.error.Message ||
+      'Не удалось создать расписание выгрузок',
+  );
+});
+
 export const groupReportService = {
   inputs: {
     openModal,
