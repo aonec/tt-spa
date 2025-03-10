c7b00c98e4a56c1e87ed0fa9335a7788b7b61eeb
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-03-10 17:21:31 +0300

    [TT-1066]

--- src/services/tasks/taskProfileService/view/TaskProfile/TaskPipeNodeInfo/TaskPipeNodeInfo.tsx
+++ src/services/tasks/taskProfileService/view/TaskProfile/TaskPipeNodeInfo/TaskPipeNodeInfo.tsx
@@ -10,24 +10,17 @@ import {
 } from './TaskPipeNodeInfo.styled';
 import { TaskPipeNodeInfoProps } from './TaskPipeNodeInfo.types';
 import { useNavigate } from 'react-router-dom';
-import { EConnectionStatusType } from 'api/types';
 import { Tooltip } from 'ui-kit/shared/Tooltip';
-import { CheckConnection, NoConnectionIcon, WarningIcon } from 'ui-kit/icons';
+import { IncorrectConfigurationIcon } from 'ui-kit/icons';
 
 export const TaskPipeNodeInfo: FC<TaskPipeNodeInfoProps> = ({ pipeNode }) => {
-  const { resource, title, id, calculator } = pipeNode;
+  const { resource, title, id } = pipeNode;
 
   const navigate = useNavigate();
 
-  const isConnectionError = !(
-    calculator?.connection?.port && calculator?.connection?.ipV4
-  );
-
-  const isMalfunction =
-    calculator?.connectionInfo?.connectionStatus ===
-    EConnectionStatusType.DeviceMalfunction;
-
-  const isConnected = calculator?.isConnected;
+  const isIncorrectConfig =
+    (pipeNode?.validationResult?.errors || []).length !== 0 ||
+    (pipeNode?.validationResult?.warnings || []).length !== 0;
 
   return (
     <Wrapper>
@@ -37,19 +30,10 @@ export const TaskPipeNodeInfo: FC<TaskPipeNodeInfoProps> = ({ pipeNode }) => {
         <GroupWrapper>
           <ResourceIconLookup resource={resource} />
           <TextWrapper>Узел {title}</TextWrapper>
-          {!isConnected && (
-            <Tooltip title="Вычислитель не опрашивается">
-              <NoConnectionIcon />
-            </Tooltip>
-          )}
-          {isConnected && isConnectionError && (
-            <Tooltip title="Проверьте настройки соединения">
-              <CheckConnection />
-            </Tooltip>
-          )}
-          {isMalfunction && (
-            <Tooltip title="Вычислитель неисправен">
-              <WarningIcon />
+
+          {isIncorrectConfig && (
+            <Tooltip title="Ошибка конфигурации узла">
+              <IncorrectConfigurationIcon />
             </Tooltip>
           )}
         </GroupWrapper>
