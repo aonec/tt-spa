ea3da6ead769c91f764e8805c9fe16051914c2d8
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-04-02 16:47:22 +0300

    [TT-1132]

--- src/services/reportsService/reportViewService/view/ReportViewPage/ReportViewTable/IndividualDevicesReport/IndividualDevicesReport.styled.ts
+++ src/services/reportsService/reportViewService/view/ReportViewPage/ReportViewTable/IndividualDevicesReport/IndividualDevicesReport.styled.ts
@@ -22,12 +22,6 @@ export const LinkSc = styled(Link)`
   margin-right: 6px;
 `;
 
-export const PhoneNumber = styled.div`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
 export const ClosingDate = styled.div`
   margin-top: 4px;
   font-size: 12px;
@@ -35,25 +29,8 @@ export const ClosingDate = styled.div`
 `;
 
 export const FullAddressWrapper = styled.div`
-  max-width: 230px;
+  max-width: 300px;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
-`;
-
-export const ReadingWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const ReadingValue = styled.div`
-  display: flex;
-  flex-direction: column;
-  line-height: 16px;
-  align-items: center;
-`;
-
-export const ReadingDate = styled.div`
-  font-size: 12px;
-  color: rgba(39, 47, 90, 0.7);
 `;
