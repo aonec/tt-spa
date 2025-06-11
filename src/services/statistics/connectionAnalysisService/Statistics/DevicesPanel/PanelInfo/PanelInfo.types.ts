8c1276b64a26d67874695e71112f1e8d7e9184db
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-06-11 12:02:39 +0300

    update react-to-print

--- src/services/statistics/connectionAnalysisService/Statistics/DevicesPanel/PanelInfo/PanelInfo.types.ts
+++ src/services/statistics/connectionAnalysisService/Statistics/DevicesPanel/PanelInfo/PanelInfo.types.ts
@@ -1,3 +1,5 @@
+import React from 'react';
+import type { JSX } from 'react';
 import { ECalculatorConnectionGroupType } from 'api/types';
 import { DownloadParams } from 'services/statistics/connectionAnalysisService/connectionAnalysisService.types';
 
