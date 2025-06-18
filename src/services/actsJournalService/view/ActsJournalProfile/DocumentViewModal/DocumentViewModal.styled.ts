1215c7364ccbf285bcb9a13ca605d6210cd8e575
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-06-18 14:46:07 +0300

    [TT-1206]

--- src/services/actsJournalService/view/ActsJournalProfile/DocumentViewModal/DocumentViewModal.styled.ts
+++ src/services/actsJournalService/view/ActsJournalProfile/DocumentViewModal/DocumentViewModal.styled.ts
@@ -4,3 +4,10 @@ export const Wrapper = styled.div`
   width: 100%;
   height: auto;
 `;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-right: 50px;
+  align-items: center;
+`;
